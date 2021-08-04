db1454fc8570ef984d3c5bbbe136f6879d84ea31
hwasurr <iamsupermazinga@gmail.com>
2021-08-04 16:24:16 +0900

    CICD: test

--- tools/check-need-deploy.sh
+++ tools/check-need-deploy.sh
@@ -4,10 +4,12 @@ APP=api
 yarn install
 
 # Run the affected command, comparing latest commit to the one before that
-yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP
 
 # Store result of the previous command (grep)
 IS_AFFECTED=$?
+
+echo IS_AFFECTED = $IS_AFFECTED
 
 if [ $IS_AFFECTED -eq 1 ]; then
     echo "🛑 - Build cancelled"
