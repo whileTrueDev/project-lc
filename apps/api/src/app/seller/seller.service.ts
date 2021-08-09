23b1d4b14e3babb4edcd997f2ea890e561c4e81d
hwasurr <iamsupermazinga@gmail.com>
2021-08-09 17:45:51 +0900

    Add: 비밀번호 암호화 추가

--- apps/api/src/app/seller/seller.service.ts
+++ apps/api/src/app/seller/seller.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma, PrismaClient, Seller } from '@prisma/client';
+import argon from 'argon2';
 
 @Injectable()
 export class SellerService {
@@ -8,16 +9,45 @@ export class SellerService {
   /**
    * 회원 가입
    */
-  signUp(signUpInput: Prisma.SellerCreateInput): Promise<Seller> {
+  async signUp(signUpInput: Prisma.SellerCreateInput): Promise<Seller> {
+    const hashedPw = await this.hashPassword(signUpInput.password);
     return this.prisma.seller.create({
-      data: signUpInput,
+      data: {
+        ...signUpInput,
+        password: hashedPw,
+      },
     });
   }
 
   /**
    * 유저 정보 조회
    */
-  findOne(findInput: Prisma.SellerFindUniqueArgs) {
-    return this.prisma.seller.findUnique(findInput);
+  async findOne(findInput: Prisma.SellerWhereUniqueInput) {
+    const seller = await this.prisma.seller.findUnique({
+      where: findInput,
+    });
+
+    return seller;
+  }
+
+  /**
+   * 비밀번호를 단방향 암호화 합니다.
+   * @param purePw 비밀번호 문자열
+   * @returns {string} 비밀번호 해시값
+   */
+  private async hashPassword(purePw: string): Promise<string> {
+    const hashed = await argon.hash(purePw);
+    return hashed;
+  }
+
+  /**
+   * 입력한 비밀번호를 해시된 비밀번호와 비교합니다.
+   * @param pwInput 입력한 비밀번호 문자열
+   * @param hashedPw 해시된 비밀번호 값
+   * @returns {boolean} 올바른 비밀번호인지 여부
+   */
+  private async verifyPassword(pwInput: string, hashedPw: string): Promise<boolean> {
+    const isCorrect = await argon.verify(hashedPw, pwInput);
+    return isCorrect;
   }
 }
