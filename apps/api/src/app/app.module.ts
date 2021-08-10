a3ecc9b176fddaec1bcb3782d20ebae790b87d57
chanuuuuu <dn0208@gmail.com>
2021-08-10 23:45:39 +0900

    [Add] 로그인 모듈 생성

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaModule } from '@project-lc/prisma-orm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SellerModule } from './seller/seller.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [FirstmallDbModule, SellerModule, PrismaModule],
+  imports: [FirstmallDbModule, SellerModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
