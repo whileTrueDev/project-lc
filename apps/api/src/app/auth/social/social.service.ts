e168ab525842a1b735892d4cb278420b0725a5cc
Jiyeon <a1919361@gmail.com>
2021-08-10 15:39:47 +0900

    [Add] socialAccount 테이블 추가

--- apps/api/src/app/auth/social/social.service.ts
+++ apps/api/src/app/auth/social/social.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@project-lc/prisma-orm';
 
 @Injectable()
 export class SocialService {
-  googleLogin(req) {
+  constructor(private readonly prisma: PrismaService) {}
+
+  public async findSellerWithEmail(email: string) {
+    const row = await this.prisma.seller.findUnique({ where: { email } });
+    return row;
+  }
+
+  /** 구글 로그인 */
+  async googleLogin(req) {
     if (!req.user) {
       // 해당 구글 계정이 없음(에러)
       return 'No user from google';
@@ -10,6 +19,8 @@ export class SocialService {
 
     console.log({ reqUser: req.user });
     // 동일 email로 가입된 계정이 있는지 확인
+    const existSeller = await this.findSellerWithEmail(req.user.email);
+    console.log({ existSeller });
 
     // 구글 계정으로 가입한것이면 로그인처리(토큰발급 & 저장)
     // 구글 계정이 아니면 구글 계정으로 로그인 유도(에러)
