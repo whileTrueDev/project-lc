97691ba34461c2b0dbeee86044393ac027455408
Jiyeon <a1919361@gmail.com>
2021-08-19 18:25:03 +0900

    [add] 마이페이지 입장시 로그인 필요 다이얼로그 추가

--- libs/components/src/lib/MypageLayout.tsx
+++ libs/components/src/lib/MypageLayout.tsx
@@ -1,4 +1,19 @@
-import { Box, useBreakpoint } from '@chakra-ui/react';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+  Center,
+  Spinner,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useProfile } from '@project-lc/hooks';
+import { useRouter } from 'next/router';
+import React from 'react';
 import MypageFooter from './MypageFooter';
 import { MypageNavbar } from './MypageNavbar';
 import { Navbar } from './Navbar';
@@ -8,8 +23,15 @@ interface MypageLayoutProps {
 }
 
 export function MypageLayout({ children }: MypageLayoutProps): JSX.Element {
+  const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef<HTMLButtonElement>(null);
+  // getServerSideprops 내에서는 훅, react-query 못씀
+  // TODO: 로그인한 유저정보를 여기서 가져오는 게 맞을지 다시 고려해보기
+  const { data, isFetching, error } = useProfile();
+
   return (
-    <Box>
+    <Box position="relative" pointerEvents={!data || isFetching ? 'none' : 'auto'}>
       <Navbar />
 
       <MypageNavbar />
@@ -19,6 +41,40 @@ export function MypageLayout({ children }: MypageLayoutProps): JSX.Element {
       </Box>
 
       <MypageFooter />
+
+      {/* 전체화면 로딩 */}
+      {(!data || isFetching) && (
+        <Center
+          position="fixed"
+          left="0"
+          top="0"
+          width="100vw"
+          height="100vh"
+          opacity="0.5"
+          bg="gray"
+        >
+          <Spinner />
+        </Center>
+      )}
+
+      {/* 로그인 필요 다이얼로그 */}
+      <AlertDialog isOpen={!!error} leastDestructiveRef={cancelRef} onClose={onClose}>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              로그인이 필요합니다
+            </AlertDialogHeader>
+
+            <AlertDialogBody>메인페이지로 이동합니다</AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button onClick={() => router.push('/')} ml={3}>
+                확인
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </Box>
   );
 }
--- libs/hooks/src/lib/queries/useProfile.tsx
+++ libs/hooks/src/lib/queries/useProfile.tsx
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import { useQuery, UseQueryOptions } from 'react-query';
 import axios from '../../axios';
 
@@ -5,8 +6,8 @@ export const getProfile = async (): Promise<any> => {
   return axios.get<any>('/auth/profile').then((res) => res.data);
 };
 
-export const useProfile = (options?: UseQueryOptions) => {
-  return useQuery<any>('Profile', getProfile, {
+export const useProfile = (options?: UseQueryOptions<any, AxiosError>) => {
+  return useQuery<any, AxiosError>('Profile', getProfile, {
     retry: false,
     ...options,
   });
