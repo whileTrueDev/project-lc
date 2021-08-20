b644e6bcf2dd748aab90ec045bf525cee5a95307
Jiyeon <a1919361@gmail.com>
2021-08-20 16:00:09 +0900

    [remove] 계정 삭제 전 비밀번호 확인 절차 일단 뺌

--- libs/components/src/lib/AccountRemoveSection.tsx
+++ libs/components/src/lib/AccountRemoveSection.tsx
@@ -1,31 +1,38 @@
-import { Button, Text, useBoolean, useDisclosure } from '@chakra-ui/react';
+import { Button, Text, useDisclosure } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import AccountRemoveDialog from './AccountRemoveDialog';
-import PasswordCheckForm from './PasswordCheckForm';
 import SettingSectionLayout from './SettingSectionLayout';
 
 export function AccountRemoveSection(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [flag, { on, off }] = useBoolean();
-  const onRemove = () => console.log('delete');
+  const router = useRouter();
+
+  const logout = () => {
+    console.log('로그아웃 처리');
+    // 로그아웃요청(토큰삭제)
+    // 프론트에서 글로벌에 저장하고 있는 유저정보 삭제
+  };
+
+  const removeRequest = () => {
+    console.log('계정 삭제');
+    return Promise.resolve('true');
+  };
+
+  const onRemove = () => {
+    // TODO: 계정삭제 요청
+    removeRequest().then(async () => {
+      logout();
+      router.push('/');
+    });
+  };
   return (
     <SettingSectionLayout title="회원 탈퇴">
       <>
         <Text>탈퇴하시면 더 이상 project-lc에 로그인 할 수 없습니다.</Text>
 
-        {flag ? (
-          <PasswordCheckForm
-            onCancel={() => off()}
-            onConfirm={() => {
-              off();
-              onOpen();
-            }}
-            onFail={() => off()}
-          />
-        ) : (
-          <Button width="200px" onClick={on}>
-            회원 탈퇴
-          </Button>
-        )}
+        <Button width="200px" onClick={onOpen}>
+          회원 탈퇴
+        </Button>
         <AccountRemoveDialog isOpen={isOpen} onClose={onClose} onRemove={onRemove} />
       </>
     </SettingSectionLayout>
--- libs/components/src/lib/PasswordCheckForm.tsx
+++ libs/components/src/lib/PasswordCheckForm.tsx
@@ -1,29 +1,63 @@
-import { Button, ButtonGroup, Input, Text } from '@chakra-ui/react';
-import { useProfile } from '@project-lc/hooks';
+import { Box, Button, ButtonGroup, Input, Text, useToast } from '@chakra-ui/react';
+import { useProfile, useValidatePassword } from '@project-lc/hooks';
+import { useForm } from 'react-hook-form';
 import SettingSectionLayout from './SettingSectionLayout';
 
 export interface PasswordCheckFormProps {
   onCancel: () => void;
   onConfirm: () => void;
-  onFail: () => void;
+  onFail?: () => void;
 }
 
+// TODO: 비밀번호 확인과정을 넣어야하나???
 export function PasswordCheckForm(props: PasswordCheckFormProps): JSX.Element {
   const { onCancel, onConfirm, onFail } = props;
+  const toast = useToast();
+
   // TODO: useProfile 대신 로그인 된 글로벌 상태에서 email값 가져오기
   const { data } = useProfile();
+  const { mutateAsync } = useValidatePassword();
+  const { register, handleSubmit, getValues, reset } = useForm();
 
-  // mutation check password
-  // -> success: onConfirm callback 실행
-  // -> fail : onFail callback 실행
+  const checkPassword = () => {
+    const password = getValues('password').trim();
+    const email = data?.sub;
+    if (!email || !password) return;
+
+    mutateAsync({ email, password })
+      .then((isValidPassword) => {
+        if (isValidPassword) {
+          onConfirm();
+        } else {
+          if (onFail) onFail();
+          toast({
+            title: '오류 알림',
+            description: '비밀번호가 틀렸습니다. 다시 입력해주세요',
+            status: 'error',
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        toast({
+          title: '오류 알림',
+          description: error.response.data.message,
+          status: 'error',
+        });
+        if (onFail) onFail();
+      })
+      .finally(() => reset());
+  };
   return (
     <SettingSectionLayout title="암호 확인">
       <Text>회원님의 계정 설정을 변경하기 전 본인확인을 위해 암호를 입력해주세요.</Text>
-      <Input />
-      <ButtonGroup>
-        <Button onClick={onCancel}>취소</Button>
-        <Button onClick={onConfirm}>암호 확인</Button>
-      </ButtonGroup>
+      <Box as="form" onSubmit={handleSubmit(checkPassword)}>
+        <Input {...register('password', { required: true })} />
+        <ButtonGroup>
+          <Button onClick={onCancel}>취소</Button>
+          <Button type="submit">암호 확인</Button>
+        </ButtonGroup>
+      </Box>
     </SettingSectionLayout>
   );
 }
