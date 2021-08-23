0c53bd03c75f6b950ccefb9cc1a3d44015e1e059
hwasurr <iamsupermazinga@gmail.com>
2021-08-23 17:53:51 +0900

    Add: type없이 로그인 요청하는 경우 처리 추가

--- libs/nest-modules/src/lib/auth/strategies/local.strategy.ts
+++ libs/nest-modules/src/lib/auth/strategies/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Request } from 'express';
 import { UserPayload, UserType } from '../auth.interface';
 import { AuthService } from '../auth.service';
@@ -16,6 +16,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(req: Request, email: string, password: string): Promise<UserPayload> {
+    if (!['seller', 'creator'].includes(req.query.type as string)) {
+      throw new ForbiddenException();
+    }
     const userPayload = await this.authService.validateUser(
       req.query.type as UserType,
       email,
