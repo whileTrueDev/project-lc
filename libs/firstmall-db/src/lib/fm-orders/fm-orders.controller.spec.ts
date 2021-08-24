f3405715658e5c7b337a51e9ee6cedaa38eb8556
hwasurr <iamsupermazinga@gmail.com>
2021-08-24 13:25:03 +0900

    Add: fm-orders 컨트롤러 테스트 추가

---
+++ libs/firstmall-db/src/lib/fm-orders/fm-orders.controller.spec.ts
@@ -0,0 +1,60 @@
+import { ExecutionContext } from '@nestjs/common';
+import { NestApplication } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Seller } from '@prisma/client';
+import request from 'supertest';
+import { JwtAuthGuard } from '../../../../nest-modules/src';
+import { FindFmOrdersDto } from '../../../../shared-types/src';
+import { ordersSample } from '../../__tests__/ordersSample';
+import { FirstmallDbService } from '../firstmall-db.service';
+import { FmOrdersController } from './fm-orders.controller';
+import { FmOrdersService } from './fm-orders.service';
+
+describe('FmOrdersController', () => {
+  let app: NestApplication;
+  let controller: FmOrdersController;
+  let service: FmOrdersService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
+      controllers: [FmOrdersController],
+      providers: [FmOrdersService, FirstmallDbService],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          const req = context.switchToHttp().getRequest();
+          req.user = { sub: 'test@test.com', type: 'seller' };
+          return true;
+        },
+      })
+      .compile();
+
+    controller = module.get<FmOrdersController>(FmOrdersController);
+    service = module.get<FmOrdersService>(FmOrdersService);
+
+    jest.spyOn(service, 'findOrders').mockImplementation(async () => ordersSample);
+
+    app = module.createNestApplication();
+    app.init();
+  });
+
+  afterAll(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('GET /fm-orders', () => {
+    it('should be return 200', async () => {
+      return request(app.getHttpServer())
+        .get('/fm-orders?search=test')
+        .expect(200, ordersSample);
+    });
+  });
+});
