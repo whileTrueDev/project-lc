3e2072a30a206213ca8404c2bc2193202754c018
hwasurr <iamsupermazinga@gmail.com>
2021-08-24 17:12:24 +0900

    Fix: "결재" 오타 수정 => "결제"

--- libs/shared-types/src/lib/constants/fmOrderStatuses.ts
+++ libs/shared-types/src/lib/constants/fmOrderStatuses.ts
@@ -10,7 +10,7 @@ export const fmOrderStatuses = {
   '65': { name: '배송중', chakraColor: 'purple' },
   '70': { name: '부분배송완료', chakraColor: 'messenger' },
   '75': { name: '배송완료', chakraColor: 'messenger' },
-  '85': { name: '결재취소', chakraColor: 'gray' },
+  '85': { name: '결제취소', chakraColor: 'gray' },
   '95': { name: '주문무효', chakraColor: 'gray' },
   '99': { name: '결제실패', chakraColor: 'gray' },
 };
