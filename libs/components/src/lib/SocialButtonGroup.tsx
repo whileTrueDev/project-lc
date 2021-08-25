db45b3892770fcef598d03d1663733b26ad89560
Jiyeon <a1919361@gmail.com>
2021-08-25 17:03:50 +0900

    [style] 소셜로그인 버튼그룹 스타일 수정

--- libs/components/src/lib/SocialButtonGroup.tsx
+++ libs/components/src/lib/SocialButtonGroup.tsx
@@ -1,13 +1,18 @@
-import { Divider, Text, VStack } from '@chakra-ui/react';
+import { Divider, Stack, Text, VStack } from '@chakra-ui/react';
 import GoogleLoginButton from './GoogleLoginButton';
 import KakaoLoginButton from './KakaoLoginButton';
 import NaverLoginButton from './NaverLoginButton';
 
 export function SocialButtonGroup(): JSX.Element {
   return (
-    <VStack spacing={2}>
-      <Text>소셜 계정으로 시작하기</Text>
-      <Divider />
+    <VStack spacing={2} py={4}>
+      <Stack direction="row" width="100%" alignItems="center">
+        <Divider flex={1} />
+        <Text align="center" flex={1.5}>
+          소셜 계정으로 시작하기
+        </Text>
+        <Divider flex={1} />
+      </Stack>
       <GoogleLoginButton />
       <NaverLoginButton />
       <KakaoLoginButton />
