c7f623072cb7af2dd05edec33ddbea1d81391f4c
Jiyeon <a1919361@gmail.com>
2021-08-26 11:20:04 +0900

    CenterBox props children type 변경

--- libs/components/src/lib/CenterBox.tsx
+++ libs/components/src/lib/CenterBox.tsx
@@ -1,4 +1,5 @@
 import { Stack, Box, useColorModeValue, Heading, Text } from '@chakra-ui/react';
+import React from 'react';
 
 export interface CenterBoxProps {
   enableShadow?: boolean;
@@ -6,7 +7,7 @@ export interface CenterBoxProps {
     title: string;
     desc: string;
   };
-  children: JSX.Element;
+  children: React.ReactNode;
 }
 
 export function CenterBox({
