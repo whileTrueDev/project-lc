5e0d23a3ca7f3d600ad85bebd85ad6d9061041a4
Jiyeon <a1919361@gmail.com>
2021-08-31 17:58:17 +0900

    [add] 상품목록 요청 쿼리훅 작성

---
+++ libs/hooks/src/lib/queries/useSellerGoodsList.tsx
@@ -0,0 +1,28 @@
+import { GoodsListDto, GoodsListRes } from '@project-lc/shared-types';
+import { useQuery, UseQueryOptions } from 'react-query';
+import axios from '../../axios';
+
+type SellerGoodsListRequestDto = GoodsListDto & { email: string };
+
+export const getSellerGoodsList = async (dto: GoodsListDto): Promise<GoodsListRes> => {
+  return axios
+    .get<GoodsListRes>('/goods/list', {
+      params: {
+        ...dto,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const useSellerGoodsList = (
+  dto: SellerGoodsListRequestDto,
+  options?: UseQueryOptions<GoodsListRes>,
+) => {
+  const { page, itemPerPage, sort, direction } = dto;
+  const queryKey = ['SellerGoodsList', dto];
+  return useQuery<GoodsListRes>(
+    queryKey,
+    () => getSellerGoodsList({ page, itemPerPage, sort, direction }),
+    { retry: 1, ...options },
+  );
+};
