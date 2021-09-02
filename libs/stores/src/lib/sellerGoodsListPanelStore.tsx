d35d898c594729ed9fcaa3a5711f3c15f97d8856
Jiyeon <a1919361@gmail.com>
2021-09-02 09:52:02 +0900

    [refactor] 페이지, 정렬 핸들러 store로 옮김

--- libs/stores/src/lib/sellerGoodsListPanelStore.tsx
+++ libs/stores/src/lib/sellerGoodsListPanelStore.tsx
@@ -9,7 +9,10 @@ export interface SellerGoodsListPanelStoreState {
   changePage(page: number): void;
   changeItemPerPage(itemPerPage: number): void;
   changeSort(sort: SortColumn, direction: SortDirection): void;
+  handlePageSizeChange(event: React.ChangeEvent<HTMLSelectElement>): void;
+  handleSortChange(event: React.ChangeEvent<HTMLSelectElement>): void;
 }
+
 export const useSellerGoodsListPanelStore = create<SellerGoodsListPanelStoreState>(
   (set, get) => ({
     page: 0,
@@ -21,5 +24,30 @@ export const useSellerGoodsListPanelStore = create<SellerGoodsListPanelStoreStat
       set((state) => ({ ...state, itemPerPage })),
     changeSort: (sort: SortColumn, direction: SortDirection) =>
       set((state) => ({ ...state, sort, direction })),
+    handlePageSizeChange: (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const { value } = event.target;
+      set((state) => ({ ...state, page: 0, itemPerPage: Number(value) }));
+    },
+    handleSortChange: (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const { value } = event.target;
+      console.log(value);
+      switch (value) {
+        case 'regist_date':
+          set((state) => ({
+            ...state,
+            sort: SortColumn.REGIST_DATE,
+            direction: SortDirection.DESC,
+          }));
+          break;
+        case 'goods_name':
+          set((state) => ({
+            ...state,
+            sort: SortColumn.GOODS_NAME,
+            direction: SortDirection.DESC,
+          }));
+          break;
+        default:
+      }
+    },
   }),
 );
