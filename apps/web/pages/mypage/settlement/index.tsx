1829e8d481f5878a9083097904ccb24e60e8cf28
chanuuuuu <dn0208@gmail.com>
2021-09-03 18:51:00 +0900

    [Refactor] 정산 가능 금액 기능 제거를 위한 디자인 변경

--- apps/web/pages/mypage/settlement/index.tsx
+++ apps/web/pages/mypage/settlement/index.tsx
@@ -6,10 +6,12 @@ import {
   SettlementListBox,
 } from '@project-lc/components';
 import { useSettlementInfo } from '@project-lc/hooks';
+
 import { Heading, VStack, Divider, Container, Grid, GridItem } from '@chakra-ui/react';
 import { useMemo } from 'react';
+
 // 정산 페이지 컴포넌트
-export function Index(props): JSX.Element {
+export function Index(): JSX.Element {
   const { data: settlementData, refetch } = useSettlementInfo();
 
   const hasAccount = useMemo(() => {
@@ -17,40 +19,37 @@ export function Index(props): JSX.Element {
   }, [settlementData?.sellerSettlementAccount]);
 
   const hasRegistration = useMemo(() => {
-    return settlementData?.businessRegistration.length > 0;
-  }, [settlementData?.businessRegistration]);
+    return settlementData?.sellerBusinessRegistration.length > 0;
+  }, [settlementData?.sellerBusinessRegistration]);
 
   return (
     <MypageLayout>
       <Container maxW="7xl" p={[1, 6, 6, 6]}>
+        <Heading mb={4}>정산</Heading>
         <VStack spacing={1} alignItems="stretch">
-          <Heading mb={4}>정산</Heading>
-          <Grid p={3} gap={3} templateColumns="repeat(5, 1fr)">
-            <GridItem colSpan={[5, 2, 2, 2]} rowSpan={2}>
+          <Grid p={3} gap={3} templateColumns="repeat(6, 1fr)">
+            <GridItem colSpan={[6, 3, 3, 3]} rowSpan={1} alignItems="stretch">
               {/* 정산 상태 BOX */}
               <SettlementStateBox
                 hasAccount={hasAccount}
                 hasRegistration={hasRegistration}
               />
             </GridItem>
-            <GridItem colSpan={[5, 3, 3, 3]} rowSpan={2} alignItems="stretch">
-              {/* 정산 목록 BOX */}
-              <SettlementListBox sellerSettlements={settlementData?.sellerSettlements} />
-            </GridItem>
-          </Grid>
-
-          <Grid p={3} pt={0} gap={3} templateColumns="repeat(6, 1fr)">
-            <GridItem colSpan={[6, 3, 3, 3]} rowSpan={2}>
-              {/* 사업자 등록증 BOX */}
-              <BusinessRegistrationBox
-                businessRegistration={settlementData?.businessRegistration[0]}
-                refetch={refetch}
-              />
-            </GridItem>
             <GridItem colSpan={[6, 3, 3, 3]} rowSpan={2} alignItems="stretch">
               {/* 계좌번호 BOX */}
               <SettlementAccountBox
                 settlementAccount={settlementData?.sellerSettlementAccount[0]}
+                refetch={refetch}
+              />
+            </GridItem>
+            <GridItem colSpan={[6, 3, 3, 3]} rowSpan={3} alignItems="stretch">
+              {/* 정산 목록 BOX */}
+              <SettlementListBox sellerSettlements={settlementData?.sellerSettlements} />
+            </GridItem>
+            <GridItem colSpan={[6, 3, 3, 3]} rowSpan={2} alignItems="stretch">
+              {/* 사업자 등록증 BOX */}
+              <BusinessRegistrationBox
+                sellerBusinessRegistration={settlementData?.sellerBusinessRegistration[0]}
                 refetch={refetch}
               />
             </GridItem>
