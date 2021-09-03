71c43e47514df829008a276e4bc0e854c7b8e281
hwasurr <iamsupermazinga@gmail.com>
2021-09-03 14:23:32 +0900

    Add: 상품 목록과 통일성을 위한 주문목록 컬럼 메뉴 삭제처리

--- libs/components/src/lib/OrderList.tsx
+++ libs/components/src/lib/OrderList.tsx
@@ -175,6 +175,8 @@ export function OrderList(): JSX.Element {
       <ChakraDataGrid
         bg={dataGridBgColor}
         autoHeight
+        disableSelectionOnClick
+        disableColumnMenu
         loading={orders.isLoading}
         columns={columns.map((x) => ({ ...x, flex: isDesktopSize ? 1 : undefined }))}
         rows={orders.data || []}
