2517d0faec2e564e974589a16b1a08140dd12ec4
Jiyeon <a1919361@gmail.com>
2021-09-08 16:57:31 +0900

    [fix] 옵션 관련 타입 수정

--- libs/shared-types/src/lib/front-type/shippingOptionFormType.ts
+++ libs/shared-types/src/lib/front-type/shippingOptionFormType.ts
@@ -1,4 +1,12 @@
-export type ShippingOptionType = 'free' | 'fixed' | 'amount' | 'cnt' | 'weight';
+export type ShippingOptionType =
+  | 'free'
+  | 'fixed'
+  | 'amount'
+  | 'amount_rep'
+  | 'cnt'
+  | 'cnt_rep'
+  | 'weight'
+  | 'weight_rep';
 export type ShippingOptionSetType = 'std' | 'add';
 
 export interface ShippingOption {
@@ -8,10 +16,22 @@ export interface ShippingOption {
   shippingSetType: ShippingOptionSetType;
   // 배송방법 타입 - free - 무료 / fixed - 고정 / amount - 금액 / cnt - 수량 / weight - 무게
   shippingOptType: ShippingOptionType;
-  // 배송지역 제한여부
-  deliveryLimit: boolean;
   // 시작구간
   sectionStart: null | number;
   // 끝구간
   sectionEnd: null | number;
+
+  // 배송비 - 지역추가시 배열로 들어감
+  costItem: ShippingCost | ShippingCost[];
+}
+
+export interface ShippingCost {
+  // 임시 id
+  tempId: number;
+  // 지역그룹 명
+  areaName: string;
+  // 해당 지역그룹의 배송비
+  cost: number;
+  // 지역그룹에 속하는 주소(시도명) - shipping_option.delivery_limit 값이 limit 인 경우
+  areaDetail?: string[];
 }
--- libs/shared-types/src/lib/front-type/shippingSetFormType.ts
+++ libs/shared-types/src/lib/front-type/shippingSetFormType.ts
@@ -2,7 +2,7 @@ import { ShippingSetCodeOptions } from '../constants/shippingTypes';
 import { ShippingOption } from './shippingOptionFormType';
 
 // 배송설정 - 선불/착불정보
-export type PrepayInfo = 'all' | 'delivery' | '';
+export type PrepayInfo = 'all' | 'delivery' | 'postpaid';
 //  배송지역 제한 - unlimit - 전국(전세계) / limit - 지정지역(국가)
 export type DeliveryLimit = 'limit' | 'unlimit';
 // 배송 설정 생성 폼 데이터 타입
@@ -16,9 +16,9 @@ export interface ShippingSetFormData {
   // 착불/선불 정보
   prepayInfo: PrepayInfo;
   // 반품 배송비 - 편도
-  refundShippingCost: number;
+  refundShippingCost: number | null;
   // 반품 배송비 - (맞)교환
-  swapShippingCost: number;
+  swapShippingCost: number | null;
   // 무료배송시 반품왕복배송비 받을지 여부(fm_shipping_set.shiping 옵션에 대응하는 값. 해당 컬럼 이름 따라간거)
   shipingFreeFlag: boolean;
   // 배송비 옵션
