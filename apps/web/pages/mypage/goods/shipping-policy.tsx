18c0bc5abcf53778c66f246789e3f311117def55
Jiyeon <a1919361@gmail.com>
2021-09-10 19:27:32 +0900

    [add] 리액트 쿼리 훅 적용함, DTO 수정 및 생성시 값 할당하는 부분 코드 수정 반드시 필요

--- apps/web/pages/mypage/goods/shipping-policy.tsx
+++ apps/web/pages/mypage/goods/shipping-policy.tsx
@@ -8,23 +8,54 @@ import {
   ModalOverlay,
   useDisclosure,
   Text,
+  Stack,
 } from '@chakra-ui/react';
 import React from 'react-transition-group/node_modules/@types/react';
+import { useDeleteShippingGroup, useProfile } from '@project-lc/hooks';
+import { useShippingGroupItemStore } from '@project-lc/stores';
+import { useShippingGroupList } from '../../../../../libs/hooks/src/lib/queries/useShippingGroupList';
 
 export function TempShippingPolicy(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { data: ProfileData } = useProfile();
+  const { data, isLoading } = useShippingGroupList(ProfileData.email, !!ProfileData);
+  const { mutateAsync } = useDeleteShippingGroup();
+  const { reset } = useShippingGroupItemStore();
+
+  const closeHandler = () => {
+    reset();
+    onClose();
+  };
 
   return (
     <MypageLayout>
-      <Text>배송비정책 생성 모달창 작업을 위한 임시 페이지</Text>
-      <Button onClick={onOpen}>배송비정책 생성</Button>
+      <Stack spacing={8}>
+        <Text>배송비정책 생성 모달창 작업을 위한 임시 페이지</Text>
 
-      <Modal isOpen={isOpen} onClose={onClose} size="full">
+        <Button onClick={onOpen} w="200px">
+          배송비정책 생성 모달 열기
+        </Button>
+
+        <Text>임시 배송비목록 (삭제테스트 용)</Text>
+        {!isLoading && !data && <Text>등록된 배송비 정책 없음</Text>}
+        {data &&
+          data.map((group) => (
+            <Stack direction="row" key={group.id} alignItems="center">
+              <Button size="sm" onClick={() => mutateAsync({ groupId: group.id })}>
+                x
+              </Button>
+              <Text>배송그룹 명 : {group.shipping_group_name} , </Text>
+              <Text>배송비 계산 기준 : {group.shipping_calcul_type}</Text>
+            </Stack>
+          ))}
+      </Stack>
+
+      <Modal isOpen={isOpen} onClose={closeHandler} size="full">
         <ModalOverlay />
         <ModalContent>
           <ModalCloseButton />
           <ModalBody>
-            <ShippingPolicyForm />
+            <ShippingPolicyForm onSuccess={closeHandler} />
           </ModalBody>
         </ModalContent>
       </Modal>
--- libs/components/src/lib/ShippingPolicyForm.tsx
+++ libs/components/src/lib/ShippingPolicyForm.tsx
@@ -1,9 +1,16 @@
 import { Button, Heading, Stack, useToast } from '@chakra-ui/react';
+import { DeliveryNation, YesOrNo_UPPERCASE } from '@prisma/client';
+import { useCreateShippingGroup } from '@project-lc/hooks';
+import { ShippingGroup } from '@project-lc/shared-types';
 import { useShippingGroupItemStore } from '@project-lc/stores';
 import ShippingPolicyBasicInfo from './ShippingPolicyBasicInfo';
 import ShippingPolicySetList from './ShippingPolicySetList';
 
-export function ShippingPolicyForm(): JSX.Element {
+export function ShippingPolicyForm({
+  onSuccess,
+}: {
+  onSuccess?: () => void;
+}): JSX.Element {
   const toast = useToast();
 
   const {
@@ -16,6 +23,8 @@ export function ShippingPolicyForm(): JSX.Element {
     detailAddress,
     shippingSets,
   } = useShippingGroupItemStore();
+
+  const { mutateAsync, isLoading } = useCreateShippingGroup();
 
   const saveShippingPolicy = () => {
     // TODO : useShippingGroupItemStore 에서 값 가져와 배송그룹명, 반송지, 배송방법 등록되어 있는지 확인 => 훅으로 분리하기
@@ -31,18 +40,67 @@ export function ShippingPolicyForm(): JSX.Element {
       toast({ title: '배송방법을 추가해주세요', status: 'error' });
       return;
     }
-    const newPolicy = {
-      groupName,
-      shippingCalculType,
-      shippingStdFree,
-      shippingAddFree,
+
+    // TODO: 타입 통일... 혹은 훅으로 분리
+    const newGroup: ShippingGroup = {
+      shipping_group_name: groupName,
+      shipping_calcul_type: shippingCalculType,
+      shipping_std_free_yn: shippingStdFree ? YesOrNo_UPPERCASE.Y : YesOrNo_UPPERCASE.N,
+      shipping_add_free_yn: shippingAddFree ? YesOrNo_UPPERCASE.Y : YesOrNo_UPPERCASE.N,
       postalCode,
       baseAddress,
       detailAddress,
-      shippingSets,
+      shippingSets: shippingSets.map((set) => {
+        const {
+          shippingSetCode,
+          shippingSetName,
+          prepayInfo,
+          deliveryLimit,
+          refundShippingCost,
+          swapShippingCost,
+          shipingFreeFlag,
+          shippingOptions,
+        } = set;
+        return {
+          shipping_set_code: shippingSetCode,
+          shipping_set_name: shippingSetName,
+          prepay_info: prepayInfo,
+          delivery_nation: DeliveryNation.korea, // TODO: global 선택 가능하도록 수정
+          delivery_limit: deliveryLimit,
+          refund_shiping_cost: refundShippingCost || 0,
+          swap_shiping_cost: swapShippingCost || 0,
+          shiping_free_yn: shipingFreeFlag ? YesOrNo_UPPERCASE.Y : YesOrNo_UPPERCASE.N,
+          shippingOptions: shippingOptions.map((opt) => {
+            const {
+              shippingSetType,
+              shippingOptType,
+              sectionStart,
+              sectionEnd,
+              costItem,
+            } = opt;
+            return {
+              shipping_set_type: shippingSetType,
+              shipping_opt_type: shippingOptType,
+              // TODO: default_yn 선택 가능하도록 수정
+              section_st: sectionStart,
+              section_ed: sectionEnd,
+              shippingCost: {
+                shipping_cost: costItem.cost,
+                shipping_area_name: costItem.areaName,
+              },
+            };
+          }),
+        };
+      }),
     };
     // 생성 요청
-    console.log('shipping policy', newPolicy);
+    console.log('newGroup', newGroup);
+
+    mutateAsync(newGroup)
+      .then(() => {
+        if (onSuccess) onSuccess();
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
@@ -62,7 +120,9 @@ export function ShippingPolicyForm(): JSX.Element {
       // TODO: 백엔드 작업 후 배송비 수정시 만들 예정
       */}
 
-      <Button onClick={saveShippingPolicy}>저장</Button>
+      <Button onClick={saveShippingPolicy} isLoading={isLoading}>
+        저장
+      </Button>
     </Stack>
   );
 }
