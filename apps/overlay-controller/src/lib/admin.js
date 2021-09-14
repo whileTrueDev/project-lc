908790791bfb5eaba9336dfbed6044516da70389
leejineun <leejineun94@gmail.com>
2021-09-15 00:45:45 +0900

    달력 삽입

--- apps/overlay-controller/src/lib/admin.js
+++ apps/overlay-controller/src/lib/admin.js
@@ -1,5 +1,9 @@
 /* eslint-env jquery */
 
 $(document).ready(function () {
-  $('#table_id').DataTable();
+  $('#table_id').DataTable({ lengthChange: false });
 });
+
+// 크리에이터 선택시 바로 소켓 아이디 받아오도록 변경
+
+// 소켓 아이디 받아오면 받아왔다고 표시
