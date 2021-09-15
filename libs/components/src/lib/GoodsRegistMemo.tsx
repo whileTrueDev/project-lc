2a8c83c190a1e38c3197431ca7b60cfe96889615
Jiyeon <a1919361@gmail.com>
2021-09-15 18:36:25 +0900

    [add] 상품등록 메모 textarea 추가

--- libs/components/src/lib/GoodsRegistMemo.tsx
+++ libs/components/src/lib/GoodsRegistMemo.tsx
@@ -1,10 +1,14 @@
-import { Text } from '@chakra-ui/react';
+/* eslint-disable react/jsx-props-no-spreading */
+import { Textarea } from '@chakra-ui/react';
+import { RegistGoodsDto } from '@project-lc/shared-types';
+import { useFormContext } from 'react-hook-form';
 import SectionWithTitle from './SectionWithTitle';
 
 export function GoodsRegistMemo(): JSX.Element {
+  const { register } = useFormContext<RegistGoodsDto>();
   return (
     <SectionWithTitle title="메모">
-      <Text>메모</Text>
+      <Textarea {...register('admin_memo')} resize="none" />
     </SectionWithTitle>
   );
 }
