7fd0d36545c37975db84dfb331579cd5053bc4cb
Jiyeon <a1919361@gmail.com>
2021-09-15 16:30:58 +0900

    [add] 기본정보 form과 연결

--- libs/components/src/lib/GoodsRegistDataBasic.tsx
+++ libs/components/src/lib/GoodsRegistDataBasic.tsx
@@ -1,18 +1,31 @@
-import { HStack, Input, Text } from '@chakra-ui/react';
+/* eslint-disable react/jsx-props-no-spreading */
+import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { RegistGoodsDto } from '@project-lc/shared-types';
+import { useFormContext } from 'react-hook-form';
 import SectionWithTitle from './SectionWithTitle';
 
 export function GoodsRegistDataBasic(): JSX.Element {
+  const { register } = useFormContext<RegistGoodsDto>();
   return (
     <SectionWithTitle title="기본정보">
-      <HStack>
-        <Text>상품명 fm_goods.goods_name</Text>
-        <Input />
-      </HStack>
+      <FormControl id="goods_name" isRequired>
+        <FormLabel>상품명</FormLabel>
+        <Input
+          placeholder="상품명을 입력하세요"
+          {...register('goods_name', { required: true, maxLength: 180 })}
+        />
+      </FormControl>
 
-      <HStack>
-        <Text>간략설명 fm_goods.summary</Text>
-        <Input />
-      </HStack>
+      <FormControl id="summary">
+        <FormLabel>간략설명</FormLabel>
+        <Input
+          placeholder="간략한 상품 설명을 입력하세요"
+          {...(register('summary'),
+          {
+            maxLength: 180,
+          })}
+        />
+      </FormControl>
     </SectionWithTitle>
   );
 }
