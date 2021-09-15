a3fa6a06325ec68697843c57c5a72d507eee3a11
Jiyeon <a1919361@gmail.com>
2021-09-15 18:07:45 +0900

    [add] 판매정보 라디오그룹 컴포넌트 작성

--- libs/components/src/lib/GoodsRegistDataSales.tsx
+++ libs/components/src/lib/GoodsRegistDataSales.tsx
@@ -1,33 +1,68 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { QuestionIcon } from '@chakra-ui/icons';
 import { Heading, HStack, Stack, Text } from '@chakra-ui/layout';
 import { Radio, RadioGroup } from '@chakra-ui/radio';
+import { GoodsStatus } from '@prisma/client';
+import { RegistGoodsDto } from '@project-lc/shared-types';
+import { ChangeHandler, useFormContext } from 'react-hook-form';
 import SectionWithTitle from './SectionWithTitle';
 import TextWithPopperButton from './TextWithPopperButton';
+
+// 상품등록 - 판매정보
+const GOODS_STATUS: { value: GoodsStatus; label: string }[] = [
+  { value: 'normal', label: '정상' },
+  { value: 'purchasing', label: '재고확보중' },
+  { value: 'unsold', label: '판매중지' },
+];
+// 상품등록 - 청약철회
+const GOODS_CANCEL_TYPE: { value: '0' | '1'; label: string }[] = [
+  { value: '0', label: '가능' },
+  { value: '1', label: '불가 (취소/교환/반품 불가)' },
+];
+
+// 상품등록 폼에서 사용하는 라디오그룹
+export function GoodsRegistRadio({
+  name,
+  values,
+}: {
+  name: keyof Omit<RegistGoodsDto, 'options'>;
+  values: {
+    value: RegistGoodsDto[typeof name];
+    label: string;
+  }[];
+}) {
+  const { register, watch } = useFormContext<RegistGoodsDto>();
+  return (
+    <RadioGroup value={watch(name, values[0].value)}>
+      {values.map((item) => {
+        const { value, label } = item;
+        return (
+          <Radio key={label} {...register(name)} value={value}>
+            {label}
+          </Radio>
+        );
+      })}
+    </RadioGroup>
+  );
+}
 
 export function GoodsRegistDataSales(): JSX.Element {
   return (
     <SectionWithTitle title="판매정보">
       <HStack>
-        <Text>판매상태 fm_goods.goods_status</Text>
-        <RadioGroup>
-          <Radio>정상</Radio>
-          <Radio>재고확보중</Radio>
-          <Radio>판매중지</Radio>
-        </RadioGroup>
+        <Text>판매상태</Text>
+        <GoodsRegistRadio name="goods_status" values={GOODS_STATUS} />
       </HStack>
 
       <HStack>
         <TextWithPopperButton
-          title="청약철회 fm_goods.cancel_type"
+          title="청약철회"
           iconAriaLabel="청약철회 설명"
           icon={<QuestionIcon />}
         >
           <Text>청약철회 불가 선택 시 결제 확인 이후, 취소, 반품, 교환이 불가합니다</Text>
         </TextWithPopperButton>
-        <RadioGroup>
-          <Radio>가능</Radio>
-          <Radio>불가 (취소/교환/반품 불가)</Radio>
-        </RadioGroup>
+        <GoodsRegistRadio name="cancel_type" values={GOODS_CANCEL_TYPE} />
       </HStack>
     </SectionWithTitle>
   );
