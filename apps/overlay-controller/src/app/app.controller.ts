ec3ca67bf901ff14b7d9deb2c6dda46b73a45c58
leejineun <43170606+leejineun@users.noreply.github.com>
2021-09-23 17:59:13 +0900

    OverlayControllerService -> app.controller 에서 바로 불러오도록 변경

--- apps/overlay-controller/src/app/app.controller.ts
+++ apps/overlay-controller/src/app/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Render, Post, Body } from '@nestjs/common';
 import { PurchaseMessageWithLoginFlag } from '@project-lc/shared-types';
 import { ConfigService } from '@nestjs/config';
-import { AppService } from './app.service';
+import { OverlayControllerService } from '@project-lc/nest-modules';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
+    private readonly overlayControllerService: OverlayControllerService,
     private readonly configService: ConfigService,
   ) {}
 
@@ -15,13 +15,13 @@ export class AppController {
   async renterTest() {
     const HOST = this.configService.get('OVERLAY_HOST_NAME');
 
-    const userIdAndUrlAndNicknames = await this.appService.getCreatorUrlAndNickname();
+    const userIdAndUrlAndNicknames = await this.overlayControllerService.getCreatorUrls();
     return { userIdAndUrlAndNicknames, HOST };
   }
 
   @Post('/purchase-message')
   async uploadMessage(@Body() data: PurchaseMessageWithLoginFlag) {
-    const upload = await this.appService.uploadPurchase(data);
+    const upload = await this.overlayControllerService.uploadPurchase(data);
     return upload;
   }
 }
--- apps/overlay-controller/src/app/app.module.ts
+++ apps/overlay-controller/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { PrismaModule } from '@project-lc/prisma-orm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OverlayControllerModule } from '@project-lc/nest-modules';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { validationSchema } from '../settings/config.validation';
 
 @Module({
@@ -13,6 +12,6 @@ import { validationSchema } from '../settings/config.validation';
     ConfigModule.forRoot({ isGlobal: true, validationSchema }),
   ],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [ConfigService],
 })
 export class AppModule {}
