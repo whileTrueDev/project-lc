4a20496d71d511158e0fa8afd88ac8de2db9796e
hwasurr <iamsupermazinga@gmail.com>
2021-10-07 14:40:26 +0900

    Fix: [버그수정] 마이페이지 네비바 하이라이팅 버그 수정

--- libs/components/src/constants/navigation.ts
+++ libs/components/src/constants/navigation.ts
@@ -25,36 +25,54 @@ export const mainNavItems: Array<NavItem> = [
 export interface MypageLink {
   name: string;
   href: string;
+  checkIsActive: (pathname: string, linkHref: string) => boolean;
   children?: MypageLink[];
 }
 
+const defaultIsActiveChecker = (pathname: string, linkHref: string): boolean =>
+  pathname.includes(linkHref);
+
 export const mypageNavLinks: MypageLink[] = [
-  { name: '홈', href: '/mypage' },
+  {
+    name: '홈',
+    href: '/mypage',
+    checkIsActive: (pathname, linkHref) => pathname === linkHref,
+  },
   {
     name: '상품',
     href: '/mypage/goods',
+    checkIsActive: defaultIsActiveChecker,
   },
   {
     name: '라이브쇼핑',
     href: '/mypage/live',
+    checkIsActive: defaultIsActiveChecker,
     children: [
       {
         name: '라이브 예약',
         href: '/mypage/live',
+        checkIsActive: defaultIsActiveChecker,
       },
       {
         name: 'VOD 관리',
         href: '/mypage/live/vod',
+        checkIsActive: defaultIsActiveChecker,
       },
     ],
   },
   {
     name: '주문',
     href: '/mypage/orders',
+    checkIsActive: defaultIsActiveChecker,
   },
   {
     name: '상점설정',
     href: '/mypage/shopinfo',
+    checkIsActive: defaultIsActiveChecker,
   },
-  { name: '정산', href: '/mypage/settlement' },
+  {
+    name: '정산',
+    href: '/mypage/settlement',
+    checkIsActive: defaultIsActiveChecker,
+  },
 ];
