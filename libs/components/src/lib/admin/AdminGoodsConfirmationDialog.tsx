8a834c64d7b193822fba64a834498d9cd7b32bd4
Jiyeon <a1919361@gmail.com>
2021-10-08 10:05:06 +0900

    [style] 상품고유번호 텍스트 색 수정, 고유번호 없을때 버튼 disable 처리

--- libs/components/src/lib/admin/AdminGoodsConfirmationDialog.tsx
+++ libs/components/src/lib/admin/AdminGoodsConfirmationDialog.tsx
@@ -16,6 +16,7 @@ import {
   ModalOverlay,
   useToast,
   Grid,
+  Text,
 } from '@chakra-ui/react';
 import { GoodsConfirmationStatus } from '@project-lc/shared-types';
 import { useGoodConfirmationMutation } from '@project-lc/hooks';
@@ -45,6 +46,7 @@ export function AdminGoodsConfirmationDialog(
     handleSubmit,
     reset,
     formState: { errors, isSubmitting },
+    watch,
   } = useForm();
   const initialRef = useRef(null);
   const toast = useToast();
@@ -102,7 +104,15 @@ export function AdminGoodsConfirmationDialog(
             <FormLabel fontSize="md">상품 ID</FormLabel>
             <FormHelperText>
               퍼스트몰에 등록한 상품의 고유번호를
-              입력하세요.(http://whiletrue.firstmall.kr/goods/view?no=41 의 41을 입력)
+              입력하세요.(http://whiletrue.firstmall.kr/goods/view?no=
+              <Text as="span" color="red">
+                41
+              </Text>
+              의&nbsp;
+              <Text as="span" color="red">
+                41
+              </Text>
+              을 입력)
             </FormHelperText>
             <Input
               id="firstmallGoodsConnectionId"
@@ -125,7 +135,11 @@ export function AdminGoodsConfirmationDialog(
           </FormControl>
         </ModalBody>
         <ModalFooter>
-          <Button type="submit" isLoading={isSubmitting}>
+          <Button
+            type="submit"
+            isLoading={isSubmitting}
+            isDisabled={!watch('firstmallGoodsConnectionId')}
+          >
             승인하기
           </Button>
         </ModalFooter>
