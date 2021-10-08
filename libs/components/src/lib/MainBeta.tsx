d9f848f0265fd4d546b82a74c415bab8e7e5029d
Jiyeon <a1919361@gmail.com>
2021-10-08 13:43:42 +0900

    [style] 비디오 AspectRatio 컴포넌트 적용

--- libs/components/src/lib/MainBeta.tsx
+++ libs/components/src/lib/MainBeta.tsx
@@ -1,10 +1,41 @@
-import { Box, Button, Flex, Heading, SimpleGrid, Stack } from '@chakra-ui/react';
+import { AspectRatio, Button, Flex, Heading, SimpleGrid, Stack } from '@chakra-ui/react';
 import { useDisplaySize } from '@project-lc/hooks';
 import { useRouter } from 'next/router';
 
+function MainBetaVideo(): React.ReactElement {
+  return (
+    <AspectRatio maxW="750px" ratio={4 / 3}>
+      <iframe
+        src="https://www.youtube.com/embed/4pIuCJTMXQU?controls=0&autoplay=1"
+        title="project-lc-main-video"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </AspectRatio>
+  );
+}
+
+function MainBetaStartButton(): React.ReactElement {
+  const router = useRouter();
+  return (
+    <Button
+      onClick={() => router.push('/login')}
+      fontWeight={600}
+      color="white"
+      size="lg"
+      fontSize="md"
+      bg="pink.400"
+      _hover={{
+        bg: 'pink.300',
+      }}
+    >
+      시작하기
+    </Button>
+  );
+}
 export function MainBetaDesktop(): React.ReactElement {
   const { isMobileSize } = useDisplaySize();
-  const router = useRouter();
 
   return (
     <SimpleGrid minH="92vh" columns={2} alignItems="center" px={2}>
@@ -12,73 +43,26 @@ export function MainBetaDesktop(): React.ReactElement {
         <Heading fontSize={{ md: '2xl', lg: '4xl' }}>팬들과 함께 소통하는 쇼핑</Heading>
         <Heading fontSize={{ md: '2xl', lg: '4xl' }}>그래서, 더욱 믿을 수 있는</Heading>
         <Flex w="100%" pt={4} alignItems="center" justify="center">
-          <Button
-            onClick={() => router.push('/login')}
-            fontWeight={600}
-            color="white"
-            size="lg"
-            fontSize="md"
-            bg="pink.400"
-            _hover={{
-              bg: 'pink.300',
-            }}
-          >
-            시작하기
-          </Button>
+          <MainBetaStartButton />
         </Flex>
       </Stack>
-      {!isMobileSize && (
-        <Box w="100%" h={{ md: 400, xl: 630 }}>
-          <iframe
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/4pIuCJTMXQU?controls=0&autoplay=1"
-            title="project-lc-main-video"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </Box>
-      )}
+      {!isMobileSize && <MainBetaVideo />}
     </SimpleGrid>
   );
 }
 
 export function MainBetaMobile(): React.ReactElement {
-  const router = useRouter();
   return (
     <Stack>
       <Stack minH="92vh">
-        <Box>
-          <iframe
-            width="100%"
-            height="300"
-            src="https://www.youtube.com/embed/4pIuCJTMXQU?controls=0&autoplay=1"
-            title="project-lc-main-video"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </Box>
+        <MainBetaVideo />
+
         <Stack minH="50vh" justify="center" alignItems="center">
           <Stack alignItems="center">
             <Heading size="lg">팬들과 함께 소통하는 쇼핑</Heading>
             <Heading size="lg">그래서, 더욱 믿을 수 있는</Heading>
             <Flex w="100%" pt={4} alignItems="center" justify="center">
-              <Button
-                onClick={() => router.push('/login')}
-                fontWeight={600}
-                isFullWidth
-                color="white"
-                size="lg"
-                fontSize="md"
-                bg="pink.400"
-                _hover={{
-                  bg: 'pink.300',
-                }}
-              >
-                시작하기
-              </Button>
+              <MainBetaStartButton />
             </Flex>
           </Stack>
         </Stack>
