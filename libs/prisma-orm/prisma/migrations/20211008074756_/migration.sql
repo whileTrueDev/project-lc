2d51a57825028a2679ce579b72a84023bfccf97e
chanuuuuu <dn0208@gmail.com>
2021-10-13 11:28:19 +0900

    [Fix] prisma 변경사항 추가

--- libs/prisma-orm/prisma/migrations/20211008074756_/migration.sql
+++ libs/prisma-orm/prisma/migrations/20211008074756_/migration.sql
@@ -16,7 +16,7 @@ CREATE TABLE `BusinessRegistrationConfirmation` (
 ALTER TABLE `BusinessRegistrationConfirmation` ADD CONSTRAINT `BusinessRegistrationConfirmation_SellerBusinessRegistration_fkey` FOREIGN KEY (`SellerBusinessRegistrationId`) REFERENCES `SellerBusinessRegistration`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- RenameIndex
-ALTER TABLE `Goods` RENAME INDEX `Goods_goodsInfoId_unique` TO `Goods_goodsInfoId_key`;
+-- ALTER TABLE `Goods` RENAME INDEX `Goods_goodsInfoId_unique` TO `Goods_goodsInfoId_key`;
 
 -- RenameIndex
 ALTER TABLE `GoodsOptionsSupplies` RENAME INDEX `GoodsOptionsSupplies_goodsOptionsId_unique` TO `GoodsOptionsSupplies_goodsOptionsId_key`;
