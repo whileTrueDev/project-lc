3fafc7492eaef4f0020524c52138c8d76215f962
leejineun <43170606+leejineun@users.noreply.github.com>
2021-10-13 15:09:45 +0900

    migrate file 통합

--- libs/prisma-orm/prisma/migrations/20211011151307_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `LiveShopping` ADD COLUMN `progress` VARCHAR(191) NOT NULL DEFAULT 'registered';
--- libs/prisma-orm/prisma/migrations/20211013060859_/migration.sql
+++ libs/prisma-orm/prisma/migrations/20211013060859_/migration.sql
@@ -1,4 +1,16 @@
 -- CreateTable
+CREATE TABLE `SellerContacts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `sellerId` INTEGER NOT NULL,
+    `email` VARCHAR(191) NOT NULL DEFAULT '',
+    `phoneNumber` VARCHAR(191) NOT NULL DEFAULT '',
+    `isDefault` BOOLEAN NOT NULL DEFAULT false,
+    `createDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `LiveShopping` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `streamId` VARCHAR(191),
@@ -7,10 +19,15 @@ CREATE TABLE `LiveShopping` (
     `goodsId` INTEGER NOT NULL,
     `contactId` INTEGER NOT NULL,
     `requests` LONGTEXT,
+    `progress` VARCHAR(191) NOT NULL DEFAULT 'registered',
+    `createDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `LiveShopping_streamId_key`(`streamId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `SellerContacts` ADD CONSTRAINT `SellerContacts_sellerId_fkey` FOREIGN KEY (`sellerId`) REFERENCES `Seller`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `LiveShopping` ADD CONSTRAINT `LiveShopping_contactId_fkey` FOREIGN KEY (`contactId`) REFERENCES `SellerContacts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
@@ -23,3 +40,9 @@ ALTER TABLE `LiveShopping` ADD CONSTRAINT `LiveShopping_goodsId_fkey` FOREIGN KE
 
 -- AddForeignKey
 ALTER TABLE `LiveShopping` ADD CONSTRAINT `LiveShopping_broadcasterId_fkey` FOREIGN KEY (`broadcasterId`) REFERENCES `Broadcaster`(`userId`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- RenameIndex
+ALTER TABLE `Goods` RENAME INDEX `Goods_goodsInfoId_unique` TO `Goods_goodsInfoId_key`;
+
+-- RenameIndex
+ALTER TABLE `GoodsOptionsSupplies` RENAME INDEX `GoodsOptionsSupplies_goodsOptionsId_unique` TO `GoodsOptionsSupplies_goodsOptionsId_key`;
