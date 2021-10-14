17824b2fa8691a0f74342ded49c570ac02d4d691
Jiyeon <a1919361@gmail.com>
2021-10-14 17:06:44 +0900

    [fix] 청약철회 표시 수정

--- libs/components/src/lib/GoodsRegistDataSales.tsx
+++ libs/components/src/lib/GoodsRegistDataSales.tsx
@@ -15,7 +15,7 @@ const GOODS_STATUS: { value: GoodsStatus; label: string }[] = [
   { value: 'unsold', label: '판매중지' },
 ];
 // 상품등록 - 청약철회
-const GOODS_CANCEL_TYPE: { value: '0' | '1'; label: string }[] = [
+export const GOODS_CANCEL_TYPE: { value: '0' | '1'; label: string }[] = [
   { value: '0', label: '가능' },
   { value: '1', label: '불가 (취소/교환/반품 불가)' },
 ];
