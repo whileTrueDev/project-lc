7a44517977efee9e63e7d53224a9108dc0db7587
Jiyeon <a1919361@gmail.com>
2021-10-15 11:41:06 +0900

    [add] 이미지업로드 input 여러사진 선택 로직 추가

--- libs/components/src/lib/ImageInput.tsx
+++ libs/components/src/lib/ImageInput.tsx
@@ -3,48 +3,65 @@ import { useRef } from 'react';
 import { FiFile } from 'react-icons/fi';
 
 export const MB = 1024 * 1024; // 1Mbytes
-const IMAGE_SIZE_LIMIT = 5 * MB;
 
 export type ImageInputErrorTypes = 'over-size' | 'invalid-format' | undefined;
 
 type ImageInputProps = {
   handleSuccess: (fileName: string, file: File) => void;
   handleError: (errorType?: ImageInputErrorTypes) => void;
-  required?: boolean;
-  variant?: 'unstype' | 'chakra';
+  variant?: 'unstyle' | 'chakra';
   size?: ButtonProps['size'];
-};
+  imageSizeLimit?: number; // 업로드 파일 용량 제한
+} & React.HTMLProps<HTMLButtonElement>;
 
 export function ImageInput({
   handleSuccess,
   handleError,
   required = true,
-  variant = 'unstype',
+  variant = 'unstyle',
+  multiple = false,
+  imageSizeLimit = 10 * MB, // 이미지 파일 크기 제한 기본 10mb
   size,
 }: ImageInputProps): JSX.Element {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const fileRegx = /^image\/[a-z]*$/;
+  const handleImageFile = (myImage: File): void => {
+    const { name: imageName } = myImage;
+    // image 확장자 검사
+    if (fileRegx.test(myImage.type) === false) {
+      handleError('invalid-format');
+      return;
+    }
+    // 사이즈 제한보다 큰 경우
+    if (myImage.size > imageSizeLimit) {
+      handleError('over-size');
+      return;
+    }
+
+    // 확장자, 이미지 사이즈 검사 통과한 경우
+    handleSuccess(imageName, myImage);
+  };
+
   const readImage = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const target = event.target as HTMLInputElement;
     const files = target.files as FileList;
-    if (files.length !== 0) {
-      const fileRegx = /^image\/[a-z]*$/;
-      const myImage = files[0];
-      const imageName = myImage.name;
-      // image 확장자 검사
-      if (fileRegx.test(myImage.type)) {
-        // 이미지 사이즈 검사
-        if (myImage.size < IMAGE_SIZE_LIMIT) {
-          handleSuccess(imageName, myImage);
-        } else {
-          // 사이즈 제한보다 큰 경우
-          handleError('over-size');
-        }
-      } else {
-        handleError('invalid-format');
-      }
-    } else {
+
+    if (files.length === 0) {
       // only chrome
       handleError();
+      return;
+    }
+
+    // 사진 1개씩만 선택 가능한 경우
+    if (!multiple) {
+      handleImageFile(files[0]);
+    } else {
+      // 사진 여러개 선택 가능한 경우
+      for (let i = 0; i < files.length; i++) {
+        const imgFile = files.item(i);
+        if (imgFile) handleImageFile(imgFile);
+      }
     }
   };
 
@@ -53,6 +70,7 @@ export function ImageInput({
       <InputGroup>
         <input
           ref={inputRef}
+          multiple={multiple}
           type="file"
           name="project-lc-image-upload"
           accept="image/*"
@@ -74,6 +92,7 @@ export function ImageInput({
     <input
       style={{ maxWidth: 300 }}
       accept="image/*"
+      multiple={multiple}
       type="file"
       required={required}
       onChange={(e): void => {
