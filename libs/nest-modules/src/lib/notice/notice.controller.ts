876869495d8e60b755ed0684c38510c86717f727
chanuuuuu <dn0208@gmail.com>
2021-10-20 13:15:42 +0900

    [Add] 관리자 controller 구현

--- libs/nest-modules/src/lib/notice/notice.controller.ts
+++ libs/nest-modules/src/lib/notice/notice.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  ValidationPipe,
+  Body,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { Notice } from '@prisma/client';
+import { NoticePostDto, NoticePatchDto } from '@project-lc/shared-types';
 import { NoticeService } from './notice.service';
-
+import { AdminGuard } from '../_nest-units/guards/admin.guard';
+import { JwtAuthGuard } from '../_nest-units/guards/jwt-auth.guard';
 @Controller('notice')
 export class NoticeController {
   constructor(private readonly noticeService: NoticeService) {}
@@ -9,5 +19,26 @@ export class NoticeController {
   @Get()
   getNotice(): Promise<Notice[]> {
     return this.noticeService.getNotices();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(AdminGuard)
+  @Get('/admin')
+  getAdminNotice(): Promise<Notice[]> {
+    return this.noticeService.getAdminNotices();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(AdminGuard)
+  @Post()
+  postNotice(@Body(ValidationPipe) dto: NoticePostDto): Promise<Notice> {
+    return this.noticeService.postNotice(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(AdminGuard)
+  @Patch()
+  patchNotice(@Body(ValidationPipe) dto: NoticePatchDto): Promise<Notice> {
+    return this.noticeService.patchNotice(dto);
   }
 }
