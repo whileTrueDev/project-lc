a4358ed4fabe15ee603a9bb65c5390a3fd5ed0b0
leejineun <43170606+leejineun@users.noreply.github.com>
2021-10-26 13:50:32 +0900

    반품 완료시, 이전 단계로 변경못하도록 수정

--- libs/components/src/lib/OrderReturnStatusDialog.tsx
+++ libs/components/src/lib/OrderReturnStatusDialog.tsx
@@ -10,7 +10,7 @@ import {
   Select,
   Text,
   useToast,
-  Center,
+  Stack,
 } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { useUpdateReturnStatusMutation } from '@project-lc/hooks';
@@ -56,14 +56,25 @@ export function OrderReturnStatusDialog(props: {
           <ModalHeader>반품 상태 관리</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text size="lg">현재상태 : {convertFmReturnStatusToString(data.status)}</Text>
-            <Center bg="gray.200" m={2}>
-              <Text m={2} borderRadius={3} fontWeight="bold">
-                &#8251; 반품완료 선택시, 환불이 진행되므로 반드시 물품 수령 및 확인 후,
-                반품완료를 선택하세요
-              </Text>
-            </Center>
-            <Select placeholder="진행 단계를 선택하세요" {...register('status')}>
+            <Text size="lg" mb={4}>
+              현재상태 : {convertFmReturnStatusToString(data.status)}
+            </Text>
+            {watch('status') === 'complete' && (
+              <Stack bg="gray.200" mb={2} p={2} borderRadius={3}>
+                <Text fontWeight="bold">
+                  &#8251; 반품완료 선택시, 환불이 진행되므로 반드시 물품 수령 및 확인 후,
+                  반품완료를 선택하세요.
+                </Text>
+                <Text color="tomato" fontWeight="bold">
+                  &#8251; 반품 완료 등록 후, 이전 단계로의 변경은 불가합니다.
+                </Text>
+              </Stack>
+            )}
+            <Select
+              placeholder="진행 단계를 선택하세요"
+              {...register('status')}
+              disabled={data.status === 'complete'}
+            >
               <option value="request">반품요청</option>
               <option value="ing">반품진행중</option>
               <option value="complete">반품완료</option>
