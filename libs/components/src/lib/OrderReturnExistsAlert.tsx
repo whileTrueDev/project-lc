192c87201b0ecc5c58c0bdeea0ee80b29cf1d227
Jiyeon <a1919361@gmail.com>
2021-10-26 09:22:39 +0900

    [refactor] 성격이 다른 컴포넌트 파일로 분리

--- libs/components/src/lib/OrderReturnExistsAlert.tsx
+++ libs/components/src/lib/OrderReturnExistsAlert.tsx
@@ -1,5 +1,4 @@
-import { Alert, AlertIcon, AlertDescription, Button, Text } from '@chakra-ui/react';
-import dayjs from 'dayjs';
+import { Alert, AlertIcon, AlertDescription, Button } from '@chakra-ui/react';
 
 export interface OrderReturnExistsAlertProps {
   targetSectionTitle: string;
@@ -29,22 +28,3 @@ export function OrderReturnExistsAlert({
 }
 
 export default OrderReturnExistsAlert;
-
-/** 결제취소요청 데이터가 있는 경우 알림문구 */
-export function OrderCancelRequestExistAlert({
-  data,
-}: {
-  data: { createDate: Date; reason: string };
-}): JSX.Element {
-  const { createDate, reason } = data;
-  const requestDate = dayjs(createDate).format('YYYY/MM/DD HH:mm');
-  return (
-    <Alert status="info">
-      <AlertIcon />
-      <AlertDescription>
-        이 주문에 대해 결제취소를 요청 했습니다. (요청 일시 : {requestDate}) (요청 사유 :{' '}
-        {reason})
-      </AlertDescription>
-    </Alert>
-  );
-}
