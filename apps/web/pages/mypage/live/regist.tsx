66d6a3dcc3ba93502750aa9beab875c91b49f218
leejineun <43170606+leejineun@users.noreply.github.com>
2021-11-01 15:20:12 +0900

    목록버튼 생성

--- apps/web/pages/mypage/live/regist.tsx
+++ apps/web/pages/mypage/live/regist.tsx
@@ -1,11 +1,25 @@
 import { MypageLayout, LiveShoppingRegist } from '@project-lc/components';
-import { Heading, Container } from '@chakra-ui/react';
+import { Container, Box, Flex, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
 
 export function Live(): JSX.Element {
+  const router = useRouter();
+
   return (
     <MypageLayout>
       <Container maxWidth="container.xl" my={12}>
-        <Heading>라이브 쇼핑 등록</Heading>
+        <Box as="section" mb={10}>
+          <Flex direction="row" alignItems="center" justifyContent="space-between">
+            <Button
+              size="sm"
+              leftIcon={<ChevronLeftIcon />}
+              onClick={() => router.push('/mypage/live')}
+            >
+              목록으로
+            </Button>
+          </Flex>
+        </Box>
         <LiveShoppingRegist />
       </Container>
     </MypageLayout>
