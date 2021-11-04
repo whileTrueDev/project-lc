9d784073422ffe91cb6adbff77c37be0ed3c84b2
Jiyeon <a1919361@gmail.com>
2021-11-04 15:59:25 +0900

    시드 메인 함수 정리

--- libs/prisma-orm/prisma/seed.ts
+++ libs/prisma-orm/prisma/seed.ts
@@ -1,294 +1,164 @@
-import { PrismaClient } from '@prisma/client';
+import {
+  Broadcaster,
+  Goods,
+  GoodsInfo,
+  PrismaClient,
+  Seller,
+  SellerContacts,
+  ShippingGroup,
+} from '@prisma/client';
+import {
+  defaultOption,
+  defaultSellCommissionData,
+  DummyGoodsDataType,
+  dummyGoodsList,
+  dummyImageUrlList,
+  dummyLiveShoppingData,
+  testadminData,
+  testBroadcasterData,
+  testsellerData,
+  testsellerExtraData,
+} from './dummyData';
 
 const prisma = new PrismaClient();
 
-const common_contents = `
-<p>상품결제정보</p>
-<br>
-<p>
-고액결제의 경우 안전을 위해 카드사에서 확인전화를 드릴 수도 있습니다.
-확인과정에서 도난 카드의 사용이나 타인 명의의 주문등 정상적인 주문이 아니라고 판단될 경우 임의로 주문을 보류 또는 취소할 수 있습니다.
-</p>
-<br>
-<p>
-무통장 입금은 상품 구매 대금은 PC뱅킹, 인터넷뱅킹, 텔레뱅킹 혹은 가까운 은행에서 직접 입금하시면 됩니다.
-주문시 입력한 입금자명과 실제입금자의 성명이 반드시 일치하여야 하며, 7일 이내로 입금을 하셔야 하며 입금되지 않은 주문은 자동취소 됩니다.
-</p>
+/** 테스트관리자 계정 생성 */
+async function createAdminAccount(): Promise<Seller> {
+  return prisma.seller.create({
+    data: testadminData,
+  });
+}
 
-<br>
-<p>
-배송정보
-</p>
-
-<ul>
-<li>- 배송 방법 : 택배</li>
-<li>- 배송 지역 : 전국지역</li>
-<li>- 배송 비용 : ₩3,000</li>
-<li>- 배송 기간 : 3일 ~ 7일</li>
-<li>- 배송 안내 : 산간벽지나 도서지방은 별도의 추가금액을 지불하셔야 하는 경우가 있습니다.</li>
-</ul>
-<p>고객님께서 주문하신 상품은 입금 확인후 배송해 드립니다.</p>
-<p>다만, 상품종류에 따라서 상품의 배송이 다소 지연될 수 있습니다.</p>
-`;
-const testSellerEmail = 'testSeller@gmail.com';
-const testSellerData = {
-  email: testSellerEmail,
-  name: 'testSeller',
-  password:
-    // asdfasdf!
-    '$argon2i$v=19$m=4096,t=3,p=1$97nVwdfXR9h8Wu38n5YuvQ$w5XgpncJVDAxURkmyJyMzDLMe2axEV6WT1PoSxNYqjY',
+export type SellerAccountType = Seller & {
+  shippingGroups: ShippingGroup[];
+  goodsCommonInfo: GoodsInfo[];
+  SellerContacts: SellerContacts[];
 };
-
-const testAdminEmail = 'testAdmin@gmail.com';
-const testAdminData = {
-  email: testAdminEmail,
-  name: 'test관리자',
-  password:
-    // asdfasdf!
-    '$argon2i$v=19$m=4096,t=3,p=1$97nVwdfXR9h8Wu38n5YuvQ$w5XgpncJVDAxURkmyJyMzDLMe2axEV6WT1PoSxNYqjY',
-};
-
-async function main(): Promise<void> {
-  await prisma.seller.upsert({
-    where: { email: testAdminEmail },
-    update: {},
-    create: testAdminData,
-  });
-  let seller = await prisma.seller.findUnique({
-    where: { email: testSellerData.email },
-  });
-
-  if (!seller) {
-    seller = await prisma.seller.create({
-      data: testSellerData,
-    });
-  }
-
-  // 테스트 상품 1 + 옵션 + 재고 데이터 생성
-  await prisma.goods.create({
+/** 테스트 판매자 계정 & 판매자 기본 공통정보, 정산계정, 연락처, 배송그룹 생성 */
+async function createSellerAccount(): Promise<SellerAccountType> {
+  const {
+    sellerShop,
+    goodsInfo,
+    sellerSettlementAccount,
+    sellerContacts,
+    shippingGroup,
+  } = testsellerExtraData;
+  // 판매자 계정 생성
+  const seller = await prisma.seller.create({
     data: {
-      seller: { connect: { email: seller.email } },
-      goods_name: 'testGoods1',
-      summary: '테스트상품1',
-      image: {
-        create: [
-          {
-            cut_number: 1,
-            image: 'https://picsum.photos/301/300',
-          },
-          {
-            cut_number: 2,
-            image: 'https://picsum.photos/300/300',
-          },
-          {
-            cut_number: 3,
-            image: 'https://picsum.photos/300/301',
-          },
-          {
-            cut_number: 4,
-            image: 'https://picsum.photos/301/301',
-          },
-          {
-            cut_number: 5,
-            image: 'https://picsum.photos/302/301',
-          },
-        ],
-      },
-      options: {
-        create: [
-          {
-            default_option: 'y',
-            price: 10000,
-            consumer_price: 3000,
-            supply: { create: { stock: 10 } },
-          },
-        ],
-      },
-      confirmation: {
-        create: { status: 'confirmed', firstmallGoodsConnectionId: 41 },
-      },
-      ShippingGroup: {
-        create: {
-          baseAddress: '반송지 주소',
-          detailAddress: '반송지 주소 상세',
-          postalCode: '12345',
-          shipping_group_name: '기본 배송 그룹',
-          seller: {
-            connect: { id: seller.id },
-          },
-          shippingSets: {
-            create: {
-              shipping_set_name: '배송세트이름',
-              shippingOptions: {
-                create: {
-                  shipping_opt_type: 'fixed',
-                  shippingCost: {
-                    create: {
-                      shipping_area_name: '대한민국',
-                      shipping_cost: 2500,
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-      GoodsInfo: {
-        create: {
-          info_name: '기본',
-          info_value: common_contents,
-          seller: { connect: { email: seller.email } },
-        },
-      },
-    },
-    include: { options: { include: { supply: true } } },
-  });
-  // 테스트 상품 2 + 옵션 + 재고 데이터 생성
-  await prisma.goods.create({
-    data: {
-      seller: { connect: { email: seller.email } },
-      goods_name: 'testGoods2',
-      summary: '테스트상품2',
-      image: {
-        create: {
-          cut_number: 1,
-          image: 'https://picsum.photos/302/303',
-        },
-      },
-      options: {
-        create: [
-          {
-            default_option: 'y',
-            price: 5000,
-            consumer_price: 4000,
-            supply: { create: { stock: 20 } },
-          },
-          {
-            price: 6000,
-            consumer_price: 5000,
-            supply: { create: { stock: 3 } },
-          },
-        ],
-      },
-      confirmation: {
-        create: { status: 'waiting' },
-      },
-    },
-    include: { options: { include: { supply: true } } },
-  });
-  // 테스트 상품 3 + 옵션 + 재고 데이터 생성
-  await prisma.goods.create({
-    data: {
-      sellerId: seller.id,
-      goods_name: 'testGoods3',
-      summary: '테스트상품3',
-      image: {
-        create: {
-          cut_number: 1,
-          image: 'https://picsum.photos/302/304',
-        },
-      },
-      options: {
-        create: [
-          {
-            default_option: 'y',
-            price: 500,
-            consumer_price: 30,
-            supply: { create: { stock: 40 } },
-          },
-        ],
-      },
-      confirmation: {
-        create: { status: 'confirmed', firstmallGoodsConnectionId: 42 },
-      },
-    },
-    include: { options: { include: { supply: true } } },
-  });
-
-  await prisma.sellerSettlementAccount.create({
-    data: {
-      bank: '농협',
-      name: '테스트',
-      number: '123456789',
-      settlementAccountImageName: 'test',
-      sellerEmail: seller.email,
+      ...testsellerData,
+      // 판매자 상점정보 생성
+      sellerShop: { create: sellerShop },
+      // 판매자 공통정보 생성
+      goodsCommonInfo: { create: goodsInfo },
+      // 판매자 정산계정 생성
+      sellerSettlementAccount: { create: sellerSettlementAccount },
+      // 판매자 연락처 생성
+      SellerContacts: { create: sellerContacts },
+      // 판매자 기본배송비그룹 생성
+      shippingGroups: { create: shippingGroup },
     },
   });
 
-  const sellerContacts = await prisma.sellerContacts.create({
-    data: {
-      sellerId: seller.id,
-      email: 'testcontact@test.com',
-    },
+  // 테스트 판매자 계정 & 판매자 기본 공통정보, 정산계정, 연락처, 배송그룹 정보 리턴
+  return prisma.seller.findUnique({
+    where: { id: seller.id },
+    include: { shippingGroups: true, goodsCommonInfo: true, SellerContacts: true },
   });
-  const testbroadcaster = await prisma.broadcaster.create({
-    data: {
-      userNickname: '테스트방송인',
-      overlayUrl: 'test',
-      userId: 'test-broadcaster',
-      userName: '테스트방송인',
-    },
-  });
-  // 테스트 상품 4 + 라이브쇼핑 생성
-  await prisma.goods.create({
-    data: {
-      sellerId: seller.id,
-      goods_name: 'testGoods4',
-      summary: '테스트상품4',
-      image: {
-        create: {
-          cut_number: 1,
-          image: 'https://picsum.photos/302/305',
-        },
-      },
-      options: {
-        create: [
-          {
-            default_option: 'y',
-            price: 500,
-            consumer_price: 30,
-            supply: { create: { stock: 40 } },
-          },
-        ],
-      },
-      confirmation: {
-        create: { status: 'confirmed', firstmallGoodsConnectionId: 68 },
-      },
-      LiveShopping: {
-        create: {
-          sellerId: seller.id,
-          contactId: sellerContacts.id,
-          sellStartDate: new Date('2021-10-01 00:00:00.000'),
-          sellEndDate: new Date(),
-          broadcasterId: testbroadcaster.userId,
-          whiletrueCommissionRate: '5',
-          broadcasterCommissionRate: '10',
-          progress: 'confirmed',
-        },
-      },
-    },
-    include: { options: { include: { supply: true } } },
+}
+
+/** 테스트방송인 생성 */
+async function createBroadcaster(): Promise<Broadcaster> {
+  return prisma.broadcaster.create({
+    data: testBroadcasterData,
   });
 }
 
 /** 판매 수수료 기본값 설정 */
 const generateDefaultSellCommission = async (): Promise<void> => {
   await prisma.sellCommission.upsert({
-    create: {
-      commissionDecimal: '0.05',
-      commissionRate: '5',
-    },
-    update: {
-      commissionDecimal: '0.05',
-      commissionRate: '5',
-    },
+    create: defaultSellCommissionData,
+    update: defaultSellCommissionData,
     where: { id: 1 },
   });
 };
 
+/** 더미 상품 데이터 생성 */
+async function createDummyGoods(
+  seller: SellerAccountType,
+  goods: DummyGoodsDataType,
+): Promise<Goods> {
+  const { goods_name, summary, confirmation } = goods;
+  const sellerDefaultShippingGroup = seller.shippingGroups[0];
+  const sellerDefaultCommonInfo = seller.goodsCommonInfo[0];
+  const createdGoods = await prisma.goods.create({
+    data: {
+      seller: { connect: { email: seller.email } },
+      ShippingGroup: {
+        connect: { id: sellerDefaultShippingGroup.id },
+      },
+      GoodsInfo: {
+        connect: { id: sellerDefaultCommonInfo.id },
+      },
+      goods_name,
+      summary,
+      image: {
+        create: dummyImageUrlList.map((url, idx) => ({
+          cut_number: idx,
+          image: url,
+        })),
+      },
+      options: { create: [defaultOption] },
+      confirmation: { create: confirmation },
+    },
+  });
+  return createdGoods;
+}
+
+/** 더미 라이브쇼핑 생성 */
+async function createDummyLiveShopping(
+  seller: SellerAccountType,
+  broadcaster: Broadcaster,
+  goods: Goods,
+): Promise<void> {
+  await prisma.liveShopping.create({
+    data: {
+      seller: { connect: { id: seller.id } },
+      goods: {
+        connect: { id: goods.id },
+      },
+      broadcaster: { connect: { id: broadcaster.id } },
+      sellerContacts: { connect: { id: seller.SellerContacts[0].id } },
+      ...dummyLiveShoppingData,
+    },
+  });
+}
+
+/** 시드 메인 함수 */
+async function main(): Promise<void> {
+  // 판매 수수료 기본값 설정
+  await generateDefaultSellCommission();
+
+  // 관리자 계정 생성
+  await createAdminAccount();
+
+  // 판매자 계정 생성
+  const seller = await createSellerAccount();
+
+  // 테스트방송인 데이터 생성
+  const testbroadcaster = await createBroadcaster();
+
+  // 더미 상품 데이터 생성
+  await createDummyGoods(seller, dummyGoodsList[0]);
+  await createDummyGoods(seller, dummyGoodsList[1]);
+  await createDummyGoods(seller, dummyGoodsList[2]);
+  const goods4 = await createDummyGoods(seller, dummyGoodsList[3]);
+
+  // 테스트상품4의 라이브쇼핑 생성
+  await createDummyLiveShopping(seller, testbroadcaster, goods4);
+}
+
 main()
-  .then(() => generateDefaultSellCommission())
   .catch((e) => {
     console.error(e);
     process.exit(1);
