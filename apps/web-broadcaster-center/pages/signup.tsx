9a584e2e2348fefa90a41c96e040e5d835b5a94f
Jiyeon <a1919361@gmail.com>
2021-12-01 17:02:43 +0900

    [add] 회원가입 시작화면 유저타입 prop 추가 -> 문구 표시 변경

--- apps/web-broadcaster-center/pages/signup.tsx
+++ apps/web-broadcaster-center/pages/signup.tsx
@@ -1,8 +1,24 @@
+import { SignupForm, SignupStart, Navbar } from '@project-lc/components';
+import { useMoveToMainIfLoggedIn } from '@project-lc/hooks';
+import { Box, Flex } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
 export interface SignUpProps {
   propname: any;
 }
 export function SignUp({ propname }: SignUpProps): JSX.Element {
-  return <div>회원가입</div>;
+  const [step, setStep] = useState(0);
+  useMoveToMainIfLoggedIn();
+  return (
+    <Box>
+      <Navbar />
+      <Flex align="center" justify="center" minH="calc(100vh - 200px)">
+        {step === 0 && (
+          <SignupStart userType="broadcaster" moveToSignupForm={() => setStep(1)} />
+        )}
+        {step === 1 && <SignupForm enableShadow moveToSignupStart={() => setStep(0)} />}
+      </Flex>
+    </Box>
+  );
 }
-
 export default SignUp;
