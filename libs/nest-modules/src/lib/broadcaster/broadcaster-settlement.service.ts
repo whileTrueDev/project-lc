f41d19f6ecfee5d31712e826a383591b8710311b
Jiyeon <a1919361@gmail.com>
2021-12-08 16:03:32 +0900

    [add] 정산정보 저장, 조회에 암호화, 복호화 적용

--- libs/nest-modules/src/lib/broadcaster/broadcaster-settlement.service.ts
+++ libs/nest-modules/src/lib/broadcaster/broadcaster-settlement.service.ts
@@ -7,12 +7,14 @@ import { PrismaService } from '@project-lc/prisma-orm';
 import { BroadcasterSettlementInfoDto } from '@project-lc/shared-types';
 import { BroadcasterSettlementInfo } from '.prisma/client';
 import { BroadcasterService } from './broadcaster.service';
+import { CipherService } from '../auth/cipher.service';
 
 @Injectable()
 export class BroadcasterSettlementService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly broadcasterService: BroadcasterService,
+    private cipherService: CipherService,
   ) {}
 
   /** 방송인 정산정보 등록 및 수정 */
@@ -28,18 +30,23 @@ export class BroadcasterSettlementService {
       );
     }
 
-    // TODO: 주민등록번호, 휴대전화는 암호화하여 저장
+    // 주민등록번호, 휴대전화는 암호화하여 저장
+    const { idCardNumber, phoneNumber } = data;
 
     try {
       return this.prisma.broadcasterSettlementInfo.upsert({
         where: { broadcasterId },
         create: {
           ...data,
+          idCardNumber: this.cipherService.getEncryptedText(idCardNumber),
+          phoneNumber: this.cipherService.getEncryptedText(phoneNumber),
           broadcaster: { connect: { id: broadcasterId } },
           confirmation: { create: { rejectionReason: null } },
         },
         update: {
           ...data,
+          idCardNumber: this.cipherService.getEncryptedText(idCardNumber),
+          phoneNumber: this.cipherService.getEncryptedText(phoneNumber),
         },
       });
     } catch (error) {
@@ -55,13 +62,40 @@ export class BroadcasterSettlementService {
     broadcasterId: number,
   ): Promise<BroadcasterSettlementInfo | null> {
     try {
-      // TODO: 주민등록번호, 휴대전화는 복호화하여 일부 가린상태로 반환
-      return this.prisma.broadcasterSettlementInfo.findUnique({
+      const data = await this.prisma.broadcasterSettlementInfo.findUnique({
         where: { broadcasterId },
         include: { confirmation: true },
       });
+      if (!data) return data;
+
+      // 주민등록번호, 휴대전화는 복호화 한 후 일부를 가린상태로 전송
+      const { idCardNumber, phoneNumber } = data;
+
+      return {
+        ...data,
+        idCardNumber: this.blindIdCardNumber(idCardNumber),
+        phoneNumber: this.blindPhoneNumber(phoneNumber),
+      };
     } catch (error) {
       throw new InternalServerErrorException(error);
     }
+  }
+
+  /** 정산정보 - 암호화된 핸드폰번호를 복호화 & 일부를 가리고 리턴함 */
+  private blindPhoneNumber(encrypted?: string): string | null {
+    if (!encrypted) return null;
+    const data = this.cipherService.getDecryptedText(encrypted);
+    // 전화번호는 010-1234-1234 형태로 입력된다
+    const [first, second, third] = data.split('-');
+    return [first, second.replace(/./g, '*'), third].join('-');
+  }
+
+  /** 정산정보 - 암호화된 주민등록번호를 복호화 & 일부를 가리고 리턴함 */
+  private blindIdCardNumber(encrypted?: string): string | null {
+    if (!encrypted) return null;
+    const data = this.cipherService.getDecryptedText(encrypted);
+    // 주민등록번호는 000000-0000000 형태로 입력된다
+    const [first, second] = data.split('-');
+    return [first, second.replace(/./g, '*')].join('-');
   }
 }
