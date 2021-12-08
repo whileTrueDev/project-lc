cbba89aa999227a8f4197ce0032cef5e67dbfecf
chanuuuuu <dn0208@gmail.com>
2021-12-08 17:35:23 +0900

    [Add] 이용약관 동의 페이지 컴포넌트 구현

--- libs/components/src/lib/SettingNeedAlertBox.tsx
+++ libs/components/src/lib/SettingNeedAlertBox.tsx
@@ -7,13 +7,19 @@ import {
   AlertDescription,
 } from '@chakra-ui/react';
 
-export function SettingNeedAlertBox({ text }: { text: React.ReactNode }): JSX.Element {
+export function SettingNeedAlertBox({
+  title,
+  text,
+}: {
+  title?: string;
+  text: React.ReactNode;
+}): JSX.Element {
   return (
     <Alert status="warning">
       <Stack>
         <HStack spacing={0}>
           <AlertIcon />
-          <AlertTitle>입력이 필요합니다!</AlertTitle>
+          <AlertTitle>{title || '입력이 필요합니다!'}</AlertTitle>
         </HStack>
         <AlertDescription>{text}</AlertDescription>
       </Stack>
