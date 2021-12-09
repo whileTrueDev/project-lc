4b22dc50081205fe21cbd6766bc71b0637993370
Jiyeon <a1919361@gmail.com>
2021-12-09 17:05:27 +0900

    [add] 반려상태, 사유 표시

--- libs/components/src/lib/BroadcasterSettlementInfoRegistBox.tsx
+++ libs/components/src/lib/BroadcasterSettlementInfoRegistBox.tsx
@@ -11,36 +11,56 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { useBroadcasterSettlementInfo, useProfile } from '@project-lc/hooks';
+import { useMemo } from 'react';
 import { BroadcasterSettlementInfoConfirmation } from '.prisma/client';
 import { BroadcasterSettlementInfoDialog } from './BroadcasterSettlementInfoDialog';
 import { GoodsConfirmStatusBadge } from './GoodsConfirmStatusBadge';
-import TextWithPopperButton from './TextWithPopperButton';
+import { GridTableItem } from './GridTableItem';
 
-function BroadcasterSettlementConfirmationBadge({
+/** 정산정보 검수상태, 반려사유 표시 */
+function BroadcasterSettlementConfirmationDisplay({
   confirmation,
 }: {
   confirmation: BroadcasterSettlementInfoConfirmation;
 }): JSX.Element {
   const { status, rejectionReason } = confirmation;
-  if (status === 'rejected') {
-    return (
-      <>
-        <Text>검수상태 : </Text>
-        <TextWithPopperButton
-          title={<GoodsConfirmStatusBadge confirmStatus={status} />}
-          iconAriaLabel=""
-          portalBody
-        >
-          <Text fontWeight="bold" mb={1}>
-            반려 사유
-          </Text>
-          <Text whiteSpace="break-spaces">{rejectionReason}</Text>
-        </TextWithPopperButton>
-      </>
-    );
-  }
 
-  return <GoodsConfirmStatusBadge confirmStatus={status} />;
+  const badge = useMemo(
+    () => <GoodsConfirmStatusBadge confirmStatus={status} />,
+    [status],
+  );
+
+  const description = useMemo(() => {
+    if (status === 'waiting') {
+      return (
+        <Text fontSize="sm">
+          입력하신 정산 정보를 관리자가 검수중입니다. 승인받은 후 수익금 정산이
+          가능합니다.
+        </Text>
+      );
+    }
+    if (status === 'rejected') {
+      return (
+        <>
+          <Text fontSize="sm">
+            승인 반려되었습니다. 아래 반려사유를 확인한 후 다시 정산 정보를 등록해주세요.
+          </Text>
+          <Text fontSize="sm">{rejectionReason}</Text>
+        </>
+      );
+    }
+    return null;
+  }, [rejectionReason, status]);
+
+  return (
+    <VStack alignItems="stretch">
+      <Flex alignItems="center">
+        <Text mr={1}>검수상태</Text>
+        {badge}
+      </Flex>
+      {description}
+    </VStack>
+  );
 }
 
 export function BroadcasterSettlementInfoRegistBox(): JSX.Element {
@@ -62,7 +82,7 @@ export function BroadcasterSettlementInfoRegistBox(): JSX.Element {
           정산 정보 등록
         </Button>
       </Flex>
-      <Divider backgroundColor="gray.100" />
+      <Divider backgroundColor="gray.100" mb={3} />
 
       {/* 정산정보 조회중인경우 */}
       {isLoading && (
@@ -73,31 +93,64 @@ export function BroadcasterSettlementInfoRegistBox(): JSX.Element {
 
       {/* 정산정보 없는 경우 */}
       {!isLoading && !settlementInfoData && (
-        <>
-          <VStack spacing={2} justifyContent="center" py={10}>
-            <Text>등록된 정산 정보가 없습니다.</Text>
-            <Text fontWeight="bold">
-              정산 정보를 등록하지 않으면 수익금 정산이 불가능합니다.
-            </Text>
-            <Text>가능한 빨리 정산 정보를 입력하여 수익금 받을 준비를 해두세요!</Text>
-            <Text>위의 정산 정보 등록 버튼을 통해 정산 정보를 입력할 수 있습니다.</Text>
-          </VStack>
-        </>
+        <VStack spacing={2} justifyContent="center" py={10}>
+          <Text>등록된 정산 정보가 없습니다.</Text>
+          <Text fontWeight="bold">
+            정산 정보를 등록하지 않으면 수익금 정산이 불가능합니다.
+          </Text>
+          <Text>가능한 빨리 정산 정보를 입력하여 수익금 받을 준비를 해두세요!</Text>
+          <Text>위의 정산 정보 등록 버튼을 통해 정산 정보를 입력할 수 있습니다.</Text>
+        </VStack>
       )}
 
       {/* 정산정보 존재하는 경우 */}
       {!isLoading && settlementInfoData && (
-        <>
-          <BroadcasterSettlementConfirmationBadge
+        <VStack spacing={4} alignItems="stretch">
+          {/* 정산정보 검수상태 표시 */}
+          <BroadcasterSettlementConfirmationDisplay
             confirmation={settlementInfoData.confirmation}
           />
-          <Grid templateColumns="2fr 3fr" borderTopColor="gray.100" borderTopWidth={1.5}>
-            {JSON.stringify(settlementInfoData, null, 2)}
-            {/* {makeListRow(sellerBusinessRegistration).map(({ title, value }) => (
-            <GridTableItem title={title} value={value} key={title} />
-          ))} */}
-          </Grid>
-        </>
+          {/* 입력한 정산정보 표시 */}
+          <VStack alignItems="stretch">
+            <Text>계약자 정보</Text>
+            <Grid
+              templateColumns="2fr 3fr"
+              borderTopColor="gray.100"
+              borderTopWidth={1.5}
+            >
+              <GridTableItem title="과세유형" value={settlementInfoData.type} />
+              <GridTableItem title="성명" value={settlementInfoData.name} />
+              <GridTableItem
+                title="주민등록번호"
+                value={settlementInfoData.idCardNumber}
+              />
+              <GridTableItem
+                title="휴대전화번호"
+                value={settlementInfoData.phoneNumber}
+              />
+              <GridTableItem
+                title="신분증"
+                value={<Button size="xs">신분증 확인</Button>}
+              />
+            </Grid>
+          </VStack>
+
+          <VStack alignItems="stretch">
+            <Text>정산계좌정보</Text>
+            <Grid
+              templateColumns="2fr 3fr"
+              borderTopColor="gray.100"
+              borderTopWidth={1.5}
+            >
+              <GridTableItem title="예금주" value={settlementInfoData.accountHolder} />
+              <GridTableItem title="계좌번호" value={settlementInfoData.accountNumber} />
+              <GridTableItem
+                title="통장사본"
+                value={<Button size="xs">신분증 확인</Button>}
+              />
+            </Grid>
+          </VStack>
+        </VStack>
       )}
 
       {/* 정산정보 등록 폼 다이얼로그 */}
