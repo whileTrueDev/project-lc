774515b2aa67879f3f7c79105f4f7a2d68df3849
Jiyeon <a1919361@gmail.com>
2021-12-16 14:38:02 +0900

    [add] 개인알림 라우터, 핸들러 1차 작성

--- libs/nest-modules/src/lib/notification/notification.controller.ts
+++ libs/nest-modules/src/lib/notification/notification.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
-import { NotificationService } from './notification.service';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UserNotification } from '@prisma/client';
+import { UserType } from '@project-lc/shared-types';
+import {
+  CreateMultipleNotificationDto,
+  CreateNotificationDto,
+  MarkNotificationReadStateDto,
+  NotificationService,
+} from './notification.service';
 
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
+  // TODO: admin가드 추가
+  /** 관리자 -> 특정 유저에게 알림생성 */
+  @Post('/admin')
+  createNotification(@Body() dto: CreateNotificationDto): Promise<UserNotification> {
+    return this.notificationService.createNotification(dto);
+  }
+
+  /** 관리자 -> 여러 유저에게 동일한 내용의 알림 생성 */
+  @Post('/admin/multiple')
+  createMultipleNotification(
+    @Body() dto: CreateMultipleNotificationDto,
+  ): Promise<boolean> {
+    return this.notificationService.createMultipleNotification(dto);
+  }
+
+  /** 특정 유저의 알림목록 조회(생성일 내림차순)
+   * @query userEmail 타겟 유저의 이메일
+   * @query userType 'seller' | 'broadcaster'
+   * @query take? 기본 6개, 몇개 조회할건지
+   */
   @Get()
-  test(): string {
-    return 'notification string';
+  findNotifications(
+    @Query('userEmail') userEmail: string,
+    @Query('userType') userType: UserType,
+    @Query('take', ParseIntPipe, new DefaultValuePipe(6)) take: number,
+  ): Promise<UserNotification[]> {
+    return this.notificationService.findNotifications({ userEmail, userType, take });
+  }
+
+  /** 특정 알림의 읽음상태 변경 */
+  @Patch()
+  markNotificationReadState(@Body() dto: MarkNotificationReadStateDto): Promise<boolean> {
+    return this.notificationService.markNotificationReadState(dto);
   }
 }
--- libs/nest-modules/src/lib/notification/notification.service.ts
+++ libs/nest-modules/src/lib/notification/notification.service.ts
@@ -1,4 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import { UserNotification } from '@prisma/client';
+import { PrismaService } from '@project-lc/prisma-orm';
+import { UserType } from '@project-lc/shared-types';
 
+// TODO: dto 파일 분리, 컨트롤러에 validationpipe 적용
+export class CreateNotificationDto {
+  userEmail: string;
+  userType: UserType;
+  title: string;
+  content: string;
+}
+export class CreateMultipleNotificationDto {
+  userEmailList: string[];
+  userType: UserType;
+  title: string;
+  content: string;
+}
+export class FindNotificationsDto {
+  userEmail: string;
+  userType: UserType;
+  take?: number;
+}
+
+export class MarkNotificationReadStateDto {
+  userEmail: string;
+  userType: UserType;
+  id: number;
+}
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  /** 특정 유저 1명에 대한 알림 생성 */
+  async createNotification(dto: CreateNotificationDto): Promise<UserNotification> {
+    const newNoti = await this.prisma.userNotification.create({
+      data: dto,
+    });
+
+    return newNoti;
+  }
+
+  /** 여러 유저에 대한 동일한 내용의 알림 생성 */
+  async createMultipleNotification(dto: CreateMultipleNotificationDto): Promise<boolean> {
+    const { userEmailList, ...rest } = dto;
+    const data = userEmailList.map((userEmail) => ({ userEmail, ...rest }));
+    await this.prisma.userNotification.createMany({
+      data,
+      skipDuplicates: true,
+    });
+    return true;
+  }
+
+  /** 특정 유저의 알림목록 조회 -  최근 6개 createDate:desc, limit: 6 */
+  async findNotifications({
+    userEmail,
+    userType,
+    take = 6,
+  }: FindNotificationsDto): Promise<UserNotification[]> {
+    const data = await this.prisma.userNotification.findMany({
+      where: {
+        userEmail,
+        userType,
+      },
+      orderBy: { createDate: 'desc' },
+      take,
+    });
+    return data;
+  }
+
+  /** 특정 알림의 읽음상태 변경 */
+  async markNotificationReadState({
+    id,
+  }: MarkNotificationReadStateDto): Promise<boolean> {
+    await this.prisma.userNotification.update({
+      where: { id },
+      data: {
+        readState: true,
+      },
+    });
+    return true;
+  }
+}
