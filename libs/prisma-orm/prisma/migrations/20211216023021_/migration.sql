9cb44533739c30964006f620696775435f68fe88
leejineun <43170606+leejineun@users.noreply.github.com>
2021-12-17 14:11:32 +0900

    migration 파일 통합

--- libs/prisma-orm/prisma/migrations/20211216023021_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Inquiry` MODIFY `readFlag` BOOLEAN NOT NULL DEFAULT false;
--- libs/prisma-orm/prisma/migrations/20211216051306_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Inquiry` MODIFY `phoneNumber` VARCHAR(191) NULL,
-    MODIFY `brandName` VARCHAR(191) NULL,
-    MODIFY `homepage` VARCHAR(191) NULL,
-    MODIFY `readFlag` BOOLEAN NULL DEFAULT false;
