de8c6d827e2e25407760e80785350063bb140e85
leejineun <43170606+leejineun@users.noreply.github.com>
2021-12-21 17:40:06 +0900

    비회원 구매 사운드 롤백

--- apps/overlay/src/lib/overlayClient.js
+++ apps/overlay/src/lib/overlayClient.js
@@ -364,7 +364,7 @@ socket.on('get non client purchase message', async (data) => {
 
   messageHtml = `
   <div class="donation-wrapper">
-    <iframe src="/audio/xmas-alarm-type-1.mp3"
+    <iframe src="/audio/alarm-type-1.wav"
     id="iframeAudio" allow="autoplay" style="display:none"></iframe>
     <div class="centered">
       <div class ="animated heartbeat" id="donation-top">
