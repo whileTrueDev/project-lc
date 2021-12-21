dfa1193b0d526744d4fdf8b47ccc551eeece3a3d
hwasurr <iamsupermazinga@gmail.com>
2021-12-21 17:14:00 +0900

    Fix: ignore build script

--- tools/ignore-vercel-build.sh
+++ tools/ignore-vercel-build.sh
@@ -1,30 +1,31 @@
 # Ignore preview builds
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-if [[ $VERCEL_GIT_COMMIT_REF != "master" || $VERCEL_GIT_COMMIT_REF != "dev" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" != "master" || "$VERCEL_GIT_COMMIT_REF" != "dev" ]]; then
     echo "🛑 - Ignoring preview builds"
-    exit 0;
-else
-    # Name of the app to check. Change this to your application name!
-    APP=web
-    # Determine version of Nx installed
-    NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
+    exit 0
+fi
 
-    # Install @nrwl/workspace in order to run the affected command
-    npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline 
 
-    npm install -D typescript
+# Name of the app to check. Change this to your application name!
+APP=web
+# Determine version of Nx installed
+NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
 
-    # Run the affected command, comparing latest commit to the one before that
-    npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+# Install @nrwl/workspace in order to run the affected command
+npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline 
 
-    # Store result of the previous command (grep)
-    IS_AFFECTED=$?
+npm install -D typescript
 
-    if [ $IS_AFFECTED -eq 1 ]; then
-        echo "🛑 - Build cancelled (No code changes)"
-        exit 0
-    elif [ $IS_AFFECTED -eq 0 ]; then
-        echo "✅ - Build can proceed (Code changes Exist)"
-        exit 1
-    fi
+# Run the affected command, comparing latest commit to the one before that
+npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+
+# Store result of the previous command (grep)
+IS_AFFECTED=$?
+
+if [ $IS_AFFECTED -eq 1 ]; then
+    echo "🛑 - Build cancelled (No code changes)"
+    exit 0
+elif [ $IS_AFFECTED -eq 0 ]; then
+    echo "✅ - Build can proceed (Code changes Exist)"
+    exit 1
 fi
