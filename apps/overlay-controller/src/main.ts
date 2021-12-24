a8abc09e7da12b02f71912254f7ec23027358dfe
hwasurr <iamsupermazinga@gmail.com>
2021-12-24 10:32:40 +0900

    Fix: deploy overlaycontroller script

--- apps/overlay-controller/src/main.ts
+++ apps/overlay-controller/src/main.ts
@@ -15,7 +15,6 @@ async function bootstrap(): Promise<void> {
 
   const appSetting = new AppSetting(app);
   appSetting.initialize();
-
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log(`Listening at http://localhost:${port}`);
