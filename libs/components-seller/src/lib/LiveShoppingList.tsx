1447a28b01187a687ddca7b4ee44531908b29c7a
leejineun <43170606+leejineun@users.noreply.github.com>
2022-01-07 17:58:38 +0900

    feat(web-seller): 라이브쇼핑 테이블 - 라이브쇼핑 이름 추가
    라이브 쇼핑 목록 테이블과 상세보기에 라이브 쇼핑 이름 추가

--- libs/components-seller/src/lib/LiveShoppingList.tsx
+++ libs/components-seller/src/lib/LiveShoppingList.tsx
@@ -118,6 +118,14 @@ export function LiveShoppingList(): JSX.Element {
 
   const columns: GridColumns = [
     {
+      field: 'liveShoppingName',
+      headerName: '라이브 쇼핑명',
+      minWidth: 350,
+      flex: 1,
+      valueFormatter: ({ row }) =>
+        row.liveShoppingName || '라이브 쇼핑명은 라이브 쇼핑 확정 후, 등록됩니다.',
+    },
+    {
       field: 'goods.confirmation.firstmallGoodsConnectionId',
       headerName: '상품명',
       minWidth: 350,
@@ -151,7 +159,7 @@ export function LiveShoppingList(): JSX.Element {
       field: 'broadcaster',
       headerName: '방송인',
       minWidth: 200,
-      flex: 1,
+
       renderCell: ({ row }: GridRowData) => (
         <Flex alignItems="center">
           <Box mr={1}>
@@ -165,7 +173,6 @@ export function LiveShoppingList(): JSX.Element {
       headerName: '방송시간',
       field: '방송시간',
       minWidth: 300,
-      flex: 1,
       renderCell: ({ row }: GridRowData) =>
         `${
           row.broadcastStartDate
@@ -181,7 +188,6 @@ export function LiveShoppingList(): JSX.Element {
       headerName: '판매시간',
       field: '판매시간',
       minWidth: 300,
-      flex: 1,
       renderCell: ({ row }: GridRowData) =>
         `${
           row.sellStartDate ? dayjs(row.sellStartDate).format('YYYY/MM/DD HH:mm') : '미정'
@@ -219,7 +225,7 @@ export function LiveShoppingList(): JSX.Element {
     {
       headerName: '',
       field: '',
-      width: 140,
+      width: 100,
       renderCell: ({ row }: GridRowData) => (
         <Stack direction="row">
           <Button
