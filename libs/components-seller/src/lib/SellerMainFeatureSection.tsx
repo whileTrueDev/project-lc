c63515efc3daf5c9c00aa7d687e0c27e2c3d5046
Jiyeon <a1919361@gmail.com>
2022-01-11 10:53:55 +0900

    [refactor] 판매자 메인 섹션 레이아웃 컴포넌트 분리

--- libs/components-seller/src/lib/SellerMainFeatureSection.tsx
+++ libs/components-seller/src/lib/SellerMainFeatureSection.tsx
@@ -1,11 +1,100 @@
-import { Stack, Text, Box, Flex, Container } from '@chakra-ui/react';
+import { Stack, Text, Box, Flex, Container, StackProps } from '@chakra-ui/react';
 import {
   sellerMainSectionText,
   featureSectionBody,
   SectionData,
 } from '@project-lc/components-constants/sellerMainText';
 import { motion } from 'framer-motion';
+import React from 'react';
 
+/** 크크쇼 판매자센터 메인 섹션 타이틀 옆 애니메이션 막대 */
+export function FadeInBar(): JSX.Element {
+  return (
+    <Box
+      position={{ base: 'relative', xl: 'absolute' }}
+      left={{ base: 0, xl: '-80px' }}
+      top={{ base: '-0.5rem', xl: '1.5rem' }}
+      w="80px"
+      height="5px"
+      display="inline-block"
+    >
+      <motion.div
+        style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          background: 'var(--chakra-colors-blue-500)',
+        }}
+        initial={{
+          right: '100%',
+          opacity: 0,
+        }}
+        whileInView={{
+          right: 0,
+          opacity: 1,
+        }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        viewport={{ once: true }}
+      />
+    </Box>
+  );
+}
+
+/** 크크쇼 판매자센터 메인페이지 섹션 공용 레이아웃 컴포넌트 */
+export function SellerMainSectionContainer({
+  children,
+  sectionData,
+  bgProps,
+}: {
+  children?: React.ReactNode;
+  sectionData: SectionData;
+  bgProps?: StackProps;
+}): JSX.Element {
+  const { title, desc } = sectionData;
+  return (
+    // 컨테이너 배경이 되는 부분
+    <Stack py={{ base: 10, sm: 14 }} {...bgProps}>
+      {/* 실제 컨텐츠 영역(최대너비 1200px임, breakpoint는 xl(1280px) 사용) */}
+      <Container maxW="container.xl">
+        <Stack spacing={{ base: 6, sm: 8 }}>
+          <Box
+            pl={{ base: 0, md: '1.5rem' }}
+            textAlign={{ base: 'center', md: 'left' }}
+            position="relative"
+          >
+            <FadeInBar />
+            <Stack>
+              {title && (
+                <Text
+                  fontFamily="Gmarket Sans"
+                  fontWeight="bold"
+                  lineHeight="shorter"
+                  whiteSpace="pre-line"
+                  fontSize={{ base: '2xl', sm: '4xl' }}
+                >
+                  {title}
+                </Text>
+              )}
+              {desc && (
+                <Text
+                  whiteSpace={{ base: 'pre-line', md: 'normal' }}
+                  fontSize={{ base: 'sm', sm: 'lg' }}
+                >
+                  {desc}
+                </Text>
+              )}
+            </Stack>
+          </Box>
+          {children}
+        </Stack>
+      </Container>
+    </Stack>
+  );
+}
+
+/** 크크쇼 판매자센터 특징 아이템 */
 export function SellerMainFeatureItem({ item }: { item: SectionData }): JSX.Element {
   const { title: subTitle, desc } = item;
   return (
@@ -41,80 +130,26 @@ export function SellerMainFeatureItem({ item }: { item: SectionData }): JSX.Elem
   );
 }
 
-export function FadeInBar(): JSX.Element {
-  return (
-    <Box
-      position={{ base: 'relative', xl: 'absolute' }}
-      left={{ base: 0, xl: '-80px' }}
-      top={{ base: 0, xl: '1.5rem' }}
-      w="80px"
-      height="5px"
-      display="inline-block"
-    >
-      <motion.div
-        style={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          background: 'var(--chakra-colors-blue-500)',
-        }}
-        initial={{
-          right: '100%',
-          opacity: 0,
-        }}
-        whileInView={{
-          right: 0,
-          opacity: 1,
-        }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
-        viewport={{ once: true }}
-      />
-    </Box>
-  );
-}
-
-/**
- * @returns 크크쇼 특징 섹션
- */
+/** 판매자센터 특징 섹션 */
 export function SellerMainFeatureSection(): JSX.Element {
   const { title } = sellerMainSectionText.feature;
   return (
-    // 배경
-    <Stack
-      bg="gray.50"
-      py={{ base: 10, sm: 14 }}
-      // TODO: 반응형일시 배경이미지 위치 어떻게 할지 생각 텍스트인데 pseudo element로 넣으면 안되려나??
-      backgroundImage={`url('/images/main/feature-bg-mobile/bg.png')`}
-      backgroundRepeat="no-repeat"
-      backgroundSize="contain"
-      backgroundPosition="bottom right"
+    <SellerMainSectionContainer
+      sectionData={{
+        title,
+      }}
+      bgProps={{
+        bg: 'gray.50',
+        backgroundImage: `url('/images/main/feature-bg-mobile/bg.png')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        backgroundPosition: 'bottom right',
+      }}
     >
-      {/* maxWidth container */}
-      <Container maxW="container.xl">
-        <Stack spacing={{ base: 6, sm: 8 }}>
-          <Box
-            pl={{ base: 0, md: '1.5rem' }}
-            textAlign={{ base: 'center', md: 'left' }}
-            position="relative"
-          >
-            <FadeInBar />
-            <Text
-              fontFamily="Gmarket Sans"
-              fontWeight="bold"
-              whiteSpace="pre-line"
-              fontSize={{ base: '2xl', sm: '4xl' }}
-            >
-              {title}
-            </Text>
-          </Box>
-          {featureSectionBody.map((item) => (
-            <SellerMainFeatureItem item={item} key={item.title} />
-          ))}
-        </Stack>
-      </Container>
-    </Stack>
+      {featureSectionBody.map((item) => (
+        <SellerMainFeatureItem item={item} key={item.title} />
+      ))}
+    </SellerMainSectionContainer>
   );
 }
 
