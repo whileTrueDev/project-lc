43bf1aaff48cfd74d97aaa42325b9b64810b4339
Jiyeon <a1919361@gmail.com>
2022-01-12 16:05:35 +0900

    [feat] 절차 영역 텍스트 애니메이션 적용

--- libs/components-seller/src/lib/SellerMainProcessSection.tsx
+++ libs/components-seller/src/lib/SellerMainProcessSection.tsx
@@ -1,10 +1,12 @@
-import { Box, Text, Flex } from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { Box, Text, Flex, Stack, TextProps } from '@chakra-ui/react';
 import {
   sellerMainSectionText,
   processSectionBody,
 } from '@project-lc/components-constants/sellerMainText';
 import { ChakraNextImage } from '@project-lc/components-core/ChakraNextImage';
 import { useDisplaySize } from '@project-lc/hooks';
+import { motion } from 'framer-motion';
 import { Fragment } from 'react';
 import { SellerMainSectionContainer } from './SellerMainFeatureSection';
 
@@ -24,33 +26,77 @@ export function SellerMainProcessImageMobile(): JSX.Element {
   );
 }
 
-/** 과정 이미지 : 데스크톱화면(750 이상)일때  
- * 
- //TODO : 텍스트만 애니메이션 적용 필요
+const container = {
+  offscreen: {},
+  onscreen: {
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const text = {
+  offscreen: { opacity: 0, y: 40 },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      bounce: 0.2,
+      duration: 1,
+    },
+  },
+};
+
+const MotionText = motion<TextProps>(Text);
+/** 과정 이미지 : 데스크톱화면(750 이상)일때
  */
 export function SellerMainProcessImageDeskTop(): JSX.Element {
   return (
-    <Flex>
-      {processSectionBody.map((item, index) => {
-        const { title, img } = item;
-        if (!title || !img) return null;
-        return (
-          <Fragment key={item.title}>
-            <Box w="100px" h="100px">
-              <ChakraNextImage
-                layout="responsive"
-                width={100}
-                height={100}
-                src={img}
-                objectFit="contain"
-              />
-              <Text>{item.title}</Text>
-            </Box>
-            {index !== processSectionBody.length - 1 && <Text> &gt;</Text>}
-          </Fragment>
-        );
-      })}
-    </Flex>
+    <motion.div
+      variants={container}
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 'all' }}
+    >
+      <Stack direction="row" pl={6}>
+        {processSectionBody.map((item, index) => {
+          const { title, img } = item;
+          if (!title || !img) return null;
+          return (
+            <Fragment key={item.title}>
+              <Stack alignItems="center" spacing={{ md: 4, lg: 6 }}>
+                <Box w={{ md: '75px', lg: '100px' }} maxW="100px" maxH="100px">
+                  <ChakraNextImage
+                    layout="responsive"
+                    width={100}
+                    height={100}
+                    src={img}
+                    objectFit="contain"
+                  />
+                </Box>
+
+                <MotionText
+                  fontSize="lg"
+                  fontWeight="bold"
+                  wordBreak="keep-all"
+                  textAlign="center"
+                  variants={text}
+                >
+                  {item.title}
+                </MotionText>
+              </Stack>
+
+              {index !== processSectionBody.length - 1 && (
+                <Box position="relative" top="10">
+                  <ChevronRightIcon boxSize="10" color="gray.700" />
+                </Box>
+              )}
+            </Fragment>
+          );
+        })}
+      </Stack>
+    </motion.div>
   );
 }
 
