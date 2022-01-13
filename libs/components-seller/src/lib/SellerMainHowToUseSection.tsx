e86dc530557455e81edaa77c45f3336f97da7284
Jiyeon <a1919361@gmail.com>
2022-01-13 11:11:53 +0900

    [fix] 이용방법 부분 방송인센터와 통일

--- libs/components-seller/src/lib/SellerMainHowToUseSection.tsx
+++ libs/components-seller/src/lib/SellerMainHowToUseSection.tsx
@@ -1,14 +1,27 @@
-import { Box, Stack, Text } from '@chakra-ui/react';
+import { Box, Flex, Stack, Text } from '@chakra-ui/react';
 import {
   howToUseSectionBody,
   SectionData,
   sellerMainSectionText,
 } from '@project-lc/components-constants/sellerMainText';
 import { ChakraNextImage } from '@project-lc/components-core/ChakraNextImage';
-import { useDisplaySize } from '@project-lc/hooks';
-import { AnimatePresence, motion } from 'framer-motion';
+import { MotionBox } from '@project-lc/components-core/MotionBox';
+import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import { SellerMainSectionContainer } from './SellerMainFeatureSection';
+
+const boxVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { staggerChildren: 0.3 } },
+};
+const itemVariants = {
+  hidden: { opacity: 0, y: -20 },
+  visible: { opacity: 1, y: 0 },
+};
+const previewVariants = {
+  hidden: { opacity: 0, y: 20, transition: { duration: 0.5 } },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
 
 /** 이용방법 이미지 영역 */
 export function SellerMainHowToUseImage({
@@ -16,44 +29,36 @@ export function SellerMainHowToUseImage({
 }: {
   selectedItem: SectionData;
 }): JSX.Element {
-  const { isMobileSize } = useDisplaySize(); // 750px 기점
   return (
-    <AnimatePresence exitBeforeEnter>
-      <motion.div
-        style={{ display: 'inline-block', minWidth: isMobileSize ? '100%' : '70%' }}
-        key={selectedItem.title || ''}
-        animate={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -10 }}
-        exit={{ opacity: 0, y: 10 }}
-        transition={{ duration: 0.15 }}
+    <AnimatePresence>
+      <Flex
+        colSpan={{ base: 3, lg: 2 }}
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        flex={{ base: 1, lg: 2 }}
       >
-        {selectedItem.img && (
-          <Box position="relative" maxW={{ base: '658px', md: '815px' }}>
+        <MotionBox
+          key={selectedItem.title || ''}
+          viewport={{ amount: 0.6, once: true }}
+          initial="hidden"
+          whileInView="visible"
+          variants={previewVariants}
+        >
+          {selectedItem.img && (
             <ChakraNextImage
-              layout="responsive"
+              src={selectedItem.img}
+              quality={100}
               width={685}
               height={400}
-              src={selectedItem.img}
-              objectFit="contain"
-              quality={100}
             />
-          </Box>
-        )}
-      </motion.div>
+          )}
+        </MotionBox>
+      </Flex>
     </AnimatePresence>
   );
 }
 
-const variants = {
-  selected: {
-    color: 'var(--chakra-colors-whiteAlpha-900)',
-    backgroundColor: 'var(--chakra-colors-blue-500)',
-    transition: {
-      default: { duration: 0.2 },
-    },
-  },
-  notSelected: {},
-};
 /** 이용방법 아이템 텍스트 상자 */
 export function SellerMainHowToUseItem({
   item,
@@ -67,22 +72,39 @@ export function SellerMainHowToUseItem({
   const { title, desc } = item;
   if (!title || !desc) return null;
   return (
-    <Box cursor="pointer" _hover={{ color: 'blue.500' }}>
-      <motion.div
-        onClick={onClick}
-        style={{ borderRadius: '1rem', padding: '1rem' }}
-        variants={variants}
-        initial="notSelected"
-        animate={selected ? 'selected' : 'notSelected'}
+    <MotionBox
+      onClick={onClick}
+      cursor="pointer"
+      variants={itemVariants}
+      whileHover={{ scale: 1.04 }}
+      whileTap={{ scale: 0.99 }}
+      display="flex"
+      alignItems="center"
+      gap={2}
+    >
+      <Box
+        width="12px"
+        height="12px"
+        bgColor="blue.500"
+        borderRadius="full"
+        boxShadow={selected ? '0 0 0 0.2rem rgb(80, 120, 255, 0.5)' : undefined}
+        transition="box-shadow ease-out 0.5s"
+      />
+      <Box
+        minH={20}
+        flex={1}
+        px={4}
+        py={2}
+        borderRadius="xl"
+        sx={{ transition: 'background-color 0.35s' }}
+        bgColor={selected ? 'blue.500' : 'unset'}
+        color={selected ? 'white' : 'unset'}
+        shadow={selected ? 'xl' : 'unset'}
       >
-        <Text fontWeight="bold" fontSize={{ base: 'lg', md: 'xl' }}>
-          {title}
-        </Text>
-        <Text fontSize={{ base: 'sm', md: 'md' }} wordBreak="keep-all">
-          {desc}
-        </Text>
-      </motion.div>
-    </Box>
+        <Text fontWeight="bold">{item.title}</Text>
+        <Text fontWeight="medium">{item.desc}</Text>
+      </Box>
+    </MotionBox>
   );
 }
 
@@ -91,28 +113,38 @@ export function SellerMainHowToUseSection(): JSX.Element {
   const [selectedItem, setSelectedItem] = useState<SectionData>(howToUseSectionBody[0]);
   return (
     <SellerMainSectionContainer sectionData={sellerMainSectionText.howToUse} hasGrayBg>
-      <Stack
-        spacing={8}
-        alignItems="center"
-        direction={{ base: 'column', md: 'row-reverse' }}
-        pl={{ base: 0, md: 4 }}
-        py={8}
+      <Flex
+        maxW="6xl"
+        margin="auto"
+        my={12}
+        gap={8}
+        justify="space-between"
+        alignItems={{ base: 'center', lg: 'center' }}
+        flexDir={{ base: 'column-reverse', lg: 'row' }}
       >
+        {/* 텍스트 상자 영역 */}
+        <MotionBox
+          variants={boxVariants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 0.6, once: true }}
+          flex={1}
+        >
+          <Stack spacing={4}>
+            {howToUseSectionBody.map((item) => (
+              <SellerMainHowToUseItem
+                key={item.title}
+                item={item}
+                selected={item === selectedItem}
+                onClick={() => setSelectedItem(item)}
+              />
+            ))}
+          </Stack>
+        </MotionBox>
+
         {/* gif 이미지 영역 */}
         <SellerMainHowToUseImage selectedItem={selectedItem} />
-
-        {/* 텍스트 상자 영역 */}
-        <Stack overflow="hidden" spacing={0}>
-          {howToUseSectionBody.map((item) => (
-            <SellerMainHowToUseItem
-              key={item.title}
-              item={item}
-              selected={item === selectedItem}
-              onClick={() => setSelectedItem(item)}
-            />
-          ))}
-        </Stack>
-      </Stack>
+      </Flex>
     </SellerMainSectionContainer>
   );
 }
