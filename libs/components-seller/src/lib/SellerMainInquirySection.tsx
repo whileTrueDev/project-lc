a230d2f2b87777a8276b9f02082b07dc9719463f
Jiyeon <a1919361@gmail.com>
2022-01-13 13:39:14 +0900

    [fix] 문의하기 다이얼로그 적용

--- libs/components-seller/src/lib/SellerMainInquirySection.tsx
+++ libs/components-seller/src/lib/SellerMainInquirySection.tsx
@@ -1,14 +1,7 @@
-import {
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalOverlay,
-  useDisclosure,
-} from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import { useDisclosure } from '@chakra-ui/react';
+import { InquiryDialog } from '@project-lc/components-shared/InquiryForm';
 import { KkshowTogether } from '@project-lc/components-shared/KkshowTogether';
-import { InquiryForm } from '@project-lc/components-shared/InquiryForm';
+import { useRouter } from 'next/router';
 
 export function SellerMainInquirySection(): JSX.Element {
   const router = useRouter();
@@ -23,19 +16,7 @@ export function SellerMainInquirySection(): JSX.Element {
         ]}
       />
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalCloseButton />
-          <ModalBody>
-            {/*
-             // TODO: 문의사항 다이얼로그 디자인 적용 
-             // TODO: 문의하기 성공 콜백 onClose전달할 수  있도록 수정필요
-            */}
-            <InquiryForm type="seller" />
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <InquiryDialog isOpen={isOpen} onClose={onClose} type="seller" />
     </>
   );
 }
