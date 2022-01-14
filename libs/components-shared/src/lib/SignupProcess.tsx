de217fbc6fb80ef213b94375c2916c4ca6bded0f
Jiyeon <a1919361@gmail.com>
2022-01-14 16:23:23 +0900

    [feat] 회원가입 과정에 개인정보이용약관 동의 추가

--- libs/components-shared/src/lib/SignupProcess.tsx
+++ libs/components-shared/src/lib/SignupProcess.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import { UserType } from '@project-lc/shared-types';
 import { useState } from 'react';
+import SignupAgreeTerms from './signup/SignupAgreeTerms';
 import { SignupForm } from './signup/SignupForm';
 import SignupStart from './signup/SignupStart';
 
@@ -13,8 +14,16 @@ export function SignupProcess({ appType }: SignupProcessProps): JSX.Element {
     <Flex align="center" justify="center" minH="calc(100vh - 200px)">
       {/* 크크쇼 시작하기 첫화면 */}
       {step === 0 && <SignupStart userType={appType} moveToNext={() => setStep(1)} />}
+      {/* 이용동의 화면 */}
+      {step === 1 && (
+        <SignupAgreeTerms
+          userType={appType}
+          moveToNext={() => setStep(2)}
+          moveToPrev={() => setStep(0)}
+        />
+      )}
       {/* 이메일, 비밀번호 입력 폼 */}
-      {step === 1 && <SignupForm userType={appType} moveToPrev={() => setStep(0)} />}
+      {step === 2 && <SignupForm userType={appType} moveToPrev={() => setStep(1)} />}
     </Flex>
   );
 }
