289874b52b1dc835afde4191b8c9b67fee754659
Jiyeon <a1919361@gmail.com>
2022-01-17 14:59:09 +0900

    [feat] 마이페이지 사이드바 추가(데스크탑 사이즈일때)

--- libs/components-shared/src/lib/MypageLayout.tsx
+++ libs/components-shared/src/lib/MypageLayout.tsx
@@ -1,15 +1,52 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, useColorModeValue, useDisclosure } from '@chakra-ui/react';
 import { MypageLink, mypageNavLinks } from '@project-lc/components-constants/navigation';
 import LoginRequireAlertDialog from '@project-lc/components-core/LoginRequireAlertDialog';
+import MotionBox from '@project-lc/components-core/MotionBox';
 import FullscreenLoading from '@project-lc/components-layout/FullscreenLoading';
 import MypageFooter from '@project-lc/components-layout/MypageFooter';
-import { useIsLoggedIn } from '@project-lc/hooks';
+import { useDisplaySize, useIsLoggedIn } from '@project-lc/hooks';
 import { UserType } from '@project-lc/shared-types';
 import React from 'react';
-import MypageBreadcrumb from './MypageBreadCrumb';
 import { FloatingHelpButton } from './FloatingHelpButton';
+import MypageBreadcrumb from './MypageBreadCrumb';
 import { MypageNavbar } from './MypageNavbar';
-import { Navbar } from './Navbar';
+import { Navbar, NavbarToggleButton } from './Navbar';
+
+export function DesktopMypageSidebar({
+  navLinks,
+}: {
+  navLinks: Array<MypageLink>;
+}): JSX.Element {
+  const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: true });
+  const borderColor = useColorModeValue('gray.200', 'gray.900');
+  return (
+    <>
+      <MotionBox
+        initial={isOpen ? 'closed' : 'open'}
+        animate={isOpen ? 'closed' : 'open'}
+        custom={40}
+        variants={variants}
+        position="absolute"
+        left={0}
+        top={0}
+      >
+        <NavbarToggleButton onToggle={onToggle} isOpen={isOpen} />
+      </MotionBox>
+      <MotionBox
+        initial={isOpen ? 'open' : 'closed'}
+        animate={isOpen ? 'open' : 'closed'}
+        variants={variants}
+        custom={200}
+        borderRight={1}
+        borderStyle="solid"
+        borderColor={borderColor}
+      >
+        <NavbarToggleButton onToggle={onToggle} isOpen={isOpen} />
+        <MypageNavbar navLinks={navLinks} />
+      </MotionBox>
+    </>
+  );
+}
 
 interface MypageLayoutProps {
   children: React.ReactNode;
@@ -17,21 +54,58 @@ interface MypageLayoutProps {
   navLinks?: Array<MypageLink>;
 }
 
+const variants = {
+  open: (width: number) => ({
+    opacity: 1,
+    width,
+    display: 'block',
+    transition: {
+      default: { type: 'spring', bounce: 0, duration: 0.2 },
+    },
+  }),
+  closed: {
+    opacity: 0,
+    width: 0,
+    transition: {
+      default: { type: 'spring', bounce: 0, duration: 0.2 },
+    },
+    transitionEnd: { display: 'none' },
+  },
+};
+
 export function MypageLayout({
   children,
   appType = 'seller',
   navLinks = mypageNavLinks,
 }: MypageLayoutProps): JSX.Element {
   const { status } = useIsLoggedIn();
+  const { isMobileSize } = useDisplaySize();
 
   return (
-    <Box position="relative" pointerEvents={status === 'loading' ? 'none' : 'auto'}>
+    <Box
+      pointerEvents={status === 'loading' ? 'none' : 'auto'}
+      maxH="100vh"
+      overflowY="hidden"
+    >
       <Navbar appType={appType} />
-      <MypageNavbar navLinks={navLinks} />
-      <MypageBreadcrumb />
-      <Box as="main" minH="calc(100vh - 60px - 60px - 60px)">
-        {children}
-      </Box>
+
+      <Flex direction="row" position="relative">
+        {/* 모바일화면이 아닌경우 사이드바 표시 */}
+        {!isMobileSize && <DesktopMypageSidebar navLinks={navLinks} />}
+
+        <Box
+          as="main"
+          // 모바일 사이즈일때는 푸터 display : none임
+          h={{ base: 'calc(100vh - 67px)', md: 'calc(100vh - 67px - 60px)' }}
+          flex={1}
+          overflow="auto"
+        >
+          {isMobileSize && <MypageBreadcrumb />}
+
+          {children}
+        </Box>
+      </Flex>
+
       <MypageFooter />
       {/* 전체화면 로딩 */}
       {status === 'loading' && <FullscreenLoading />}
