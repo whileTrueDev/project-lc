01814976fe9b173ae9ff2ea5396f819a387774b4
leejineun <43170606+leejineun@users.noreply.github.com>
2022-01-20 11:06:39 +0900

    feat: 스크롤바 css 적용

--- libs/utils-frontend/src/lib/createChakraTheme.ts
+++ libs/utils-frontend/src/lib/createChakraTheme.ts
@@ -22,6 +22,24 @@ export const createChakraTheme = () =>
           color: mode('gray.800', 'whiteAlpha.900')(props),
           bg: mode('white', 'gray.800')(props),
         },
+        // 스크롤바 영역전체
+        '::-webkit-scrollbar': {
+          width: '6px', // vertical
+          height: '9px', // horizontal
+          opacity: 0.7,
+        },
+        // 스크롤바 바 없는 부분
+        '::-webkit-scrollbar-track': {
+          opacity: 0.7,
+        },
+        // 스크롤바 바 부분
+        '::-webkit-scrollbar-thumb': {
+          backgroundColor: 'gray',
+          borderRadius: '26px',
+        },
+        '::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: mode('gray.600', 'gray.300')(props),
+        },
       }),
     },
     fonts: {
