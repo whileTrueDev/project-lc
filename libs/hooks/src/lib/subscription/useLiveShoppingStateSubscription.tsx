562735c361c63d218b4b137b930919803abff82a
Jiyeon <a1919361@gmail.com>
2022-01-25 14:46:28 +0900

    [refactor] 알림소리 훅 이동

--- libs/hooks/src/lib/subscription/useLiveShoppingStateSubscription.tsx
+++ libs/hooks/src/lib/subscription/useLiveShoppingStateSubscription.tsx
@@ -4,11 +4,38 @@ import {
   LiveShoppingStateServerToClientEvents,
 } from '@project-lc/shared-types';
 import { getRealtimeApiHost } from '@project-lc/utils';
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { useQueryClient } from 'react-query';
 import { io, Socket } from 'socket.io-client';
 import axiosInstance from '../../axios';
-import { useAlarmAudio } from '../useLiveShoppingStateBoardAlertState';
+
+export function useAlarmAudio(): {
+  audio: HTMLAudioElement | null;
+  playAudio: () => void;
+} {
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+
+  // node환경에서는 audio 객체 없어서 오류남. 브라우저 환경에 마운트 된 후 audio 생성하도록 하였음
+  useEffect(() => {
+    setAudio(new Audio('/audio/fever.mp3'));
+  }, []);
+
+  const playAudio = useCallback(() => {
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.play();
+    }
+  }, [audio]);
+  return { audio, playAudio };
+}
 
 type UseLiveShoppingStateSubscriptionReturn = {
   message: string;
@@ -16,9 +43,13 @@ type UseLiveShoppingStateSubscriptionReturn = {
   setAlert: Dispatch<SetStateAction<boolean>>;
 };
 
+/** 방송인 현황판 실시간 메시지, 알림 위한 소켓연결
+ * @return message:string 방송인 현황판으로 보내진 관리자 메시지
+ * @return alert:boolean 알림 발생한 경우 true
+ * @return setAlert 알림 애니메이션 종료 후 알림 off 하기위한 함수
+ */
 export const useLiveShoppingStateSubscription = (
   liveShoppingId: number,
-  onCreated?: (liveShoppingId: number) => void,
 ): UseLiveShoppingStateSubscriptionReturn => {
   const [message, setMessage] = useState<string>('');
   const [alert, setAlert] = useState<boolean>(false);
@@ -29,6 +60,8 @@ export const useLiveShoppingStateSubscription = (
     >();
 
   const { playAudio } = useAlarmAudio();
+
+  // * 알림 발생한경우 소리냄
   useEffect(() => {
     if (alert) playAudio();
   }, [alert, playAudio]);
@@ -63,7 +96,7 @@ export const useLiveShoppingStateSubscription = (
       _client.off('purchaseMessageUpdated');
       _client.close();
     };
-  }, [client, liveShoppingId, onCreated, queryClient]);
+  }, [client, liveShoppingId, queryClient]);
 
   return { message, alert, setAlert };
 };
