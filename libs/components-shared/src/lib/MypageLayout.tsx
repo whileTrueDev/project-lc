d63dce4513e136b0bf871b43fdeaecfdca4123b4
Jiyeon <a1919361@gmail.com>
2022-01-26 14:57:54 +0900

    [fix] 마이페이지 레이아웃 불필요한 스크롤바 생기는 문제 해결

--- libs/components-shared/src/lib/MypageLayout.tsx
+++ libs/components-shared/src/lib/MypageLayout.tsx
@@ -30,32 +30,41 @@ export function MypageLayout({
   return (
     <Flex
       direction="column"
-      maxH="100vh"
-      overflowY="hidden"
+      height="100vh"
       pointerEvents={status === 'loading' ? 'none' : 'auto'}
     >
       {/* 상단 네비바 */}
       <Navbar appType={appType} />
 
       {/* 사이드바 제외한 영역 */}
-      <Flex h={`calc(100vh - ${NAVBAR_HEIGHT}px)`}>
+      <Flex height="100%" overflow="hidden" position="relative">
         {/* 사이드바 영역 */}
         {!isMobileSize && <DesktopMypageSidebar navLinks={navLinks} />}
 
         {/* 중간 영역 - 메인패널 */}
-        <Box flex={1} as="main" overflow="auto">
-          {/* 마이페이지 메인 컨텐츠 영역 = 스크롤되는 영역
-          (전체높이 - 네비바높이 - 푸터높이)를 최소높이로 하여 푸터가 항상 하단에 위치하도록 함 */}
-          <Box minH={`calc(100vh - ${NAVBAR_HEIGHT}px - ${FOOTER_HEIGHT}px)`}>
-            {/* 브레드크럼 네비게이션 표시 */}
-            <MypageBreadcrumb />
-            {/* 실제 메인 컨텐츠 표시 */}
-            {children}
+        <Flex
+          direction="column"
+          overflow="hidden"
+          position="relative"
+          flexGrow="1"
+          height="100%"
+          width="100%"
+        >
+          {/* 마이페이지 메인 컨텐츠 영역 = 스크롤되는 영역 */}
+          <Box className="scroll-content" maxHeight="inherit" overflowY="scroll">
+            {/* 메인컨텐츠 wrapper(푸터제외 영역) */}
+            <Box
+              className="content-wrapper"
+              overflowY="hidden"
+              minHeight={`calc(100vh - ${NAVBAR_HEIGHT}px - ${FOOTER_HEIGHT}px)`}
+            >
+              <MypageBreadcrumb />
+              {children}
+            </Box>
+            {/* 하단 푸터 */}
+            <MypageFooter />
           </Box>
-
-          {/* 하단 푸터 */}
-          <MypageFooter />
-        </Box>
+        </Flex>
       </Flex>
 
       {/* 전체화면 로딩 */}
