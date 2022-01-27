2bfab6c9141037600cca77b1dc620c3c48851a17
hwasurr <iamsupermazinga@gmail.com>
2022-01-27 17:15:40 +0900

    fix: kkshow 페이지 로고 크기 수정 및 이미지 불러오지 못하는 현상 수정

--- apps/web-kkshow/pages/index.tsx
+++ apps/web-kkshow/pages/index.tsx
@@ -1,15 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import { kkshowFooterLinkList } from '@project-lc/components-constants/footerLinks';
 import { CommonFooter } from '@project-lc/components-layout/CommonFooter';
+import { KkshowTogether } from '@project-lc/components-shared/KkshowTogether';
 import {
   SellerMainBetaDesktop,
   SellerMainBetaMobile,
 } from '@project-lc/components-shared/MainBeta';
 import { KkshowNavbar } from '@project-lc/components-web-kkshow/KkshowNavbar';
-
-import { KkshowTogether } from '@project-lc/components-shared/KkshowTogether';
 import { useDisplaySize } from '@project-lc/hooks';
-
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
