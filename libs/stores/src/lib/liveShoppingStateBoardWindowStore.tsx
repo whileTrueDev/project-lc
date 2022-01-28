4fcdbc64443f2fd49088ff7a7eb1d128a93ac4eb
Jiyeon <a1919361@gmail.com>
2022-01-28 11:31:16 +0900

    [fix] 현황판 store 수정(창 하나만 저장)

--- libs/stores/src/lib/liveShoppingStateBoardWindowStore.tsx
+++ libs/stores/src/lib/liveShoppingStateBoardWindowStore.tsx
@@ -1,22 +1,26 @@
 import create from 'zustand';
 
 export interface LiveShoppingStateBoardWindowStore {
-  windows: Window[];
-  addWindow: (w: Window) => void;
-  closeWindows: () => void;
+  _window: Window | null;
+  openWindow: (url: string, target: string, features: string) => void;
+  closeWindow: () => void;
 }
 
 export const liveShoppingStateBoardWindowStore =
   create<LiveShoppingStateBoardWindowStore>((set, get) => ({
-    windows: [],
-    addWindow: (w: Window) => {
-      set((state) => ({
-        ...state,
-        windows: [...state.windows, w],
-      }));
+    _window: null,
+    openWindow: (url: string, target: string, features: string) => {
+      const { _window } = get();
+      if (_window) {
+        _window.location.href = url;
+        _window.focus();
+      } else {
+        const w = window.open(url, target, features);
+        if (w) set((state) => ({ ...state, _window: w }));
+      }
     },
-    closeWindows: () => {
-      const { windows } = get();
-      windows.forEach((w) => w.close());
+    closeWindow: () => {
+      const { _window } = get();
+      if (_window) _window.close();
     },
   }));
