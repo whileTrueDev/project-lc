06d166f49d115982b0a1fbee1e7e4025efde1c8a
Jiyeon <a1919361@gmail.com>
2022-02-08 11:45:21 +0900

    [feat] urlCard 컴포넌트 활용하여 상품홍보페이지url 컴포넌트 생성

---
+++ libs/components-web-bc/src/lib/PromotionPageUrlCard.tsx
@@ -0,0 +1,51 @@
+import { useToast } from '@chakra-ui/react';
+import { useProfile } from '@project-lc/hooks';
+import { useState } from 'react';
+import { UrlCard } from './OverlayUrlCard';
+
+/** useProfile 에서 "상품홍보페이지url" 값을 가져와서 표시하고, 클립보드에 복사하는 컴포넌트
+ * profileData.BroadcasterPromotionPage 값이 없는경우 아무것도 표시되지 않는다
+ */
+export function PromotionPageUrlCard(): JSX.Element {
+  const toast = useToast();
+  const { data: profileData } = useProfile();
+
+  // 오버레이 주소 10초간만 보여주기 위한 기본값
+  const DEFAULT_URL = '[URL복사] 버튼을 눌러주세요.';
+  const [urlValue, setUrlValue] = useState<string>(DEFAULT_URL);
+
+  // 10초간 overlayUrl을 보여주는 함수
+  const handleShowUrl = (): void => {
+    if (
+      !profileData?.BroadcasterPromotionPage ||
+      profileData?.BroadcasterPromotionPage.url === urlValue
+    ) {
+      return;
+    }
+
+    const { url } = profileData.BroadcasterPromotionPage;
+    if (!url) return;
+    setUrlValue(url);
+
+    // 클립보드 복사
+    navigator.clipboard.writeText(url);
+
+    toast({ title: '복사되었습니다.', status: 'success' });
+
+    setTimeout(() => {
+      setUrlValue(DEFAULT_URL);
+    }, 8 * 1000);
+  };
+
+  if (!profileData?.BroadcasterPromotionPage) return <></>;
+
+  return (
+    <UrlCard
+      label="상품 홍보 페이지 URL"
+      inputValue={!profileData?.agreementFlag ? '이용 동의가 필요합니다.' : urlValue}
+      inputDisabled={!urlValue}
+      buttonDisabled={!profileData?.agreementFlag}
+      buttonHandler={handleShowUrl}
+    />
+  );
+}
