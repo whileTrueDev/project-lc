bb34f432cf4ee84051a488c7ca21a39b999c1116
Jiyeon <a1919361@gmail.com>
2022-02-09 17:53:21 +0900

    [feat] 상품홍보 생성dto 작성

--- libs/shared-types/src/lib/dto/broadcasterProductPromotion.dto.ts
+++ libs/shared-types/src/lib/dto/broadcasterProductPromotion.dto.ts
@@ -1,5 +1,32 @@
-import { IsInt } from 'class-validator';
+import { IsInt, IsNumber, IsOptional } from 'class-validator';
 
 export class broadcasterProductPromotionDto {
   @IsInt() fmGoodsSeq: number;
+}
+
+/**  방송홍보 레코드 생성 dto */
+export class CreateProductPromotionDto {
+  /** 상품홍보페이지 id */
+  @IsNumber()
+  broadcasterPromotionPageId: number;
+
+  /** project-lc 상품 고유번호 */
+  @IsNumber()
+  goodsId: number;
+
+  /** 퍼스트몰 상품 고유번호
+   * => ProductPromotion.fmGoodsSeq 는
+   * GoodsConfirmation.firstmallGoodsConnectionId 와
+   * LiveShopping.fmGoodsSeq 와 중복되면 안됨
+   * */
+  @IsNumber()
+  fmGoodsSeq: number;
+
+  @IsNumber()
+  @IsOptional()
+  broadcasterCommissionRate: number;
+
+  @IsNumber()
+  @IsOptional()
+  whiletrueCommissionRate: number;
 }
