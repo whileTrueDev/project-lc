3b884c0e692e254f913e86c8208d3f984be8a015
Jiyeon <a1919361@gmail.com>
2022-02-10 11:01:02 +0900

    [feat] 관리자용 중복페이지url, 중복 상품번호 조회 쿼리 훅 작성

---
+++ libs/hooks/src/lib/queries/useAdminDuplicatePromotionPageFlag.tsx
@@ -0,0 +1,43 @@
+import { AxiosError } from 'axios';
+import { useQuery, UseQueryResult } from 'react-query';
+import axios from '../../axios';
+
+export type DuplicateFlag = boolean;
+
+/** 방송인 상품홍보페이지 url 중복인지 확인 */
+export const getAdminDuplicatePromotionPageFlag = async (
+  url: string,
+): Promise<DuplicateFlag> => {
+  return axios
+    .get<DuplicateFlag>('/admin/promotion-page/duplicate', { params: { url } })
+    .then((res) => res.data);
+};
+
+export const useAdminDuplicatePromotionPageFlag = (
+  url: string,
+): UseQueryResult<DuplicateFlag, AxiosError> => {
+  return useQuery<DuplicateFlag, AxiosError>(
+    'AdminDuplicatePromotionPage',
+    () => getAdminDuplicatePromotionPageFlag(url),
+    { enabled: !!url },
+  );
+};
+
+/** 상품홍보에 등록할 fmGoodsSeq이 중복인지 확인 */
+export const getAdminDuplicateFmGoodsSeqFlagForProductPromotion = async (
+  fmGoodsSeq: number,
+): Promise<DuplicateFlag> => {
+  return axios
+    .get<DuplicateFlag>('/admin/promotion-page/duplicate', { params: { fmGoodsSeq } })
+    .then((res) => res.data);
+};
+
+export const useAdminDuplicateFmGoodsSeqFlagForProductPromotion = (
+  fmGoodsSeq: number,
+): UseQueryResult<DuplicateFlag, AxiosError> => {
+  return useQuery<DuplicateFlag, AxiosError>(
+    'AdminDuplicatePromotionPage',
+    () => getAdminDuplicateFmGoodsSeqFlagForProductPromotion(fmGoodsSeq),
+    { enabled: !!fmGoodsSeq },
+  );
+};
