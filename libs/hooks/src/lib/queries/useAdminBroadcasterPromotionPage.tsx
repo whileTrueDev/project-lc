bce40fb502112a87a77df846f7bbc2d837ed8ac4
Jiyeon <a1919361@gmail.com>
2022-02-10 11:00:26 +0900

    [feat] 방송인 상품홍보페이지 조회 쿼리 훅 작성

---
+++ libs/hooks/src/lib/queries/useAdminBroadcasterPromotionPage.tsx
@@ -0,0 +1,23 @@
+import { BroadcasterPromotionPageListRes } from '@project-lc/shared-types';
+import { AxiosError } from 'axios';
+import { useQuery, UseQueryResult } from 'react-query';
+import axios from '../../axios';
+
+export type AdminBroadcasterPromotionPage = BroadcasterPromotionPageListRes;
+
+export const getAdminBroadcasterPromotionPage =
+  async (): Promise<AdminBroadcasterPromotionPage> => {
+    return axios
+      .get<AdminBroadcasterPromotionPage>('/admin.promotion-pages')
+      .then((res) => res.data);
+  };
+
+export const useAdminBroadcasterPromotionPage = (): UseQueryResult<
+  AdminBroadcasterPromotionPage,
+  AxiosError
+> => {
+  return useQuery<AdminBroadcasterPromotionPage, AxiosError>(
+    'AdminBroadcasterPromotionPageList',
+    getAdminBroadcasterPromotionPage,
+  );
+};
