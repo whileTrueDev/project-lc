4cde7554abde17f2f934fad1817c6e32df0f8c14
hwasurr <iamsupermazinga@gmail.com>
2022-02-10 16:08:13 +0900

    fix: 마이그레이션파일 정리

--- libs/prisma-orm/prisma/migrations/20220207072059_/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220207072059_/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `BroadcasterSettlementItems` ADD COLUMN `sellType` ENUM('liveShopping', 'broadcasterPromotionPage') NULL;
+ALTER TABLE `BroadcasterSettlementItems` ADD COLUMN `sellType` ENUM('liveShopping', 'broadcasterPromotionPage', 'normal') NULL;
 
 -- AlterTable
-ALTER TABLE `SellerSettlementItems` ADD COLUMN `sellType` ENUM('liveShopping', 'broadcasterPromotionPage') NULL;
+ALTER TABLE `SellerSettlementItems` ADD COLUMN `sellType` ENUM('liveShopping', 'broadcasterPromotionPage', 'normal') NULL;
--- libs/prisma-orm/prisma/migrations/20220209050328_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `BroadcasterSettlementItems` MODIFY `sellType` ENUM('liveShopping', 'broadcasterPromotionPage', 'normal') NULL;
-
--- AlterTable
-ALTER TABLE `SellerSettlementItems` MODIFY `sellType` ENUM('liveShopping', 'broadcasterPromotionPage', 'normal') NULL;
