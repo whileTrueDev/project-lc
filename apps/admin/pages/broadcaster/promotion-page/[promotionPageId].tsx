e222f3573f6589f3a20e2deef59fbd8f0e0aca06
Jiyeon <a1919361@gmail.com>
2022-02-10 16:42:21 +0900

    [feat] ui 상품홍보페이지 삭제 모달 생성

--- apps/admin/pages/broadcaster/promotion-page/[promotionPageId].tsx
+++ apps/admin/pages/broadcaster/promotion-page/[promotionPageId].tsx
@@ -1,21 +1,49 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { Box, Button, Heading, Link, Stack, Text } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Button,
+  Heading,
+  Link,
+  Stack,
+  Text,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react';
 import AdminPageLayout from '@project-lc/components-admin/AdminPageLayout';
 import { boxStyle } from '@project-lc/components-constants/commonStyleProps';
+import { ConfirmDialog } from '@project-lc/components-core/ConfirmDialog';
 import {
   useAdminBroadcasterPromotionPage,
+  useAdminBroadcasterPromotionPageDeleteMutation,
   useAdminProductPromotion,
 } from '@project-lc/hooks';
 import { useRouter } from 'next/router';
 import { useCallback } from 'react';
 
 export function BroadcasterPromotionPageDetail(): JSX.Element {
+  const toast = useToast();
   const router = useRouter();
   const goBackToList = useCallback(() => {
     router.push('/broadcaster/promotion-page');
   }, [router]);
   const promotionPageId = Number(router.query.promotionPageId);
 
+  // 페이지 삭제
+  const deleteDialog = useDisclosure();
+  const deleteRequest = useAdminBroadcasterPromotionPageDeleteMutation();
+  const onDeleteDialogConfirm = useCallback(async () => {
+    deleteRequest.mutateAsync({ pageId: promotionPageId }).then((res) => {
+      deleteDialog.onClose();
+      toast({ title: '해당 방송인 상품홍보페이지를 삭제하였습니다', status: 'success' });
+      goBackToList();
+    });
+  }, [deleteDialog, deleteRequest, goBackToList, promotionPageId, toast]);
+
+  // 데이터
   const { data: listData, isLoading } = useAdminBroadcasterPromotionPage();
   const { data: productPromotionList } = useAdminProductPromotion(promotionPageId);
 
@@ -36,9 +64,15 @@ export function BroadcasterPromotionPageDetail(): JSX.Element {
     <AdminPageLayout>
       <Box position="relative">
         <Button onClick={goBackToList}>돌아가기</Button>
+
         <Heading mt={4} size="lg">
           {userNickname}의 상품홍보페이지
         </Heading>
+
+        <Stack direction="row">
+          <Button onClick={deleteDialog.onOpen}>삭제</Button>
+          <Button>수정</Button>
+        </Stack>
 
         <Stack direction="row">
           <Text>페이지 id : </Text>
@@ -81,6 +115,24 @@ export function BroadcasterPromotionPageDetail(): JSX.Element {
           )}
         </Stack>
       </Box>
+
+      {/* 상품홍보페이지 삭제 모달창 */}
+      <ConfirmDialog
+        title="상품홍보페이지 정보 삭제"
+        isOpen={deleteDialog.isOpen}
+        onClose={deleteDialog.onClose}
+        onConfirm={onDeleteDialogConfirm}
+      >
+        <Alert status="error">
+          <AlertIcon />
+          <Stack>
+            <AlertTitle mr={2}>
+              {userNickname}의 상품홍보페이지 정보를 삭제하시겠습니까?
+            </AlertTitle>
+            <AlertDescription>홍보중인 상품 정보 또한 모두 삭제됩니다</AlertDescription>
+          </Stack>
+        </Alert>
+      </ConfirmDialog>
     </AdminPageLayout>
   );
 }
