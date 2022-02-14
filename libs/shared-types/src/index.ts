e44ea70b7e4766b2f04882e41b2e344aa81d3750
hwasurr <iamsupermazinga@gmail.com>
2022-02-14 10:57:19 +0900

    feat(api): csrf protection

--- libs/shared-types/src/index.ts
+++ libs/shared-types/src/index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 export * from './lib/constants/banks';
 export * from './lib/constants/broadcasterContacts';
+export * from './lib/constants/csrfkey';
 export * from './lib/constants/fmDeliveryCompanies';
 export * from './lib/constants/fmExportConfirmStatuses';
 export * from './lib/constants/fmOrderPayments';
