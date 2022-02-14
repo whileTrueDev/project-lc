5a475f8b976c9b385e98f35c5b39a4f57a2178df
hwasurr <iamsupermazinga@gmail.com>
2022-02-14 16:58:37 +0900

    fix: 잘못된 캐시 로직로 인해 올바르게 데이터를 불러오지 못하는 현상 수정2

--- libs/nest-core/src/lib/interceptors/http-cache.interceptor.ts
+++ libs/nest-core/src/lib/interceptors/http-cache.interceptor.ts
@@ -73,7 +73,7 @@ export class HttpCacheInterceptor extends CacheInterceptor {
           : ttlValueOrFactory;
       return next.handle().pipe(
         tap((response) => {
-          if (isNil(response)) {
+          if (!isNil(response)) {
             const args = isNil(ttl) ? [key, response] : [key, response, { ttl }];
             this.cacheManager.set(...args);
           }
