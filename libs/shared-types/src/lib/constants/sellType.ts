f97312b34f5c135774a1f9acc6f7e09d3e22a368
hwasurr <iamsupermazinga@gmail.com>
2022-02-14 15:46:34 +0900

    fix: SellType 변경사항 적용

--- libs/shared-types/src/lib/constants/sellType.ts
+++ libs/shared-types/src/lib/constants/sellType.ts
@@ -4,6 +4,7 @@ export const convertSellTypeToString = (
   sellType?: SellType | 'normal' | null,
 ): string => {
   if (sellType === 'liveShopping') return '라이브쇼핑';
-  if (sellType === 'broadcasterPromotionPage') return '상품홍보';
+  if (sellType === 'productPromotion') return '상품홍보';
+  if (sellType === 'normal') return '기본판매';
   return '기본판매';
 };
