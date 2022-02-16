09f6682969f6a3de765fbee7e0d126e988a732a0
Jiyeon <a1919361@gmail.com>
2022-02-16 16:53:25 +0900

    [refactor] 방송인 정산등록 개인정보 이용방침 부분 수정

--- libs/components-core/src/lib/TermBox.tsx
+++ libs/components-core/src/lib/TermBox.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@chakra-ui/react';
+import { Box, BoxProps } from '@chakra-ui/react';
 import { boxStyle } from '@project-lc/components-constants/commonStyleProps';
+import 'suneditor/dist/css/suneditor.min.css';
 
 /** 이용약관 등 긴 문구를 스크롤되는 박스 안에 표시하는 컴포넌트 */
 export default function TermBox({ text }: { text: string }): JSX.Element {
@@ -14,5 +15,17 @@ export default function TermBox({ text }: { text: string }): JSX.Element {
     >
       {text}
     </Box>
+  );
+}
+
+/** html string 형태의 텍스트 표시하는 컴포넌트(css 적용되어있음) */
+export function HtmlStringBox(props: BoxProps & { htmlString: string }): JSX.Element {
+  const { htmlString, ...rest } = props;
+  return (
+    <Box
+      {...rest}
+      className="sun-editor-editable"
+      dangerouslySetInnerHTML={{ __html: htmlString }}
+    />
   );
 }
