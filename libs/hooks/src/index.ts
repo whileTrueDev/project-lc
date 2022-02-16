2d6bf233440170761bd38242baffac4f22952291
Jiyeon <a1919361@gmail.com>
2022-02-16 16:35:52 +0900

    [feat] 방송인, 판매자 이용동의, 정책 조회 훅 생성

--- libs/hooks/src/index.ts
+++ libs/hooks/src/index.ts
@@ -124,3 +124,4 @@ export * from './lib/useHorizontalScroll';
 export * from './lib/useOrderExportableCheck';
 export * from './lib/useOrderReturnOrRefundStatus';
 export * from './lib/useSocialLoginFailAlarm';
+export * from './lib/useTerms';
