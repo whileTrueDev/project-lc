a67e67c35db4747ef90fd25432f7fd53c986b3a0
leejineun <43170606+leejineun@users.noreply.github.com>
2022-02-17 17:58:04 +0900

    feat: 휴면 계정 분리용 테이블 추가 및 복제 삭제 코드 추가

--- apps/mailer/src/app/app.service.ts
+++ apps/mailer/src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@project-lc/prisma-orm';
 import * as dayjs from 'dayjs';
+import { Broadcaster, Seller } from '@prisma/client';
 import { MailNoticeService } from '../lib/mail/mail-notice.service';
 import { S3Service } from '../lib/s3/s3.service';
 @Injectable()
@@ -10,10 +11,6 @@ export class AppService {
     private readonly mailNoticeService: MailNoticeService,
     private readonly s3Service: S3Service,
   ) {}
-
-  getHello(): string {
-    return 'Hello world!';
-  }
 
   getLastLoginDate(): Promise<any> {
     return this.prisma
@@ -51,7 +48,29 @@ export class AppService {
     );
   }
 
-  private copyBroadcasterRow(targetMail): Promise<any> {
+  private getBroadcasterId(broadcasterId): Promise<{ id: number }> {
+    return this.prisma.broadcaster.findFirst({
+      where: {
+        id: broadcasterId,
+      },
+      select: {
+        id: true,
+      },
+    });
+  }
+
+  private getSellerId(broadcasterId): Promise<{ id: number }> {
+    return this.prisma.seller.findFirst({
+      where: {
+        id: broadcasterId,
+      },
+      select: {
+        id: true,
+      },
+    });
+  }
+
+  private copyBroadcasterRow(broadcasterId): Promise<any> {
     return this.prisma
       .$executeRaw`INSERT INTO InactiveBroadcaster (id, email, userName, userNickname, overlayUrl, avatar, password)
     SELECT 
@@ -59,10 +78,10 @@ export class AppService {
     FROM 
         Broadcaster
     WHERE 
-        email = ${targetMail};`;
+        id = ${broadcasterId};`;
   }
 
-  private copySellerRow(targetMail): Promise<any> {
+  private copySellerRow(sellerId): Promise<any> {
     return this.prisma
       .$executeRaw`INSERT INTO InactiveSeller (id, email, name, avatar, password)
     SELECT 
@@ -70,13 +89,37 @@ export class AppService {
     FROM 
         Seller
     WHERE 
-        email = ${targetMail};`;
+        id = ${sellerId};`;
   }
 
-  private updateBroadcasterNull(targetMail): Promise<any> {
+  private copySocialBroadcasterRow(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveBroadcasterSocialAccount
+      (serviceId, provider, name, registDate, profileImage, accessToken, refreshToken, broadcasterId)
+    SELECT 
+      serviceId, provider, name, registDate, profileImage, accessToken, refreshToken, broadcasterId
+    FROM 
+      BroadcasterSocialAccount
+    WHERE 
+      id = ${broadcasterId};`;
+  }
+
+  private copySocialSellerRow(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveSellerSocialAccount 
+      (serviceId, provider, name, registDate, profileImage, accessToken, refreshToken, sellerId)
+    SELECT 
+      serviceId, provider, name, registDate, profileImage, accessToken, refreshToken, sellerId
+    FROM 
+      SellerSocialAccount
+    WHERE 
+      sellerId = ${sellerId};`;
+  }
+
+  private updateBroadcasterNull(broadcasterId): Promise<Broadcaster> {
     return this.prisma.broadcaster.update({
       where: {
-        email: targetMail,
+        id: broadcasterId,
       },
       data: {
         email: null,
@@ -89,10 +132,17 @@ export class AppService {
     });
   }
 
-  private updateSellerNull(targetMail): Promise<any> {
+  private deleteBroadcasterSocialAccountNull(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+      DELETE FROM 
+        BroadcasterSocialAccount 
+      WHERE broadcasterId = ${broadcasterId}`;
+  }
+
+  private updateSellerNull(sellerId): Promise<Seller> {
     return this.prisma.seller.update({
       where: {
-        email: targetMail,
+        id: sellerId,
       },
       data: {
         email: null,
@@ -104,26 +154,189 @@ export class AppService {
     });
   }
 
+  private deleteSellerSocialAccountNull(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+      Delete FROM
+        SellerSocialAccount 
+      WHERE sellerId = ${sellerId}`;
+  }
+
+  private copyBroadcasterAddress(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveBroadcasterAddress 
+      (id, address, detailAddress, postalCode, createDate, broadcasterId)
+    SELECT 
+      id, address, detailAddress, postalCode, createDate, broadcasterId
+    FROM 
+      BroadcasterAddress
+    WHERE 
+      id = ${broadcasterId}
+    `;
+  }
+
+  private copyBroadcasterChannel(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveBroadcasterChannel
+      (id, url, createDate, broadcasterId)
+    SELECT 
+      id, url, createDate, broadcasterId
+    FROM 
+      BroadcasterChannel
+    WHERE 
+      id = ${broadcasterId}
+    `;
+  }
+
+  private copyBroadcasterContacts(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveBroadcasterContacts
+      (id, name, email, phoneNumber, isDefault, createDate, broadcasterId)
+    SELECT 
+      id, name, email, phoneNumber, isDefault, createDate, broadcasterId
+    FROM 
+      BroadcasterChannel
+    WHERE 
+      id = ${broadcasterId}
+    `;
+  }
+
+  private copyBroadcasterSettlementInfo(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveBroadcasterSettlementInfo
+      (id, type, name, idCardNumber, phoneNumber, bank, accountNumber, accountHolder, idCardImageName, accountImageName, taxManageAgreement, personalInfoAgreement, settlementAgreement, broadcasterId)
+    SELECT 
+    id, type, name, idCardNumber, phoneNumber, bank, accountNumber, accountHolder, idCardImageName, accountImageName, taxManageAgreement, personalInfoAgreement, settlementAgreement, broadcasterId
+    FROM 
+      BroadcasterSettlementInfo
+    WHERE 
+      id = ${broadcasterId}
+    `;
+  }
+
+  private copySellerBusinessRegistration(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveSellerBusinessRegistration
+      (id, sellerEmail, companyName, businessRegistrationNumber, representativeName, businessType, businessItem, businessAddress, taxInvoiceMail, businessRegistrationImageName, mailOrderSalesNumber, mailOrderSalesImageName, businessRegistrationConfirmationId )
+    SELECT 
+    id, sellerEmail, companyName, businessRegistrationNumber, representativeName, businessType, businessItem, businessAddress, taxInvoiceMail, businessRegistrationImageName, mailOrderSalesNumber, mailOrderSalesImageName, businessRegistrationConfirmationId 
+    FROM 
+      SellerBusinessRegistration
+    WHERE 
+      id = ${sellerId}
+    `;
+  }
+
+  private copySellerContacts(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveSellerContacts
+      (id, sellerId, email, phoneNumber, isDefault, createDate)
+    SELECT 
+    id, sellerId, email, phoneNumber, isDefault, createDate
+    FROM 
+      SellerContacts
+    WHERE 
+      id = ${sellerId}
+    `;
+  }
+
+  private copySellerSettlementAccount(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+    INSERT INTO InactiveSellerSettlementAccount
+      (id, sellerEmail, bank, number, name, settlementAccountImageName)
+    SELECT 
+      id, sellerEmail, bank, number, name, settlementAccountImageName
+    FROM 
+      SellerSettlementAccount
+    WHERE 
+      id = ${sellerId}
+    `;
+  }
+
+  private deleteBroadcasterAddress(broadcasterId): Promise<any> {
+    return this.prisma.broadcasterAddress.delete({
+      where: {
+        broadcasterId,
+      },
+    });
+  }
+
+  private deleteBroadcasterChannel(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    DELETE FROM BroadcasterChannel WHERE broadcasterId=${broadcasterId}`;
+  }
+
+  private deleteBroadcasterContacts(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    DELETE FROM BroadcasterContacts WHERE broadcasterId=${broadcasterId}`;
+  }
+
+  private deleteBroadcasterSettlementInfo(broadcasterId): Promise<any> {
+    return this.prisma.$executeRaw`
+    DELETE FROM BroadcasterSettlementInfo WHERE broadcasterId=${broadcasterId}`;
+  }
+
+  private deleteSellerBusinessRegistration(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+    DELETE FROM SellerBusinessRegistration WHERE sellerId=${sellerId}`;
+  }
+
+  private deleteSellerContacts(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+    DELETE FROM SellerContacts WHERE sellerId=${sellerId}`;
+  }
+
+  private deleteSellerSettlementAccount(sellerId): Promise<any> {
+    return this.prisma.$executeRaw`
+    DELETE FROM SellerSettlementAccount WHERE sellerId=${sellerId}`;
+  }
+
   async moveInactiveUserData(inactivateTarget): Promise<void> {
-    if (inactivateTarget.userType === 'broadcaster') {
+    if (inactivateTarget.userType === 'seller') {
+      // s3 데이터 분리
+      const sellerId = await this.getSellerId(inactivateTarget.userEmail);
+      Promise.all([
+        await this.copySellerRow(sellerId.id),
+        await this.copySocialSellerRow(sellerId.id),
+        this.updateSellerNull(sellerId.id),
+        this.deleteSellerSocialAccountNull(sellerId.id),
+        this.copySellerBusinessRegistration(sellerId.id).then(() =>
+          this.deleteSellerBusinessRegistration(sellerId.id),
+        ),
+        this.copySellerContacts(sellerId.id).then(() =>
+          this.deleteSellerContacts(sellerId.id),
+        ),
+        this.copySellerSettlementAccount(sellerId.id).then(() =>
+          this.deleteSellerSettlementAccount(sellerId.id),
+        ),
+        this.s3Service.moveObjects('business-registration', inactivateTarget.userEmail),
+        this.s3Service.moveObjects('settlement-account', inactivateTarget.userEmail),
+        this.s3Service.moveObjects('mail-order', inactivateTarget.userEmail),
+      ]);
+    } else if (inactivateTarget.userType === 'broadcaster') {
+      const broadcasterId = await this.getBroadcasterId(inactivateTarget.userEmail);
       // s3 데이터 분리
       Promise.all([
-        await this.copySellerRow(inactivateTarget.userEmail),
-        this.updateSellerNull(inactivateTarget.userEmail),
+        await this.copyBroadcasterRow(broadcasterId.id),
+        await this.copySocialBroadcasterRow(broadcasterId.id),
+        this.updateBroadcasterNull(broadcasterId.id),
+        this.deleteBroadcasterSocialAccountNull(broadcasterId.id),
+        this.copyBroadcasterAddress(broadcasterId.id).then(() =>
+          this.deleteBroadcasterAddress(broadcasterId.id),
+        ),
+        this.copyBroadcasterChannel(broadcasterId.id).then(() =>
+          this.deleteBroadcasterChannel(broadcasterId.id),
+        ),
+        this.copyBroadcasterContacts(broadcasterId.id).then(() =>
+          this.deleteBroadcasterContacts(broadcasterId.id),
+        ),
+        this.copyBroadcasterSettlementInfo(broadcasterId.id).then(() =>
+          this.deleteBroadcasterSettlementInfo(broadcasterId.id),
+        ),
         this.s3Service.moveObjects(
           'broadcaster-account-image',
           inactivateTarget.userEmail,
         ),
         this.s3Service.moveObjects('broadcaster-id-card', inactivateTarget.userEmail),
-      ]);
-    } else if (inactivateTarget.userType === 'seller') {
-      // s3 데이터 분리
-      Promise.all([
-        await this.copyBroadcasterRow(inactivateTarget.userEmail),
-        this.updateBroadcasterNull(inactivateTarget.userEmail),
-        this.s3Service.moveObjects('business-registration', inactivateTarget.userEmail),
-        this.s3Service.moveObjects('settlement-account', inactivateTarget.userEmail),
-        this.s3Service.moveObjects('mail-order', inactivateTarget.userEmail),
       ]);
     }
   }
