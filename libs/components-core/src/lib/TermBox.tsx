7fd28753a28ee25a2d619cadf56eb0280899bf3b
Jiyeon <a1919361@gmail.com>
2022-02-17 09:30:36 +0900

    [fix] 약관 표시박스 다크모드 스타일 수정

--- libs/components-core/src/lib/TermBox.tsx
+++ libs/components-core/src/lib/TermBox.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps } from '@chakra-ui/react';
+import { Box, BoxProps, useColorModeValue } from '@chakra-ui/react';
 import { boxStyle } from '@project-lc/components-constants/commonStyleProps';
 import 'suneditor/dist/css/suneditor.min.css';
 
@@ -21,10 +21,14 @@ export default function TermBox({ text }: { text: string }): JSX.Element {
 /** html string 형태의 텍스트 표시하는 컴포넌트(css 적용되어있음) */
 export function HtmlStringBox(props: BoxProps & { htmlString: string }): JSX.Element {
   const { htmlString, ...rest } = props;
+  const bgColor = useColorModeValue('white', 'gray.700');
+  const fontColor = useColorModeValue('gray.800', 'white');
   return (
     <Box
       {...rest}
       className="sun-editor-editable"
+      bg={bgColor}
+      color={fontColor}
       dangerouslySetInnerHTML={{ __html: htmlString }}
     />
   );
