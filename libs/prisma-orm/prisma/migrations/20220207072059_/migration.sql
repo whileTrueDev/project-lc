e8da07f43a3f0a8b035c68b052ccada65ef126bd
hwasurr <iamsupermazinga@gmail.com>
2022-02-21 10:08:48 +0900

    chore: 버전 5 마이그레이션 병합

--- libs/prisma-orm/prisma/migrations/20220207072059_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `BroadcasterSettlementItems` ADD COLUMN `sellType` ENUM('liveShopping', 'productPromotion', 'normal') NULL;
-
--- AlterTable
-ALTER TABLE `SellerSettlementItems` ADD COLUMN `sellType` ENUM('liveShopping', 'productPromotion', 'normal') NULL;
