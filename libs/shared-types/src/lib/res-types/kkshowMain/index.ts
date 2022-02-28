0593bce2d732d9cc8401385197bcc926f6f42ec6
Jiyeon <a1919361@gmail.com>
2022-02-28 15:53:15 +0900

    [feat] 크크쇼 메인 라이브예고 데이터 타입 작성

--- libs/shared-types/src/lib/res-types/kkshowMain/index.ts
+++ libs/shared-types/src/lib/res-types/kkshowMain/index.ts
@@ -1,1 +1,2 @@
 export * from './carouselItem';
+export * from './liveTrailer';
---
+++ libs/shared-types/src/lib/res-types/kkshowMain/liveTrailer.ts
@@ -0,0 +1,11 @@
+// 크크쇼 메인 라이브예고 데이터
+export interface KkShowMainLiveTrailer {
+  imageUrl: string; // 홍보용이미지 url
+  productLinkUrl: string; // 판매상품링크 url
+  broadcasterNickname: string; // 방송인 활동명
+  broadcasterDescription: string; // 방송인 설명(해시태그)
+  liveShoppingName: string; // 라이브쇼핑명
+  broadcastStartDate: Date; // 방송시작일시
+  productDiscountRate: number; // 할인율(0~100사이)
+  productPrice: number; // 판매가격
+}
