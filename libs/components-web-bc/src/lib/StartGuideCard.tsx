1653d8d14400e77e1c61a535dd4dbe1dc222bd3d
hwasurr <iamsupermazinga@gmail.com>
2022-03-03 17:25:01 +0900

    feat: 방송인센터 마이페이지 홈 카드 디자인 수정

--- libs/components-web-bc/src/lib/StartGuideCard.tsx
+++ libs/components-web-bc/src/lib/StartGuideCard.tsx
@@ -1,27 +1,26 @@
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 
 export function StartGuideCard({ onOpen }: { onOpen: () => void }): JSX.Element {
   return (
-    <>
-      <Box
-        borderRadius="md"
-        borderWidth="1px"
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        p={3}
-      >
-        <Text fontWeight="bold">
-          <Text as="span" color="red.500">
-            [필수]
-          </Text>
-          &nbsp; 크크쇼 시작 가이드
+    <Flex
+      borderRadius="md"
+      borderWidth="1px"
+      flexDir="column"
+      alignItems="center"
+      justifyContent="space-between"
+      p={3}
+      h={150}
+    >
+      <Text fontWeight="bold">
+        <Text as="span" color="red.500">
+          [필수]
         </Text>
+        &nbsp; 크크쇼 시작 가이드
+      </Text>
 
-        <Button variant="solid" size="sm" onClick={onOpen}>
-          시작하기
-        </Button>
-      </Box>
-    </>
+      <Button variant="solid" onClick={onOpen}>
+        시작하기
+      </Button>
+    </Flex>
   );
 }
