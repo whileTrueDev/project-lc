30d4022025a19460df3661c1fad8fea3ed9f7199
hwasurr <iamsupermazinga@gmail.com>
2022-03-04 11:40:37 +0900

    feat: 로고 화이트, 다크 variant 추가

--- libs/components-shared/src/lib/KksLogo.tsx
+++ libs/components-shared/src/lib/KksLogo.tsx
@@ -1,5 +1,5 @@
 import { ColorMode, useColorMode } from '@chakra-ui/color-mode';
-import { ChakraNextImage } from '@project-lc/components-core/ChakraNextImage';
+import { Image, ImageProps } from '@chakra-ui/react';
 import { UserType } from '@project-lc/shared-types';
 import { useMemo } from 'react';
 
@@ -9,21 +9,31 @@ export const darkLogo = 'kksMainLogoDarkMode.png';
 export const darkBigLogo = 'kksMainLogoDarkModeBig.png';
 export const lightLogo = 'kksMainLogoLightMode.png';
 export const lightBigLogo = 'kksMainLogoLightModeBig.png';
+export const whiteLogo = 'kksMainOnlyWhite.png';
+// 판매자 로고
 export const sellerLogo = 'kkshow-seller-lightmode.png';
 export const sellerDarkLogo = 'kkshow-seller-darkmode.png';
+// 방송인 로고
 export const broadcasterLogo = 'kkshow-broadcaster-lightmode.png';
 export const broadcasterDarkLogo = 'kkshow-broadcaster-darkmode.png';
 
-export interface KksLogoProps {
+export interface KksLogoProps extends ImageProps {
   appType?: UserType;
-  size?: 'small';
+  variant?: 'white' | 'dark';
 }
 
 interface GetCorrectLogoOption {
   appType: KksLogoProps['appType'];
   colorMode: ColorMode;
+  variant?: KksLogoProps['variant'];
 }
-function getCorrectLogoInfo({ appType, colorMode }: GetCorrectLogoOption): string {
+function getCorrectLogoInfo({
+  appType,
+  colorMode,
+  variant,
+}: GetCorrectLogoOption): string {
+  if (variant === 'white') return LOGO_S3_PREFIX + whiteLogo;
+  if (variant === 'dark') return LOGO_S3_PREFIX + darkLogo;
   switch (appType) {
     case 'broadcaster':
       if (colorMode === 'dark') return LOGO_S3_PREFIX + broadcasterDarkLogo;
@@ -43,20 +53,22 @@ function getCorrectLogoInfo({ appType, colorMode }: GetCorrectLogoOption): strin
  * @description 이미지 크기가 400x150보다 클 경우 manual 옵션을 사용하세요.
  * 이미지 비율은 x:y = 1.6969 : 1
  */
-export function KksLogo(props: KksLogoProps): JSX.Element | null {
-  const { appType } = props;
+export function KksLogo({
+  appType,
+  variant,
+  ...props
+}: KksLogoProps): JSX.Element | null {
   const { colorMode } = useColorMode();
 
   const logoSrc = useMemo(
-    () => getCorrectLogoInfo({ colorMode, appType }),
-    [colorMode, appType],
+    () => getCorrectLogoInfo({ colorMode, appType, variant }),
+    [colorMode, appType, variant],
   );
 
   if (!appType) {
-    return <ChakraNextImage src={logoSrc} width="76.36" height="34" />;
+    return <Image loading="eager" src={logoSrc} width="76.36" height="34" {...props} />;
   }
-
-  return <ChakraNextImage src={logoSrc} width="76.36" height="45" />;
+  return <Image loading="eager" src={logoSrc} width="76.36" height="45" {...props} />;
 }
 
 export default KksLogo;
