b7b12f3b5166fa784f82988be69e415efd61c07d
leejineun <43170606+leejineun@users.noreply.github.com>
2022-03-04 10:36:18 +0900

    fix: 오타수정 및 빌드에러 수정

--- libs/shared-types/src/index.ts
+++ libs/shared-types/src/index.ts
@@ -22,7 +22,7 @@ export * from './lib/constants/shippingTypes';
 export * from './lib/constants/signupRegisterOptions';
 export * from './lib/constants/socialLoginUserTypeKey';
 export * from './lib/dto/adminClassChangeHistory.dto';
-export * from './lib/dto/AdminClassDto.dto';
+export * from './lib/dto/adminClassDto.dto';
 export * from './lib/dto/adminSettlementInfo.dto';
 export * from './lib/dto/adminSignUpDto.dto';
 export * from './lib/dto/broadcaster.dto';
