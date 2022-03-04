cf4873445250119d5c75b260cec904d1cbc35f84
Jiyeon <a1919361@gmail.com>
2022-03-04 15:41:05 +0900

    [feat] 파일인풋 다이얼로그 컴포넌트 생성

---
+++ libs/components-core/src/lib/ImageInputDialog.tsx
@@ -0,0 +1,95 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ModalProps,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { FileReaderResultType, readAsDataURL } from '@project-lc/utils';
+import { useState } from 'react';
+import { ImageInput, ImageInputErrorTypes, ImageInputProps } from './ImageInput';
+
+export type ImageInputFileReadData = {
+  url: FileReaderResultType;
+  filename: string;
+  file: File;
+};
+/** 단일 이미지파일 등록 인풋 컴포넌트 및 미리보기 */
+export interface ImageInputDialogProps
+  extends Omit<ImageInputProps, 'handleSuccess' | 'handleError'>,
+    Pick<ModalProps, 'isOpen' | 'onClose'> {
+  onConfirm: (data: ImageInputFileReadData) => Promise<any>;
+  onError?: (error: ImageInputErrorTypes) => any;
+  modalTitle?: string;
+}
+export function ImageInputDialog(props: ImageInputDialogProps): JSX.Element {
+  const { modalTitle, isOpen, onClose, onConfirm, onError, ...restProps } = props;
+
+  const [imagePreview, setImagePreview] = useState<ImageInputFileReadData | null>(null);
+
+  const handleSuccess = async (fileName: string, file: File): Promise<void> => {
+    readAsDataURL(file).then(async ({ data }) => {
+      const imageData = { url: data, filename: fileName, file };
+      setImagePreview(imageData);
+    });
+  };
+
+  const handleRegist = (): void => {
+    if (!imagePreview) return;
+    onConfirm(imagePreview).then(() => {
+      setImagePreview(null);
+      onClose();
+    });
+  };
+
+  const handleClose = (): void => {
+    setImagePreview(null);
+    onClose();
+  };
+
+  const handleError = (error: ImageInputErrorTypes): void => {
+    if (onError) onError(error);
+    console.log(error);
+  };
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        {modalTitle && <ModalHeader>{modalTitle}</ModalHeader>}
+
+        <ModalCloseButton />
+        <ModalBody>
+          <Stack>
+            <ImageInput
+              {...restProps}
+              handleSuccess={handleSuccess}
+              handleError={handleError}
+            />
+            {imagePreview ? (
+              <img src={imagePreview.url as string} alt={imagePreview.filename} />
+            ) : (
+              <Text>이미지 파일을 선택하여 등록할 이미지를 확인하세요</Text>
+            )}
+          </Stack>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={handleRegist}>
+            등록하기
+          </Button>
+          <Button variant="ghost" onClick={handleClose}>
+            닫기
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default ImageInputDialog;
