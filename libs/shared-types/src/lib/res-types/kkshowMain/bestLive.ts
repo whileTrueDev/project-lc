4b2642e8fa5411503f923ceabc05264c3fefa7bb
Jiyeon <a1919361@gmail.com>
2022-03-04 18:10:17 +0900

    [fix] bestLive 타입에서 order 삭제, liveShoppingId추가

--- libs/shared-types/src/lib/res-types/kkshowMain/bestLive.ts
+++ libs/shared-types/src/lib/res-types/kkshowMain/bestLive.ts
@@ -1,8 +1,8 @@
 // 크크쇼 메인 베스트 라이브 섹션 아이템
 export interface KkshowMainBestLiveItem {
-  order: number; // 순위
   videoUrl: string; // 유튜브 임베드 위한 url
   broadcasterProfileImageUrl: string; // 방송인 프로필사진 url
   liveShoppingDescription: string; // 쵸단x귀빈정 <= 이부분에 표시될 문구
   liveShoppingTitle: string; // 해피쵸이어 <= 이부분에 표시될 문구
+  liveShoppingId: number; // 라이브쇼핑 아이디
 }
