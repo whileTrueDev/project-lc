07cce5692c1e3e41d4b8cb5db4bab6c257277e4a
Jiyeon <a1919361@gmail.com>
2022-03-06 16:18:35 +0900

    [refactor] 캐러셀 아이템 컴포넌트 타입별 분리

--- libs/components-admin/src/lib/kkshow-main/AdminKkshowMainCarouselItemView.tsx
+++ libs/components-admin/src/lib/kkshow-main/AdminKkshowMainCarouselItemView.tsx
@@ -8,159 +8,111 @@ import {
   Stack,
   Text,
 } from '@chakra-ui/react';
-import { KkshowMainCarouselItem } from '@project-lc/shared-types';
+import {
+  KkshowMainResData,
+  NowPlayingLiveItem,
+  PreviousLiveItem,
+  ProductAndBroadcasterInfo,
+  SimpleBannerItem,
+  UpcomingLiveItem,
+} from '@project-lc/shared-types';
 import { getAdminHost } from '@project-lc/utils';
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
-export function AdminKkshowMainCarouselItemView({
-  item,
-  index,
-}: {
-  item: KkshowMainCarouselItem;
+interface CarouselItemProps {
   index: number;
-}): JSX.Element {
-  const { type } = item;
-  const { register, watch } = useFormContext();
+}
 
-  const productInfo = useMemo(() => {
-    return (
-      <>
-        <Box>
-          <Text>상품링크</Text>
-          <Input {...register(`carousel.${index}.productLinkUrl` as const)} size="sm" />
-        </Box>
-        <Box>
-          <Text>상품명</Text>
-          <Input {...register(`carousel.${index}.productName` as const)} size="sm" />
-        </Box>
-        <Box>
-          <Text>기존가격</Text>
-          <Input
-            type="number"
-            {...register(`carousel.${index}.normalPrice` as const, {
-              valueAsNumber: true,
-            })}
-            size="sm"
-          />
-        </Box>
-        <Box>
-          <Text>할인가격</Text>
-          <Input
-            type="number"
-            color="red"
-            {...register(`carousel.${index}.discountedPrice` as const, {
-              valueAsNumber: true,
-            })}
-            size="sm"
-          />
-        </Box>
-      </>
-    );
-  }, [index, register]);
+/** 크크쇼 메인 캐러셀 아이템 컴포넌트 - 이미지 배너 */
+export function CarouselItemSimpleBanner({
+  index,
+  item,
+}: CarouselItemProps & { item: SimpleBannerItem }): JSX.Element {
+  const { register } = useFormContext<KkshowMainResData>();
+  return (
+    <>
+      <Text fontWeight="bold">이미지배너</Text>
+      <ImageBanner imageUrl={item.imageUrl} />
+      <Box>
+        <Text>배너 링크 주소</Text>
+        <Input {...register(`carousel.${index}.linkUrl` as const)} size="sm" />
+      </Box>
+    </>
+  );
+}
 
-  switch (type) {
-    case 'simpleBanner':
-      return (
-        <>
-          <Text fontWeight="bold">이미지배너</Text>
-          <ImageBanner imageUrl={item.imageUrl} />
-          <Box>
-            <Text>배너 링크 주소</Text>
-            <Input
-              {...register(`carousel.${index}.linkUrl` as const, {
-                required: true,
-              })}
-              size="sm"
-            />
-          </Box>
-        </>
-      );
-    case 'upcoming':
-      return (
-        <>
-          <Text fontWeight="bold">라이브예고</Text>
-          <ImageBanner imageUrl={item.imageUrl} />
-          <Stack direction="row">
-            <BroadcasterProfile profileImageUrl={item.profileImageUrl} />
+/** 크크쇼 메인 캐러셀 아이템 컴포넌트 - 라이브 예고 */
+export function CarouselItemUpcomingLive({
+  index,
+  item,
+}: CarouselItemProps & { item: UpcomingLiveItem }): JSX.Element {
+  return (
+    <>
+      <Text fontWeight="bold">라이브예고</Text>
+      <ImageBanner imageUrl={item.imageUrl} />
+      <CarouselItemProductAndBroadcasterInfo index={index} {...item} />
+    </>
+  );
+}
 
-            {item.productImageUrl && (
-              <ProductImage productImageUrl={item.productImageUrl} />
-            )}
-            {productInfo}
-          </Stack>
-        </>
-      );
-    case 'nowPlaying':
-      return (
-        <>
-          <Text fontWeight="bold">현재라이브</Text>
-          <Box>
-            <Stack direction="row">
-              <Text>플랫폼 </Text>
-              <RadioGroup mb={1} value={watch(`carousel.${index}.platform` as const)}>
-                <HStack>
-                  <Radio
-                    {...register(`carousel.${index}.platform` as const)}
-                    value="twitch"
-                  >
-                    트위치
-                  </Radio>
-                  <Radio
-                    {...register(`carousel.${index}.platform` as const)}
-                    value="youtube"
-                  >
-                    유튜브
-                  </Radio>
-                </HStack>
-              </RadioGroup>
-            </Stack>
+/** 크크쇼 메인 캐러셀 아이템 컴포넌트 - 현재 라이브 */
+export function CarouselItemNowPlayingLive({
+  index,
+  item,
+}: CarouselItemProps & { item: NowPlayingLiveItem }): JSX.Element {
+  const { register, watch } = useFormContext<KkshowMainResData>();
+  return (
+    <>
+      <Text fontWeight="bold">현재라이브</Text>
+      <Box>
+        <Stack direction="row">
+          <Text>플랫폼 </Text>
+          <RadioGroup mb={1} value={watch(`carousel.${index}.platform` as const)}>
+            <HStack>
+              <Radio {...register(`carousel.${index}.platform` as const)} value="twitch">
+                트위치
+              </Radio>
+              <Radio {...register(`carousel.${index}.platform` as const)} value="youtube">
+                유튜브
+              </Radio>
+            </HStack>
+          </RadioGroup>
+        </Stack>
 
-            <VideoImbed
-              videoUrl={
-                item.platform === 'twitch'
-                  ? `https://player.twitch.tv/?channel=${item.videoUrl}&parent=${
-                      getAdminHost().includes('localhost') ? 'localhost' : getAdminHost()
-                    }`
-                  : `https://www.youtube.com/embed/${item.videoUrl}`
-              }
-            />
-          </Box>
+        <VideoImbed
+          videoUrl={
+            item.platform === 'twitch'
+              ? `https://player.twitch.tv/?channel=${item.videoUrl}&parent=${
+                  getAdminHost().includes('localhost') ? 'localhost' : getAdminHost()
+                }`
+              : `https://www.youtube.com/embed/${item.videoUrl}`
+          }
+        />
+      </Box>
 
-          <Stack direction="row">
-            <BroadcasterProfile profileImageUrl={item.profileImageUrl} />
+      <CarouselItemProductAndBroadcasterInfo index={index} {...item} />
+    </>
+  );
+}
 
-            {item.productImageUrl && (
-              <ProductImage productImageUrl={item.productImageUrl} />
-            )}
-            {productInfo}
-          </Stack>
-        </>
-      );
-    case 'previous':
-      return (
-        <>
-          <Text fontWeight="bold">이전라이브</Text>
-          <VideoImbed
-            videoUrl={`https://www.youtube.com/embed/${item.videoUrl.replace(
-              'https://youtu.be/',
-              '',
-            )}`}
-          />
-          <Stack direction="row">
-            <BroadcasterProfile profileImageUrl={item.profileImageUrl} />
-
-            {item.productImageUrl && (
-              <ProductImage productImageUrl={item.productImageUrl} />
-            )}
-            {productInfo}
-          </Stack>
-        </>
-      );
-
-    default:
-      return <Text>{`not supported carousel item type : ${type}`}</Text>;
-  }
+/** 크크쇼 메인 캐러셀 아이템 컴포넌트 - 이전 라이브 */
+export function CarouselItemPreviousLive({
+  index,
+  item,
+}: CarouselItemProps & { item: PreviousLiveItem }): JSX.Element {
+  return (
+    <>
+      <Text fontWeight="bold">이전라이브</Text>
+      <VideoImbed
+        videoUrl={`https://www.youtube.com/embed/${item.videoUrl.replace(
+          'https://youtu.be/',
+          '',
+        )}`}
+      />
+      <CarouselItemProductAndBroadcasterInfo index={index} {...item} />
+    </>
+  );
 }
 
 export function ImageBanner({ imageUrl }: { imageUrl: string }): JSX.Element {
@@ -211,5 +163,47 @@ export function ProductImage({
       <Text>상품</Text>
       <img src={productImageUrl} alt="" width="50" height="50" />
     </Box>
+  );
+}
+
+export function CarouselItemProductAndBroadcasterInfo(
+  props: ProductAndBroadcasterInfo & { index: number },
+): JSX.Element {
+  const { index, profileImageUrl, productImageUrl } = props;
+  const { register } = useFormContext<KkshowMainResData>();
+  return (
+    <Stack direction="row">
+      <BroadcasterProfile profileImageUrl={profileImageUrl} />
+      <ProductImage productImageUrl={productImageUrl || ''} />
+      <Box>
+        <Text>상품링크</Text>
+        <Input {...register(`carousel.${index}.productLinkUrl` as const)} size="sm" />
+      </Box>
+      <Box>
+        <Text>상품명</Text>
+        <Input {...register(`carousel.${index}.productName` as const)} size="sm" />
+      </Box>
+      <Box>
+        <Text>기존가격</Text>
+        <Input
+          type="number"
+          {...register(`carousel.${index}.normalPrice` as const, {
+            valueAsNumber: true,
+          })}
+          size="sm"
+        />
+      </Box>
+      <Box>
+        <Text>할인가격</Text>
+        <Input
+          type="number"
+          color="red"
+          {...register(`carousel.${index}.discountedPrice` as const, {
+            valueAsNumber: true,
+          })}
+          size="sm"
+        />
+      </Box>
+    </Stack>
   );
 }
