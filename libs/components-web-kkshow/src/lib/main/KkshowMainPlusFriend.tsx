aa93769105ea6fc59c2dec9ec830cb76a9d03010
iamsupermazinga <iamsupermazinga@gmail.com>
2022-03-08 15:40:30 +0900

    refactor: 카카오 친구추가 새창에서 띄우도록 수정

--- libs/components-web-kkshow/src/lib/main/KkshowMainPlusFriend.tsx
+++ libs/components-web-kkshow/src/lib/main/KkshowMainPlusFriend.tsx
@@ -22,7 +22,7 @@ export function KkshowMainPlusFriend(): JSX.Element {
             align="center"
           >
             <NextLink href={kakaoChannel} passHref>
-              <LinkOverlay>
+              <LinkOverlay isExternal>
                 <Heading fontSize={{ base: 'lg', md: '2xl' }}>
                   크크쇼랑 플친 맺고 다양한 혜택과 소식 받아보세요!
                 </Heading>
