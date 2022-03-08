06e95267cd85610ff2fd1f9edc8d1cf8468276b2
Jiyeon <a1919361@gmail.com>
2022-03-08 16:59:34 +0900

    [docs] s3KeyType 주석 추가

--- libs/utils-s3/src/lib/s3.ts
+++ libs/utils-s3/src/lib/s3.ts
@@ -14,19 +14,26 @@ import path from 'path';
 
 // 추후에 S3에 저장할 데이터 종류가 더해지는 경우 추가
 export type s3KeyType =
-  | 'business-registration'
-  | 'goods'
-  | 'mail-order'
-  | 'settlement-account'
-  | 'vertical-banner'
-  | 'donation-images'
-  | 'broadcaster-id-card' // 방송인 신분증
-  | 'broadcaster-account-image' // 방송인 통장사본
+  | 'avatar' // 프로필 이미지 avatar/:userType/:email/:filename.ext
+  | 'broadcaster-account-image' // 방송인 통장사본 broadcaster-account-image/:email/:filename.ext
+  | 'broadcaster-id-card' // 방송인 신분증 broadcaster-id-card/:email/:filename.ext
+  | 'business-registration' // 사업자등록증 business-registration/:email/:filename.ext
+  | 'donation-images' // 도네이션 이미지
+  | 'goods' // 상품 이미지 goods/:email/:filename.ext
+  | 'horizontal-banner' // 가로배너
+  // | 'inactive-broadcaster-account-image' // 비활성 방송인 통장사본
+  // | 'inactive-broadcaster-id-card' // 비활성 방송인 신분증
+  // | 'inactive-business-registration' // 비활성 판매자 사업자등록증
+  // | 'inactive-mail-order' // 비활성 판매자 통신판매업신고증
+  // | 'inactive-settlement-account' // 비활성 판매자 통장사본
+  | 'kkshow-main-carousel-images' // 크크쇼 메인 캐러셀 배너이미지 kkshow-main-carousel-images/:filename.ext
+  | 'live-shopping-images' // 크크쇼 메인 라이브 쇼핑 섬네일 이미지 live-shopping-images/:liveShoppingId/:type/:filename.ext
+  | 'mail-order' // 판매자 통신판매업신고증 mail-order/:email/:filename.ext
   | 'overlay-logo'
-  | 'horizontal-banner'
-  | 'kkshow-main-carousel-images' // 크크쇼 메인 캐러셀 배너 이미지
-  | 'live-shopping-images'; // 라이브 쇼핑 섬네일
-
+  | 'public' // 예시이미지, 가이드이미지 등 공개이미지
+  | 'settlement-account' // 판매자 통장사본 settlement-account/:email/:filename.ext
+  | 'vertical-banner' // 세로배너
+;
 export type s3TaggingKeys = 'overlayImageType'; // s3 object 태그 객체 키
 export interface S3UploadImageOptions {
   filename: string | null;
