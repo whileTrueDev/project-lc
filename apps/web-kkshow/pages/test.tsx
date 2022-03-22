73f169dbece5ca3fd062ed5a338e267e812bca53
hwasurr <iamsupermazinga@gmail.com>
2022-03-22 16:44:20 +0900

    chore: 개발중 생성한 필요없는 파일 제거

--- apps/web-kkshow/pages/test.tsx
+++
@@ -1,5 +0,0 @@
-import { TwitchLiveEmbed } from '@project-lc/components-shared/TwitchLiveEmbed';
-
-export default function Test(): JSX.Element {
-  return <TwitchLiveEmbed channel="zilioner" height={400} />;
-}
