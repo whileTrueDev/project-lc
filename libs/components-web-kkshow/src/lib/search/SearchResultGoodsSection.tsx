e4a5aa5a547d249f77ab43cdd6f6a321b5cb7b50
Jiyeon <a1919361@gmail.com>
2022-03-24 09:38:22 +0900

    [feat] 상품아이템 마우스오버시 이미지 스케일

--- libs/components-web-kkshow/src/lib/search/SearchResultGoodsSection.tsx
+++ libs/components-web-kkshow/src/lib/search/SearchResultGoodsSection.tsx
@@ -1,20 +1,35 @@
-import { Image, LinkBox, LinkOverlay, Stack, Text } from '@chakra-ui/react';
+import { Box, LinkBox, LinkOverlay, Stack, Text, useBoolean } from '@chakra-ui/react';
 import { SearchResultItem } from '@project-lc/shared-types';
+import { motion } from 'framer-motion';
 import NextLink from 'next/link';
 import {
   SearchResultEmptyText,
   SearchResultSectionContainer,
 } from './SearchResultSectionContainer';
 
+const variants = {
+  scale: { scale: 1.1 },
+  normal: { scale: 1.0 },
+};
+
 /** 검색결과 - 상품아이템 => 쇼핑탭에서 작성한 상품컴포넌트와 비슷할듯하다 => 확인 후 수정필요
  */
 function GoodsCard({ item }: { item: SearchResultItem }): JSX.Element {
+  const [mouseOver, setMouseOver] = useBoolean(false);
   return (
-    <LinkBox>
+    <LinkBox onMouseOver={setMouseOver.on} onMouseOut={setMouseOver.off}>
       <NextLink href={item.linkUrl} passHref>
         <LinkOverlay isExternal={item.linkUrl.includes('http')}>
           <Stack w={{ base: '132px', sm: '260px' }}>
-            <Image src={item.imageUrl} rounded="xl" objectFit="cover" />
+            <Box rounded="xl" overflow="hidden">
+              <motion.img
+                src={item.imageUrl}
+                initial="normal"
+                animate={mouseOver ? 'scale' : 'normal'}
+                variants={variants}
+                style={{ objectFit: 'cover' }}
+              />
+            </Box>
             <Text fontFamily="Gmarket Sans" textAlign="center">
               {item.title}
             </Text>
