088e674b8abc054c383b98b7d148a2f391982506
Jiyeon <a1919361@gmail.com>
2022-03-24 11:08:04 +0900

    [feat] 레이아웃 컴포넌트에 더보기 버튼 추가

--- libs/components-web-kkshow/src/lib/search/SearchResultSectionContainer.tsx
+++ libs/components-web-kkshow/src/lib/search/SearchResultSectionContainer.tsx
@@ -1,24 +1,34 @@
-import { Box, Text, Heading, Stack, Container } from '@chakra-ui/react';
+import { Box, Text, Heading, Stack, Container, Button } from '@chakra-ui/react';
 
 /** 상품, 라이브컨텐츠, 크리에이터 영역 레이아웃 컴포넌트 */
 export function SearchResultSectionContainer({
   title,
   resultCount,
   children,
+  seeMoreButtonHandler,
 }: {
   title: string;
   resultCount?: number;
   children: React.ReactNode;
+  seeMoreButtonHandler?: () => void;
 }): JSX.Element {
   return (
     <Box>
       <Container maxW="6xl">
         <Stack py={8}>
-          <Stack direction="row" alignItems="flex-end" fontWeight="bold">
-            <Heading color="blue.500" fontSize="3xl">
-              {title}
-            </Heading>
-            {resultCount && <Text>{resultCount}개의 검색결과</Text>}
+          <Stack direction="row" justify="space-between" fontWeight="bold" width="100%">
+            <Stack direction="row" alignItems="flex-end">
+              <Heading color="blue.500" fontSize="3xl">
+                {title}
+              </Heading>
+              {resultCount && <Text>{resultCount}개의 검색결과</Text>}
+            </Stack>
+
+            <Box alignSelf="flex-end">
+              <Button colorScheme="blue" variant="link" onClick={seeMoreButtonHandler}>
+                더보기
+              </Button>
+            </Box>
           </Stack>
           <Box>{children}</Box>
         </Stack>
