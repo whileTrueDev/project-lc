4204b1c93f7e39a72b36f5f4ad20a814b9d6dde3
leejineun <43170606+leejineun@users.noreply.github.com>
2022-03-25 10:55:09 +0900

    chore: text index 추가

---
+++ libs/prisma-orm/prisma/migrations/20220325004803_/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE FULLTEXT INDEX `Broadcaster_userNickname_idx` ON `Broadcaster`(`userNickname`);
+
+-- CreateIndex
+CREATE FULLTEXT INDEX `Goods_goods_name_idx` ON `Goods`(`goods_name`);
