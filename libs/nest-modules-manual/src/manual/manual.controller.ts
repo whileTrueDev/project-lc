5f36bbfdd304a2669dc947ee9c0be2dc8d7cda6a
Jiyeon <a1919361@gmail.com>
2022-03-25 16:25:45 +0900

    [feat] 이용안내 컨트롤러 작성 - 개별 이용안내 조회 라우터

--- libs/nest-modules-manual/src/manual/manual.controller.ts
+++ libs/nest-modules-manual/src/manual/manual.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, UseInterceptors } from '@nestjs/common';
+import { Manual } from '@prisma/client';
 import { HttpCacheInterceptor } from '@project-lc/nest-core';
+import { ManualService } from './manual.service';
 
 // @UseInterceptors(HttpCacheInterceptor)
 @Controller('manual')
 export class ManualController {
-  @Get()
-  test(): string {
-    return 'this is manual';
+  constructor(private readonly manualService: ManualService) {}
+
+  @Get(':id')
+  getOneManual(@Param('id', ParseIntPipe) id: number): Promise<Manual> {
+    return this.manualService.getOneManualById(id);
   }
 }
