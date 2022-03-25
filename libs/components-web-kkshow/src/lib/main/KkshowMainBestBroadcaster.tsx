39fe56e9d11f191be6887623537b955d1076fc19
Jiyeon <a1919361@gmail.com>
2022-03-25 11:10:33 +0900

    [fix] 베스트 방송인 컴포넌트 export, link url 없는경우 수정

--- libs/components-web-kkshow/src/lib/main/KkshowMainBestBroadcaster.tsx
+++ libs/components-web-kkshow/src/lib/main/KkshowMainBestBroadcaster.tsx
@@ -59,7 +59,7 @@ interface BestBroadcasterItemProps {
   avatarUrl: string;
   href: string;
 }
-function BestBroadcasterItem(props: BestBroadcasterItemProps): JSX.Element {
+export function BestBroadcasterItem(props: BestBroadcasterItemProps): JSX.Element {
   return (
     <LinkBox>
       <Stack
@@ -84,13 +84,19 @@ function BestBroadcasterItem(props: BestBroadcasterItemProps): JSX.Element {
           }}
         />
 
-        <NextLink passHref href={props.href || '#'}>
-          <LinkOverlay isExternal={props.href.includes('http')}>
-            <Heading noOfLines={2} fontSize="xl">
-              {props.broadcasterName}
-            </Heading>
-          </LinkOverlay>
-        </NextLink>
+        {props.href ? (
+          <NextLink passHref href={props.href || '#'}>
+            <LinkOverlay isExternal={props.href.includes('http')}>
+              <Heading noOfLines={2} fontSize="xl">
+                {props.broadcasterName}
+              </Heading>
+            </LinkOverlay>
+          </NextLink>
+        ) : (
+          <Heading noOfLines={2} fontSize="xl">
+            {props.broadcasterName}
+          </Heading>
+        )}
       </Stack>
     </LinkBox>
   );
