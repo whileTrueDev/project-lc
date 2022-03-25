1c509de74ec4b0a00e37bc97206106e01fd47b49
Jiyeon <a1919361@gmail.com>
2022-03-25 11:09:13 +0900

    [feat] 크크쇼 네비바 흰색배경 variant 추가

--- libs/components-web-kkshow/src/lib/KkshowNavbar.tsx
+++ libs/components-web-kkshow/src/lib/KkshowNavbar.tsx
@@ -1,16 +1,38 @@
-import { Box, Flex, Link, Stack } from '@chakra-ui/react';
+import { Box, Flex, Link, Stack, useColorModeValue } from '@chakra-ui/react';
 import { kkshowNavLinks, NavItem } from '@project-lc/components-constants/navigation';
 import { ColorModeSwitcher } from '@project-lc/components-core/ColorModeSwitcher';
-import { KksLogo } from '@project-lc/components-shared/KksLogo';
+import { KkshowLogoVariant, KksLogo } from '@project-lc/components-shared/KksLogo';
 import NextLink from 'next/link';
+import GlobalSearcher from './GlobalSearcher';
 
-export function KkshowNavbar(): JSX.Element {
+type KkshowNavbarVariant = 'blue' | 'white';
+interface KkshowNavbar {
+  variant?: KkshowNavbarVariant;
+}
+/**
+ * @param variant 'blue'인 경우 배경색 파랑, 글자색 흰색 고정인 네비바(메인페이지나 쇼핑탭)
+ *                'white'인 경우 라이트모드에서는 배경색 흰색, 글자색 검정
+ *                             다크모드에서는 배경 검정, 글자 흰색인 네비바(검색페이지)
+ */
+export function KkshowNavbar({ variant = 'blue' }: KkshowNavbar): JSX.Element {
   const navHeight = 120;
+
+  const palette = {
+    bg: useColorModeValue('white', 'gray.800'),
+    color: useColorModeValue('gray.700', 'whiteAlpha.900'),
+    logoVariant: useColorModeValue('light', 'dark'),
+  };
+
+  if (variant === 'blue') {
+    palette.bg = 'blue.500';
+    palette.color = 'whiteAlpha.900';
+    palette.logoVariant = 'white';
+  }
 
   return (
     <Box
-      bg="blue.500"
-      color="whiteAlpha.900"
+      bg={palette.bg}
+      color={palette.color}
       pt={{ base: 0, md: 6 }}
       minH={navHeight}
       w="100%"
@@ -28,7 +50,10 @@ export function KkshowNavbar(): JSX.Element {
         <Box>
           <NextLink href="/" passHref>
             <Link>
-              <KksLogo variant="white" h={{ base: 30, md: 45 }} />
+              <KksLogo
+                variant={palette.logoVariant as KkshowLogoVariant}
+                h={{ base: 30, md: 45 }}
+              />
             </Link>
           </NextLink>
         </Box>
@@ -43,8 +68,7 @@ export function KkshowNavbar(): JSX.Element {
         {/* 우측 */}
         <Flex alignItems="center">
           <ColorModeSwitcher _hover={{}} />
-          {/* // TODO: 검색 기능 추가 이후 주석 해제 */}
-          {/* <GlobalSearcher /> */}
+          <GlobalSearcher />
         </Flex>
       </Flex>
 
