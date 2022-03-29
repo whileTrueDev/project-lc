859addb90a2c898c1968e9541dca1822257e239b
Jiyeon <a1919361@gmail.com>
2022-03-29 10:01:37 +0900

    [fix] 이용안내 조회 훅 수정

--- libs/hooks/src/lib/queries/useManual.tsx
+++ libs/hooks/src/lib/queries/useManual.tsx
@@ -1,14 +1,19 @@
-import { Manual } from '@prisma/client';
+import { Manual, UserType } from '@prisma/client';
 import { AxiosError } from 'axios';
 import { useQuery, UseQueryResult } from 'react-query';
 import axios from '../../axios';
 
-export const getManual = async (id: number): Promise<Manual> => {
-  return axios.get<Manual>('/manual', { params: { id } }).then((res) => res.data);
+export const getManualList = async (target: UserType): Promise<Manual[]> => {
+  return axios
+    .get<Manual[]>('/manual/list', { params: { target } })
+    .then((res) => res.data);
 };
-
-export const useManual = (id: number): UseQueryResult<Manual, AxiosError> => {
-  return useQuery<Manual, AxiosError>(['Manual', id], () => getManual(id), {
-    enabled: !!id,
-  });
+export const useManualList = (target: UserType): UseQueryResult<Manual[], AxiosError> => {
+  return useQuery<Manual[], AxiosError>(
+    ['ManualList', target],
+    () => getManualList(target),
+    {
+      enabled: !!target,
+    },
+  );
 };
