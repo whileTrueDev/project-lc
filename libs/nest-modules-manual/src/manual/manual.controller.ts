82521950c69b0ac4b8d45735c3294f73337a083c
Jiyeon <a1919361@gmail.com>
2022-03-29 10:00:59 +0900

    [fix] 이용안내 컨트롤러 수정

--- libs/nest-modules-manual/src/manual/manual.controller.ts
+++ libs/nest-modules-manual/src/manual/manual.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, ParseIntPipe, Query, UseInterceptors } from '@nestjs/common';
-import { Manual } from '@prisma/client';
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
+import { Manual, UserType } from '@prisma/client';
 import { HttpCacheInterceptor } from '@project-lc/nest-core';
 import { ManualService } from './manual.service';
 
@@ -8,8 +8,8 @@ import { ManualService } from './manual.service';
 export class ManualController {
   constructor(private readonly manualService: ManualService) {}
 
-  @Get('')
-  getOneManual(@Query('id', ParseIntPipe) id: number): Promise<Manual> {
-    return this.manualService.getOneManualById(id);
+  @Get('list')
+  getManualList(@Query('target') target: UserType): Promise<Manual[]> {
+    return this.manualService.getManualList(target);
   }
 }
