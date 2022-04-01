eb7038915a70ff1fa19db616949e458c24e2c41c
hwasurr <iamsupermazinga@gmail.com>
2022-04-01 10:47:31 +0900

    fix: 필요 없는 mailer 관련 환경변수 제거

--- apps/batch-inactive-handler/src/settings/config.validation.ts
+++ apps/batch-inactive-handler/src/settings/config.validation.ts
@@ -3,7 +3,6 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   DATABASE_URL: Joi.string().required(),
   S3_BUCKET_NAME: Joi.string().required(),
-  MAILER_HOST: Joi.string().required(),
   // Redis as Message Queue host url
   MQ_REDIS_URL: Joi.string().required(),
 });
