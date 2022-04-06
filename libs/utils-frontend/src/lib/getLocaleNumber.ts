8bb8324b0b3bf9023d2652e602e748c628b82d92
hwasurr <iamsupermazinga@gmail.com>
2022-04-06 16:09:45 +0900

    fix: 로케일숫자컨버터 함수 테스트 케이스 추가, 올바르게 변환되지 않는 현상 수정

--- libs/utils-frontend/src/lib/getLocaleNumber.ts
+++ libs/utils-frontend/src/lib/getLocaleNumber.ts
@@ -1,7 +1,9 @@
 import { Decimal } from '@prisma/client/runtime';
 
 export const getLocaleNumber = (num?: number | string | Decimal): string => {
-  if (!num) return '0';
-  if (Number.isNaN(num)) return Number(num).toLocaleString();
-  return num.toLocaleString();
+  if (num === 0) return '0';
+  if (!num) return '';
+  const converted = Number(num);
+  if (Number.isNaN(converted)) return num.toLocaleString();
+  return converted.toLocaleString();
 };
