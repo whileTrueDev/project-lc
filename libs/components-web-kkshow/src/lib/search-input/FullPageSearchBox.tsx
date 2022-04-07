2a70a8e23d265d3b4210b3e1b0576ee4896372e7
hwasurr <iamsupermazinga@gmail.com>
2022-04-07 17:24:18 +0900

    refactor: 검색 코드 정리 및 버그 수정
    - 모바일 화면에서 검색 이후 검색drawer 닫히지 않음
    - 모바일 화면에서 검색을 위해 submit 키를 눌러야만 하는데, 찾기 힘듦 (검색 실행 아이콘추가 필요할 듯)
    - 모바일 화면에서 최근검색어 긴 검색어 있는 경우 깨져보이는 현상 수정
    - 모바일 화면에서 한눈에 검색결과를 파악하기  힘듦
    - 검색 컴포넌트 코드 정리

---
+++ libs/components-web-kkshow/src/lib/search-input/FullPageSearchBox.tsx
@@ -0,0 +1,96 @@
+import { ChevronLeftIcon, SearchIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Tooltip,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { useKkshowSearchStore } from '@project-lc/stores';
+import { RefObject } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { SearchRecentKeywords } from './SearchHelpPopover';
+import { SearchForm, SearchInputBox, SearchInputBoxProps } from './SearchInputBox';
+
+interface FullPageSearchBoxProps {
+  searchBoxInputRef?: SearchInputBoxProps['inputRef'];
+}
+export function FullPageSearchBox({
+  searchBoxInputRef,
+}: FullPageSearchBoxProps): JSX.Element {
+  const closeSearchDrawer = useKkshowSearchStore((s) => s.closeSearchDrawer);
+  const { setValue } = useFormContext<SearchForm>();
+
+  return (
+    <Box>
+      <Flex my={4} display={{ base: 'flex', xl: 'none' }} alignItems="center">
+        <IconButton
+          aria-label="exit-search-drawer"
+          color={useColorModeValue('gray.600', 'gray.200')}
+          onClick={() => closeSearchDrawer()}
+          variant="unstyle"
+          icon={<ChevronLeftIcon w="30px" h="35px" />}
+        />
+        <SearchInputBox inputRef={searchBoxInputRef} />
+      </Flex>
+
+      {/* 최근검색어 */}
+      <Flex my={4} flexDirection="column">
+        <SearchRecentKeywords
+          onItemClick={(item) => {
+            setValue('keyword', item);
+          }}
+        />
+      </Flex>
+    </Box>
+  );
+}
+
+interface FullPageSearchDrawerProps extends FullPageSearchBoxProps {
+  containerRef: RefObject<HTMLElement | null>;
+}
+export function FullPageSearchDrawer({
+  containerRef,
+  searchBoxInputRef,
+}: FullPageSearchDrawerProps): JSX.Element {
+  const { isSearchDrawerOpen, openSearchDrawer, closeSearchDrawer } =
+    useKkshowSearchStore();
+
+  return (
+    <>
+      {/* 모바일 검색drawer 오픈 버튼 */}
+      <Tooltip label="검색" fontSize="xs">
+        <IconButton
+          size="md"
+          fontSize="lg"
+          variant="unstyle"
+          color="current"
+          icon={<SearchIcon />}
+          aria-label="toggle search"
+          onClick={openSearchDrawer}
+        />
+      </Tooltip>
+      <Drawer
+        onClose={closeSearchDrawer}
+        isOpen={isSearchDrawerOpen}
+        size="full"
+        initialFocusRef={searchBoxInputRef}
+        closeOnEsc
+        trapFocus
+        portalProps={{ containerRef }}
+      >
+        <DrawerOverlay display={{ base: 'flex', md: 'none' }} />
+        <DrawerContent display={{ base: 'flex', md: 'none' }}>
+          <DrawerBody p={0}>
+            <FullPageSearchBox searchBoxInputRef={searchBoxInputRef} />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
+export default FullPageSearchBox;
---
+++ libs/components-web-kkshow/src/lib/search-input/SearchHelpPopover.tsx
@@ -0,0 +1,105 @@
+import { SmallCloseIcon } from '@chakra-ui/icons';
+import { Box, Flex, IconButton, Text, useColorModeValue } from '@chakra-ui/react';
+import { useKkshowSearchStore } from '@project-lc/stores';
+import { MouseEvent, useEffect } from 'react';
+
+interface SearchHelpPopoverProps {
+  onItemClick: (item: string) => void;
+}
+export function SearchHelpPopover({ onItemClick }: SearchHelpPopoverProps): JSX.Element {
+  const { isSearchPopoverOpen } = useKkshowSearchStore();
+  return (
+    <Box
+      display={{ base: 'none', md: isSearchPopoverOpen ? 'block' : 'none' }}
+      position="absolute"
+      width="100%"
+      height="fit-content"
+      insetY={12}
+      flex={1}
+      bgColor={useColorModeValue('white', 'gray.600')}
+      color={useColorModeValue('black', 'white')}
+      zIndex="docked"
+      rounded="md"
+      boxShadow="md"
+    >
+      <SearchRecentKeywords onItemClick={onItemClick} />
+    </Box>
+  );
+}
+
+export function SearchRecentKeywords({
+  onItemClick,
+}: SearchHelpPopoverProps): JSX.Element {
+  const hoverColor = useColorModeValue('gray.50', 'gray.700');
+  const { keywords, setKeywords, loadKeywords } = useKkshowSearchStore();
+
+  useEffect(() => {
+    loadKeywords();
+  }, [loadKeywords]);
+
+  const deleteLocalStorageSearchKeyword = (toDeleteKeyword: string): void => {
+    const localDataArray: string[] = JSON.parse(
+      window.localStorage.getItem('searchKeyword') || '[]',
+    );
+    const index = localDataArray.indexOf(toDeleteKeyword);
+    if (index !== -1) {
+      localDataArray.splice(index, 1);
+      window.localStorage.setItem('searchKeyword', JSON.stringify(localDataArray));
+      setKeywords(JSON.parse(window.localStorage.getItem('searchKeyword') || '[]'));
+    }
+  };
+
+  return (
+    <Box p={{ base: 2, md: 0 }} my={3}>
+      <Text
+        ml={2}
+        as="sup"
+        fontSize={{ base: 'md', md: 'xs' }}
+        fontWeight={{ base: 'bold', md: 'unset' }}
+        color={useColorModeValue('blue.400', 'gray.400')}
+      >
+        최근 검색어
+      </Text>
+      {keywords.length === 0 && <Text ml={3}>최근 검색어가 없습니다</Text>}
+      {keywords.map((item: string) => (
+        <Flex
+          key={item}
+          _hover={{ backgroundColor: hoverColor }}
+          justifyContent="space-between"
+          alignItems="center"
+          borderBottomWidth={{ base: 'thin', md: 0 }}
+          py={1}
+        >
+          <Flex
+            width="100%"
+            flex={1}
+            pl={3}
+            textAlign="left"
+            wordBreak="break-all"
+            as="button"
+            type="submit"
+            onMouseDown={(e: MouseEvent<HTMLElement>) => e.preventDefault()}
+            onClick={() => onItemClick(item)}
+          >
+            <Text fontWeight="medium" noOfLines={{ base: 2, md: 1 }}>
+              {item}
+            </Text>
+          </Flex>
+          <IconButton
+            m={1}
+            color="gray"
+            variant="ghost"
+            rounded="full"
+            size="sm"
+            aria-label="search-button-icon"
+            icon={<SmallCloseIcon />}
+            onMouseDown={(e) => e.preventDefault()}
+            onClick={() => deleteLocalStorageSearchKeyword(item)}
+          />
+        </Flex>
+      ))}
+    </Box>
+  );
+}
+
+export default SearchHelpPopover;
