f782ea052f0d654df615c07fc7b6deffa2117d8b
hwasurr <iamsupermazinga@gmail.com>
2022-04-11 12:15:29 +0900

    ci: vercel 빌드 오류 수정

--- tools/ignore-vercel-build.sh
+++ tools/ignore-vercel-build.sh
@@ -7,16 +7,12 @@ fi
 
 # Name of the app to check. Change this to your application name!
 APP=$1
-# Determine version of Nx installed
-NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
 
 # Install @nrwl/workspace in order to run the affected command
-npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline 
-
-npm install -D typescript
+yarn
 
 # Run the affected command, comparing latest commit to the one before that
-npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
+yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 
 # Store result of the previous command (grep)
 IS_AFFECTED=$?
@@ -25,6 +21,6 @@ if [ $IS_AFFECTED -eq 1 ]; then
     echo "🛑 - Build cancelled (No code changes)"
     exit 0
 elif [ $IS_AFFECTED -eq 0 ]; then
-    echo "✅ - Build can proceed (Code changes Exist)"
+    echo "✅ - Build can proceed (Code changes exist)"
     exit 1
 fi
