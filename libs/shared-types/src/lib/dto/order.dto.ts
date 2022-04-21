bffa263751d4017994e08eb1bf6962cd0e6e8ecc
Jiyeon <a1919361@gmail.com>
2022-04-21 11:18:40 +0900

    [fix] createOrderDto타입이 Prisma.Order 타입 참조하게 수정

--- libs/shared-types/src/lib/dto/order.dto.ts
+++ libs/shared-types/src/lib/dto/order.dto.ts
@@ -1,11 +1,17 @@
-import { SellType } from '@prisma/client';
+import {
+  SellType,
+  Order,
+  OrderItem,
+  OrderItemOption,
+  OrderItemSupport,
+} from '@prisma/client';
 import { Type } from 'class-transformer';
 import {
+  ArrayNotEmpty,
   IsArray,
   IsBoolean,
   IsEmail,
   IsEnum,
-  IsInt,
   IsNumber,
   IsOptional,
   IsString,
@@ -18,61 +24,66 @@ export class CreateOrderItemSupportDto {
   @IsString()
   @IsOptional()
   /** 후원메시지 */
-  message?: string;
+  message?: OrderItemSupport['message'];
 
   @IsString()
   @IsOptional()
   /** 후원한 사람 닉네임(로그인한 경우 로그인한 사람의 닉네임, 비로그인은 '') */
-  nickname?: string;
+  nickname?: OrderItemSupport['nickname'];
 
-  @IsInt()
+  @IsNumber()
   /** 후원 대상 방송인 고유번호 */
-  broadcasterId: number;
+  broadcasterId: OrderItemSupport['broadcasterId'];
 }
 
 /** 주문상품옵션 OrderItemOption 생성 dto */
 export class CreateOrderItemOptionDto {
   /**  주문했을 당시 옵션명 */
   @IsString()
-  name: string;
+  name: OrderItemOption['name'];
 
   /** 주문했을 당시 옵션값 */
   @IsString()
-  value: string;
+  value: OrderItemOption['value'];
 
   /**  옵션 선택 수량(개수) */
   @IsNumber()
-  quantity: number;
+  quantity: OrderItemOption['quantity'];
 
   /** 주문했을 당시 소비자가 (미할인가)  @db.Decimal(10, 2)  */
   @IsNumber()
-  normalPrice: number;
+  normalPrice: OrderItemOption['normalPrice'];
 
   /** 주문했을 당시 판매가 (할인가) @db.Decimal(10, 2)  */
   @IsNumber()
-  discountPrice: number;
+  discountPrice: OrderItemOption['discountPrice'];
 
   /** 주문했을 당시 옵션 개당 무게 (단위 kg) Float? */
   @IsNumber()
   @IsOptional()
-  weight?: number;
+  weight?: OrderItemOption['weight'];
+
+  /** 참조하는 상품옵션 고유번호 */
+  @IsNumber()
+  goodsOptionId: OrderItemOption['goodsOptionId'];
 }
 
 /** 주문상품 OrderItem 생성 dto */
 export class CreateOrderItemDto {
   /** 연결된 상품 고유번호 */
-  @IsInt()
-  goodsId: number;
+  @IsNumber()
+  goodsId: OrderItem['goodsId'];
 
   /** 판매유형(주문경로) @default(normal) */
   @IsEnum(SellType)
   @IsOptional()
-  channel?: SellType;
+  channel?: OrderItem['channel'];
 
   /** 주문상품 옵션들[] */
   @IsArray()
+  @ArrayNotEmpty()
   @ValidateNested({ each: true })
-  @Type(() => CreateOrderItemDto)
+  @Type(() => CreateOrderItemOptionDto)
   options: CreateOrderItemOptionDto[];
 
   /** 주문상품에 연결된 후원정보 */
@@ -82,114 +93,115 @@ export class CreateOrderItemDto {
 
   /** 주문당시 이 주문 상품에 포함된  배송비 Decimal @default("0.00") @db.Decimal(10, 2)  */
   @IsNumber()
-  shippingCost: number;
+  shippingCost: OrderItem['shippingCost'];
 
   /**  이 주문 상품에 동일 판매자의 배송비가 포함되었는지 여부 @default(false)  */
   @IsBoolean()
   @IsOptional()
-  shippingCostIncluded?: boolean;
+  shippingCostIncluded?: OrderItem['shippingCostIncluded'];
 
   /**  연결된 배송정책 id */
-  @IsInt()
-  shippingGroupId: number;
+  @IsNumber()
+  shippingGroupId: OrderItem['shippingGroupId'];
 }
 
 /** 주문 Order 생성 dto */
 export class CreateOrderDto {
   /** 소비자 고유번호(비회원 주문인경우 undefined) */
   @ValidateIf((o) => !o.nonMemberOrderFlag)
-  @IsInt()
+  @IsNumber()
   @IsOptional()
-  customerId?: number;
+  customerId?: Order['customerId'];
 
   /** 비회원 주문인 경우 true로 보냄. @default(false) */
   @IsBoolean()
   @IsOptional()
-  nonMemberOrderFlag?: boolean;
+  nonMemberOrderFlag?: Order['nonMemberOrderFlag'];
 
   /** 비회원 주문인 경우 입력받는 비밀번호 - 비회원이 주문조회, 취소시 사용할예정 */
   @IsString()
   @IsOptional()
-  nonMemberOrderPassword?: string;
+  nonMemberOrderPassword?: Order['nonMemberOrderPassword'];
 
   /** 주문금액 = 실제 주문 상품/상품옵션 의 금액 합 */
   @IsNumber()
-  orderPrice: number;
+  orderPrice: Order['orderPrice'];
 
-  /** 결제금액 = 할인(쿠폰;할인코드;마일리지 적용)이후 사용자가 실제 결제한 금액 */
+  /** 결제금액 = 할인(쿠폰,할인코드,마일리지 적용)이후 사용자가 실제 결제한 금액 */
   @IsNumber()
-  paymentPrice: number;
+  paymentPrice: Order['paymentPrice'];
 
   // ---------------- 선물하기가 아닌경우 validation, 선물하기인 경우 서비스함수에서 후원방송인 정보로 저장함
   /** 받는사람명 */
   @ValidateIf((o) => !o.giftFlag)
   @IsString()
-  recipientName: string;
+  recipientName: Order['recipientName'];
 
   /** 받는사람 연락처 */
   @ValidateIf((o) => !o.giftFlag)
   @IsString()
-  recipientPhone: string;
+  recipientPhone: Order['recipientPhone'];
 
   /** 받는사람 이메일 */
   @ValidateIf((o) => !o.giftFlag)
   @IsEmail()
-  recipientEmail: string;
+  recipientEmail: Order['recipientEmail'];
 
   /** 받는사람 주소(배송지) 도로명 */
   @ValidateIf((o) => !o.giftFlag)
   @IsString()
-  recipientAddress: string;
+  recipientAddress: Order['recipientAddress'];
 
   /** 받는사람 주소(배송지) 주소 상세 */
   @ValidateIf((o) => !o.giftFlag)
   @IsString()
-  recipientDetailAddress: string;
+  recipientDetailAddress: Order['recipientDetailAddress'];
 
   /** 받는사람 주소(배송지) 우편 번호 */
   @ValidateIf((o) => !o.giftFlag)
   @IsString()
-  recipientPostalCode: string;
+  recipientPostalCode: Order['recipientPostalCode'];
   // ---------------- 선물하기가 아닌경우 validation, 선물하기인 경우 서비스함수에서 후원방송인 정보로 저장함
 
   /** 주문자명 */
   @IsString()
-  ordererName: string;
+  ordererName: Order['ordererName'];
 
   /** 주문자 연락처 */
   @IsString()
-  ordererPhone: string;
+  ordererPhone: Order['ordererPhone'];
 
   /** 주문자 이메일 */
   @IsEmail()
-  ordererEmail: string;
+  ordererEmail: Order['ordererEmail'];
 
   /** 배송메시지 */
   @IsString()
-  memo: string;
+  memo: Order['memo'];
 
   /** 선물주문 여부/ @default(false) */
   @IsBoolean()
   @IsOptional()
-  giftFlag?: boolean;
+  giftFlag?: Order['giftFlag'];
 
   /** 후원상품 포함여부 @default(false) */
   @IsBoolean()
   @IsOptional()
-  supportOrderIncludeFlag?: boolean;
+  supportOrderIncludeFlag?: Order['supportOrderIncludeFlag'];
 
   /** 묶음배송여부 @default(false) */
   @IsBoolean()
   @IsOptional()
-  bundleFlag?: boolean;
+  bundleFlag?: Order['bundleFlag'];
 
   /** String? // 현금영수증번호 */
   @IsString()
   @IsOptional()
-  cashReceipts?: string;
+  cashReceipts?: Order['cashReceipts'];
 
   /** 주문에 연결된 주문상품들 */
   @IsArray()
+  @ArrayNotEmpty()
   @ValidateNested({ each: true })
   @Type(() => CreateOrderItemDto)
   orderItems: CreateOrderItemDto[];
