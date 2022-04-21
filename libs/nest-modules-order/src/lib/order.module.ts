6ff0847f1d6b44358e2d972ff4895414836de9e5
Jiyeon <a1919361@gmail.com>
2022-04-21 11:19:17 +0900

    [fix] broadcasterModule 잘못된 import 수정

--- libs/nest-modules-order/src/lib/order.module.ts
+++ libs/nest-modules-order/src/lib/order.module.ts
@@ -1,14 +1,15 @@
 import { Module, DynamicModule } from '@nestjs/common';
+import { UserPwManager } from '@project-lc/nest-core';
 import { BroadcasterModule } from '@project-lc/nest-modules-broadcaster';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({})
 export class OrderModule {
-  private static readonly providers = [OrderService];
+  private static readonly providers = [OrderService, UserPwManager];
   private static readonly exports = [];
   private static readonly controllers = [OrderController];
-  private static readonly imports = [BroadcasterModule];
+  private static readonly imports = [BroadcasterModule.withoutControllers()];
 
   static withoutControllers(): DynamicModule {
     return {
