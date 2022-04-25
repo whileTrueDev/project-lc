616858e2f53388232644252ec516affeba677af8
Jiyeon <a1919361@gmail.com>
2022-04-25 11:55:32 +0900

    [feat] 주문 기간조회 필터 추가

--- libs/components-web-kkshow/src/lib/mypage/orderList/CustomerOrderList.tsx
+++ libs/components-web-kkshow/src/lib/mypage/orderList/CustomerOrderList.tsx
@@ -1,8 +1,9 @@
-import { Stack, Text, Button } from '@chakra-ui/react';
+import { Stack, Text, Button, Center } from '@chakra-ui/react';
 import { INFINITE_ORDER_LIST_QUERY_KEY, useInfiniteOrderList } from '@project-lc/hooks';
 import { GetOrderListDto } from '@project-lc/shared-types';
 import { useEffect, useState } from 'react';
 import { useQueryClient } from 'react-query';
+import CustomerOrderPeriodFilter from './CustomerOrderPeriodFilter';
 
 export function CustomerOrderList({ customerId }: { customerId: number }): JSX.Element {
   const queryClient = useQueryClient();
@@ -41,47 +42,44 @@ export function CustomerOrderList({ customerId }: { customerId: number }): JSX.E
     <Stack>
       <Text>주문/배송내역</Text>
       {/* 주문내역 조회 필터 - 기간 */}
+      <CustomerOrderPeriodFilter
+        changePeriod={({ periodStart, periodEnd }) => {
+          setDto((prev) => ({
+            ...prev,
+            periodStart,
+            periodEnd,
+          }));
+        }}
+      />
       {/* 주문내역목록 */}
       {data.pages.map((group, i) => (
         // eslint-disable-next-line react/no-array-index-key
         <Stack key={i}>
           <Text fontWeight="bold"> group index : {i}</Text>
           <Stack>
-            <Text>
-              {group.orders.map((order) => (
-                <Text key={order.id}>
-                  {order.orderCode} {order.createDate}
-                </Text>
-              ))}
-            </Text>
+            {group.orders.map((order) => (
+              <Text key={order.id}>
+                {order.orderCode} {order.createDate}
+              </Text>
+            ))}
           </Stack>
         </Stack>
       ))}
 
       {/* 더보기 버튼 */}
       {hasNextPage && (
-        <Button
-          type="button"
-          isLoading={isFetchingNextPage}
-          onClick={() => fetchNextPage()}
-        >
-          Fetch more
-        </Button>
+        <Center>
+          <Button
+            type="button"
+            size="sm"
+            isLoading={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+          >
+            더보기
+          </Button>
+        </Center>
       )}
       <Text>{JSON.stringify(dto)}</Text>
-      <Button
-        onClick={() => {
-          setDto((prev) => ({
-            ...prev,
-            periodStart: '2021-04-01',
-            periodEnd: '2021-05-20',
-            customerId: undefined,
-            sellerId: 2,
-          }));
-        }}
-      >
-        기간제한
-      </Button>
 
       <Text>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</Text>
     </Stack>
