7af120365b67f63371219ae5689e69300199d628
hwasurr <iamsupermazinga@gmail.com>
2022-04-27 15:51:15 +0900

    fix: 마이그레이션 파일 충돌 수정

--- libs/prisma-orm/prisma/migrations/20220419063400_/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220419063400_/migration.sql
@@ -1,15 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `reason` on the `ConfirmHistory` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `ConfirmHistory` DROP COLUMN `reason`;
-
--- AlterTable
-ALTER TABLE `PrivacyApproachHistory` ADD COLUMN `reason` VARCHAR(191) NULL;
-
 -- AlterTable
 ALTER TABLE `CartItemOption` ADD COLUMN `goodsOptionsId` INTEGER NULL;
 
