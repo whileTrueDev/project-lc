21f8922fd7b3fdaf7b800b4671eb5b8e21f38c79
Jiyeon <a1919361@gmail.com>
2022-04-28 15:47:06 +0900

    [fix] 주문 더미 시드데이터 추가

--- libs/prisma-orm/prisma/migrations/20220421084134_/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220421084134_/migration.sql
@@ -1,12 +1,10 @@
 /*
   Warnings:
 
-  - You are about to drop the column `reason` on the `ConfirmHistory` table. All the data in the column will be lost.
   - Added the required column `goodsOptionId` to the `OrderItemOption` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE `ConfirmHistory` DROP COLUMN `reason`;
+
 
 -- AlterTable
 ALTER TABLE `Customer` MODIFY `gender` ENUM('male', 'female', 'unknown') NULL DEFAULT 'unknown';
@@ -17,6 +15,3 @@ ALTER TABLE `Order` ADD COLUMN `deleteFlag` BOOLEAN NULL DEFAULT false,
 
 -- AlterTable
 ALTER TABLE `OrderItemOption` ADD COLUMN `goodsOptionId` INTEGER NOT NULL;
-
--- AlterTable
-ALTER TABLE `PrivacyApproachHistory` ADD COLUMN `reason` VARCHAR(191) NULL;
