1697d9c425711a54aa7dd10230c41a25fcbbb95f
leejineun <43170606+leejineun@users.noreply.github.com>
2022-05-02 18:00:42 +0900

    feat: 모바일 구현 / 모바일 몇몇 데이터 표시안되는 현상 수정 필요

--- libs/components-web-kkshow/src/lib/payment/DestinationInfo.tsx
+++ libs/components-web-kkshow/src/lib/payment/DestinationInfo.tsx
@@ -13,9 +13,11 @@ import {
   Radio,
   RadioGroup,
   Stack,
+  FormErrorMessage,
   Textarea,
   Checkbox,
   CheckboxGroup,
+  FormControl,
 } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { useDefaultCustomerAddress, useProfile } from '@project-lc/hooks';
@@ -36,9 +38,6 @@ export function DestinationInfo(): JSX.Element {
   } = useDisclosure();
 
   const [checked, setChecked] = useState('manual');
-  const [headPhoneNumber, setHeadPhoneNumber] = useState('');
-  const [bodyPhoneNumber, setBodyPhoneNumber] = useState('');
-  const [tailPhoneNumber, setTailPhoneNumber] = useState('');
   const { data: profile } = useProfile();
   const { data: defaultAddress, isLoading } = useDefaultCustomerAddress(
     profile?.id || undefined,
@@ -50,20 +49,32 @@ export function DestinationInfo(): JSX.Element {
     watch,
     reset,
     getValues,
+    resetField,
     formState: { errors },
   } = useFormContext<any>();
 
   function handleRadio(value: string): void {
     setChecked(value);
     if (value === 'manual') {
-      reset();
+      resetField('recipient');
+      resetField('postalCode');
+      resetField('address');
+      resetField('detailAddress');
+      resetField('phone1');
+      resetField('phone2');
+      resetField('phone3');
     }
   }
 
   useEffect(() => {
     if (!isLoading && defaultAddress) {
       setChecked('default');
-    }
+      setValue('recipient', defaultAddress.recipient);
+      setValue('phone', defaultAddress.phone);
+      setValue('postalCode', defaultAddress.postalCode);
+      setValue('address', defaultAddress.address);
+      setValue('detailAddress', defaultAddress.detailAddress);
+    } else if ()
   }, []);
 
   return (
@@ -102,111 +113,139 @@ export function DestinationInfo(): JSX.Element {
           </Flex>
           <Flex direction="column" alignItems="flex-start" mt={3}>
             <Text fontWeight="bold">배송지주소</Text>
-            <VStack>
-              <Text>
-                {`(${defaultAddress?.postalCode}) ${defaultAddress?.address}`}{' '}
-                {`${defaultAddress?.detailAddress}`}
-              </Text>
-            </VStack>
+            <Flex direction="column">
+              <Text>{`(${defaultAddress?.postalCode}) ${defaultAddress?.address}`}</Text>
+              <Text>{`${defaultAddress?.detailAddress}`}</Text>
+            </Flex>
           </Flex>
         </>
       ) : (
         <>
-          <Flex direction="column">
-            <Text fontWeight="bold">수령인</Text>
-            <Input
-              w="sm"
-              placeholder="수령인"
-              value={watch('recipient')}
-              // onChange={(e) => {
-              //   setValue('recipient', e.target.value);
-              // }}
-              {...register('recipient', {
-                required: {
-                  value: true,
-                  message: '받는사람의 이름을 입력해주세요(2글자 이상)',
-                },
-                minLength: 2,
-              })}
-            />
-          </Flex>
+          <FormControl isInvalid={!!errors.recipient}>
+            <Flex direction="column">
+              <Text fontWeight="bold">수령인</Text>
+              <Input
+                w={{ base: '100%', md: '15%' }}
+                placeholder="수령인"
+                value={watch('recipient')}
+                {...register('recipient', {
+                  required: {
+                    value: true,
+                    message: '받는사람의 이름을 입력해주세요(2글자 이상)',
+                  },
+                  minLength: 2,
+                })}
+              />
+              <FormErrorMessage>
+                {errors.recipient && errors.recipient.message}
+              </FormErrorMessage>
+            </Flex>
+          </FormControl>
           <Flex direction="column">
             <Text fontWeight="bold">연락처</Text>
-            <HStack>
-              <Input
-                w="15%"
-                value={watch('phone').slice(0, 3)}
-                {...register('phone1', {
-                  required: { value: true, message: '휴대전화를 올바르게 입력해주세요.' },
-                  minLength: 3,
-                  maxLength: 4,
-                })}
-              />
-              <Text>-</Text>
-              <Input
-                w="15%"
-                value={watch('phone').slice(3, 7)}
-                {...register('phone2', {
-                  required: { value: true, message: '휴대전화를 올바르게 입력해주세요.' },
-                  minLength: 4,
-                  maxLength: 4,
-                })}
-              />
-              <Text>-</Text>
-              <Input
-                w="15%"
-                value={watch('phone').slice(7, 11)}
-                {...register('phone3', {
-                  required: { value: true, message: '휴대전화를 올바르게 입력해주세요.' },
-                  minLength: 4,
-                  maxLength: 4,
-                })}
-              />
-            </HStack>
+            <FormControl
+              isInvalid={!!errors.phone1 || !!errors.phone2 || !!errors.phone3}
+            >
+              <HStack>
+                <Input
+                  w={{ base: '20%', md: '15%' }}
+                  type="number"
+                  maxLength={3}
+                  {...register('phone1', {
+                    required: {
+                      value: true,
+                      message: '휴대전화를 올바르게 입력해주세요.',
+                    },
+                    minLength: 2,
+                    maxLength: 3,
+                  })}
+                />
+                <Text>-</Text>
+                <Input
+                  w={{ base: '20%', md: '15%' }}
+                  type="number"
+                  maxLength={4}
+                  {...register('phone2', {
+                    required: {
+                      value: true,
+                      message: '휴대전화를 올바르게 입력해주세요.',
+                    },
+                    minLength: 3,
+                    maxLength: 4,
+                  })}
+                />
+                <Text>-</Text>
+                <Input
+                  w={{ base: '20%', md: '15%' }}
+                  type="number"
+                  maxLength={4}
+                  {...register('phone3', {
+                    required: {
+                      value: true,
+                      message: '휴대전화를 올바르게 입력해주세요.',
+                    },
+                    minLength: 3,
+                    maxLength: 4,
+                  })}
+                />
+              </HStack>
+              <FormErrorMessage>휴대전화 번호를 올바르게 입력해주세요.</FormErrorMessage>
+            </FormControl>
           </Flex>
-          <Flex direction="column" alignItems="flex-start">
-            <Text fontWeight="bold">배송지주소</Text>
-            <HStack mt={2} mb={2}>
-              <Button size="sm" onClick={addressOnOpen}>
-                주소찾기
-              </Button>
-              <Input
-                isReadOnly
-                w="15"
-                placeholder="우편번호"
-                value={getValues('postalCode')}
-                {...register('postalCode', {
-                  required: { value: true, message: '우편번호가 올바르지 않습니다' },
-                })}
-              />
-            </HStack>
-            <VStack>
-              <Input
-                isReadOnly
-                placeholder="기본주소"
-                w="md"
-                value={getValues('address')}
-                {...register('address', {
-                  required: { value: true, message: '주소를 입력해주세요' },
-                })}
-              />
-              <Input
-                placeholder="상세주소"
-                w="md"
-                value={watch('detailAddress')}
-                // onChange={(e) => {
-                //   setValue('detailAddress', e.target.value);
-                // }}
-                {...register('detailAddress', {
-                  required: { value: true, message: '상세주소를 입력해주세요' },
-                  maxLength: {
-                    value: 30,
-                    message: '30자 이상 작성할 수 없습니다.',
-                  },
-                })}
-              />
-            </VStack>
-          </Flex>
+          <FormControl
+            isInvalid={!!errors.postalCode || !!errors.address || !!errors.detailAddress}
+          >
+            <Flex direction="column" alignItems="flex-start">
+              <Text fontWeight="bold">배송지주소</Text>
+              <HStack mt={2} mb={2}>
+                <Button size="sm" onClick={addressOnOpen}>
+                  주소찾기
+                </Button>
+                <Input
+                  isReadOnly
+                  w="15"
+                  placeholder="우편번호"
+                  value={getValues('postalCode')}
+                  {...register('postalCode', {
+                    required: { value: true, message: '우편번호가 올바르지 않습니다' },
+                  })}
+                />
+              </HStack>
+              <FormErrorMessage>
+                {errors.postalCode && errors.postalCode.message}
+              </FormErrorMessage>
+              <Flex direction="column" w="100%">
+                <Input
+                  w={{ base: '100%', md: '50%' }}
+                  isReadOnly
+                  placeholder="기본주소"
+                  value={getValues('address')}
+                  {...register('address', {
+                    required: { value: true, message: '주소를 입력해주세요' },
+                  })}
+                  mb={2}
+                />
+                <FormErrorMessage>
+                  {errors.address && errors.address.message}
+                </FormErrorMessage>
+                <Input
+                  w={{ base: '100%', md: '50%' }}
+                  placeholder="상세주소"
+                  value={watch('detailAddress')}
+                  {...register('detailAddress', {
+                    required: { value: true, message: '상세주소를 입력해주세요' },
+                    maxLength: {
+                      value: 30,
+                      message: '30자 이상 작성할 수 없습니다.',
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.detailAddress && errors.detailAddress.message}
+                </FormErrorMessage>
+              </Flex>
+            </Flex>
+          </FormControl>
         </>
       )}
       <DeliveryAddressDialog isOpen={addressIsOpen} onClose={addressOnClose} />
