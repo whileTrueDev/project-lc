c9df8b439e258299f7fe9c661234d9378c51faf1
Jiyeon <a1919361@gmail.com>
2022-05-03 14:20:23 +0900

    [feat] 주문상세조회 훅 생성

---
+++ libs/hooks/src/lib/queries/useOrderDetail.tsx
@@ -0,0 +1,20 @@
+import { OrderDetailRes } from '@project-lc/shared-types';
+import { AxiosError } from 'axios';
+import { useQuery, UseQueryResult } from 'react-query';
+import axios from '../../axios';
+
+export const getOrderDetail = async (id: number): Promise<OrderDetailRes> => {
+  return axios.get<OrderDetailRes>(`/order/${id}`).then((res) => res.data);
+};
+
+export const useOrderDetail = (
+  id: number,
+): UseQueryResult<OrderDetailRes, AxiosError> => {
+  return useQuery<OrderDetailRes, AxiosError>(
+    ['OrderDetail', id],
+    () => getOrderDetail(id),
+    {
+      enabled: !!id,
+    },
+  );
+};
