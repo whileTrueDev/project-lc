6bbddbe2535a7748204fdc3d2f37dc7f46023c33
Jiyeon <a1919361@gmail.com>
2022-05-04 10:40:01 +0900

    [feat] 반품 생성 핸들러 작성

--- libs/nest-modules-return/src/lib/return.controller.ts
+++ libs/nest-modules-return/src/lib/return.controller.ts
@@ -1,4 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseInterceptors,
+  ValidationPipe,
+} from '@nestjs/common';
+import { HttpCacheInterceptor } from '@project-lc/nest-core';
+import { CreateReturnDto, CreateReturnRes } from '@project-lc/shared-types';
+import { ReturnService } from './return.service';
 
+@UseInterceptors(HttpCacheInterceptor)
 @Controller('return')
-export class ReturnController {}
+export class ReturnController {
+  constructor(private readonly returnService: ReturnService) {}
+
+  /** 반품요청 생성 */
+  @Post()
+  createReturn(@Body(ValidationPipe) dto: CreateReturnDto): Promise<CreateReturnRes> {
+    return this.returnService.createReturn(dto);
+  }
+}
--- libs/nest-modules-return/src/lib/return.service.ts
+++ libs/nest-modules-return/src/lib/return.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from '@project-lc/prisma-orm';
+import { ServiceBaseWithCache } from '@project-lc/nest-core';
+import { Cache } from 'cache-manager';
+import { CreateReturnDto, CreateReturnRes } from '@project-lc/shared-types';
+import { nanoid } from 'nanoid';
 
 @Injectable()
-export class ReturnService {}
+export class ReturnService extends ServiceBaseWithCache {
+  #RETURN_CACHE_KEY = 'return';
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject(CACHE_MANAGER) protected readonly cacheManager: Cache,
+  ) {
+    super(cacheManager);
+  }
+
+  /** 반품코드 생성 */
+  private createReturnCode(): string {
+    return nanoid();
+  }
+
+  /** 반품요청 생성 */
+  async createReturn(dto: CreateReturnDto): Promise<CreateReturnRes> {
+    const { orderId, items, images, ...rest } = dto;
+    const data = await this.prisma.return.create({
+      data: {
+        ...rest,
+        returnCode: this.createReturnCode(),
+        order: { connect: { id: orderId } },
+        items: {
+          create: items.map((item) => ({
+            orderItem: { connect: { id: item.orderItemId } },
+            orderItemOption: { connect: { id: item.orderItemOptionId } },
+            amount: item.amount,
+          })),
+        },
+        images: {
+          create: images,
+        },
+      },
+    });
+
+    return data;
+  }
+  /** 반품요청 내역 조회 */
+  /** 특정 반품요청 상세 조회 */
+  /** 반품요청 상태 변경 */
+  /** 반품요청 삭제 */
+}
