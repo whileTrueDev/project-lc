a5701e9c01c01ee5801c350873e02ba73e6b654b
Jiyeon <a1919361@gmail.com>
2022-05-04 13:40:32 +0900

    [fix] order 수정,생성,삭제시 캐시삭제

--- libs/nest-modules-order/src/lib/order.service.ts
+++ libs/nest-modules-order/src/lib/order.service.ts
@@ -174,6 +174,8 @@ export class OrderService extends ServiceBaseWithCache {
     if (order.giftFlag) {
       return this.removerecipientInfo(order);
     }
+
+    await this._clearCaches(this.#ORDER_CACHE_KEY);
     return order;
   }
 
@@ -385,6 +387,7 @@ export class OrderService extends ServiceBaseWithCache {
       data: updateInput,
     });
 
+    await this._clearCaches(this.#ORDER_CACHE_KEY);
     return true;
   }
 
@@ -452,6 +455,8 @@ export class OrderService extends ServiceBaseWithCache {
         data: { purchaseConfirmationDate: new Date(), step: 'purchaseConfirmed' },
       });
     }
+
+    await this._clearCaches(this.#ORDER_CACHE_KEY);
     return true;
   }
 }
