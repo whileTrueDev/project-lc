0f59b049072739dd08465dbb8040a77e8ca4fbf1
Jiyeon <a1919361@gmail.com>
2022-05-04 11:17:07 +0900

    [feat] 반품내역조회 dto 작성

--- libs/shared-types/src/lib/dto/return.dto.ts
+++ libs/shared-types/src/lib/dto/return.dto.ts
@@ -1,6 +1,4 @@
-//* ------- 생성 dto
-
-import { Order, OrderItem, OrderItemOption } from '@prisma/client';
+import { Customer, Order, OrderItem, OrderItemOption, Seller } from '@prisma/client';
 import { Type } from 'class-transformer';
 import {
   ArrayNotEmpty,
@@ -10,6 +8,8 @@ import {
   IsString,
   ValidateNested,
 } from 'class-validator';
+
+//* ------- 생성 dto -------
 
 /** 반품요청 이미지 생성dto */
 export class CreateReturnImageDto {
@@ -72,7 +72,34 @@ export class CreateReturnDto {
   @Type(() => CreateReturnImageDto)
   images?: CreateReturnImageDto[];
 }
+
+//* ------- 조회 dto -------
 /** 반품요청 내역 조회 dto */
+export class GetReturnListDto {
+  /** 한 페이지에 몇개 표시할것인지 */
+  @Type(() => Number)
+  @IsNumber()
+  @IsOptional()
+  take?: number = 10; // take 값 없을때 기본값
+
+  /** 몇개 건너뛰고 조회할것인지 */
+  @Type(() => Number)
+  @IsNumber()
+  @IsOptional()
+  skip?: number = 0;
+
+  /** 특정 소비자의 반품요청 내역 조회시 사용 */
+  @Type(() => Number)
+  @IsNumber()
+  @IsOptional()
+  customerId?: Customer['id'];
+
+  /** 특정 판매자가 판매중인 상품이 포함된 반품요청내역 조회시 사용 */
+  @Type(() => Number)
+  @IsNumber()
+  @IsOptional()
+  sellerId?: Seller['id'];
+}
 /** 특정 반품요청 상세 조회 dto */
 /** 반품요청 상태 변경 dto */
 /** 반품요청 삭제 dto */
