9f4eafb8606a6bb5a533b9dbb7932629dac9963b
Jiyeon <a1919361@gmail.com>
2022-05-06 16:17:46 +0900

    [fix] 사유 required로 변경, 입력안했을때 오류문구출력

--- libs/components-web-kkshow/src/lib/mypage/exchange-return/ReasonSection.tsx
+++ libs/components-web-kkshow/src/lib/mypage/exchange-return/ReasonSection.tsx
@@ -1,18 +1,22 @@
-import { Stack, Text, Textarea } from '@chakra-ui/react';
+import { FormControl, FormErrorMessage, Text, Textarea } from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
 
 export function ReasonSection(): JSX.Element {
-  const { register } = useFormContext();
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   return (
-    <Stack>
+    <FormControl isInvalid={!!errors.reason}>
       <Text fontWeight="bold">재배송/환불 사유</Text>
       <Textarea
         resize="none"
         maxLength={255}
         placeholder="재배송 혹은 환불 요청 사유를 입력해주세요(255자 이내)"
-        {...register('reason')}
+        {...register('reason', { required: '사유를 입력해주세요' })}
       />
-    </Stack>
+      <FormErrorMessage>{errors.reason && errors.reason.message}</FormErrorMessage>
+    </FormControl>
   );
 }
 
