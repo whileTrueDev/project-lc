bd66ba33a4f96768c417c23b0bcd57935e8a49cc
Jiyeon <a1919361@gmail.com>
2022-05-11 16:43:49 +0900

    [refactor] paymentService에서 토스페이먼츠 api 요청하는 부분 TosspaymentApi 사용하도록 수정

--- libs/nest-modules-payment/src/lib/payment.service.ts
+++ libs/nest-modules-payment/src/lib/payment.service.ts
@@ -1,114 +1,47 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-  PaymentByOrderId,
-  PaymentTransaction,
-  TossPaymentCancelDto,
-} from '@project-lc/shared-types';
-import axios, { AxiosRequestConfig } from 'axios';
-
-type PaymentsByDateRequestType = {
-  startDate: string;
-  endDate: string;
-  startingAfter: string;
-  limit: number;
-};
+import { PaymentByOrderId, PaymentTransaction } from '@project-lc/shared-types';
+import { PaymentsByDateRequestType, TossPaymentsApi } from '@project-lc/utils';
 
 @Injectable()
 export class PaymentService {
-  private BASE_URL = 'https://api.tosspayments.com/v1';
   private PAYMENTS_SECRET_KEY: string;
-  private axiosConfigHeaders: AxiosRequestConfig['headers'];
   constructor(private readonly configService: ConfigService) {
     this.PAYMENTS_SECRET_KEY = this.configService.get('PAYMENTS_SECRET_KEY');
-    this.axiosConfigHeaders = {
-      Authorization: `Basic ${Buffer.from(`${this.PAYMENTS_SECRET_KEY}:`).toString(
-        'base64',
-      )}`,
-      'Content-Type': 'application/json',
-    };
   }
 
   /** 결제승인 요청 API */
   async createPayment(dto): Promise<boolean> {
-    return axios
-      .post(
-        `${this.BASE_URL}/payments/${dto.paymentKey}`,
-        {
-          orderId: dto.orderId,
-          amount: dto.amount,
-        },
-        {
-          responseType: 'json',
-          headers: this.axiosConfigHeaders,
-        },
-      )
-      .then(() => {
-        return true;
-      })
-      .catch((err) => {
-        console.error(err);
-        return false;
-      });
+    try {
+      await TossPaymentsApi.createPayment(dto);
+      return true;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   }
 
   /** 주문번호별 결제내역 */
   async getPaymentByOrderId(orderId: string): Promise<PaymentByOrderId> {
-    return axios
-      .get(`${this.BASE_URL}/payments/orders/${orderId}`, {
-        headers: this.axiosConfigHeaders,
-      })
-      .then((v) => {
-        return v.data;
-      })
-      .catch((err) => {
-        console.error(err);
-        return false;
-      });
+    try {
+      return TossPaymentsApi.getPaymentByOrderId(orderId);
+    } catch (error) {
+      console.error(error.response);
+      throw new HttpException(
+        error.response.message || 'error in getPaymentByOrderId',
+        error.response.status || 500,
+      );
+    }
   }
 
   /** 날짜 기간 거래내역 */
   async getPaymentsByDate(dto: PaymentsByDateRequestType): Promise<PaymentTransaction> {
-    return axios
-      .get(`${this.BASE_URL}/transactions`, {
-        params: {
-          startDate: dto.startDate,
-          endDate: dto.endDate,
-          startingAfter: dto.startingAfter,
-          limit: dto.limit,
-        },
-        headers: this.axiosConfigHeaders,
-        responseType: 'json',
-      })
-      .then((v) => {
-        return v.data;
-      })
-      .catch((err) => {
-        console.error(err);
-        return false;
-      });
-  }
-
-  /** 토스페이먼츠 결제취소요청 */
-  async requestCancelTossPayment({
-    paymentKey,
-    cancelReason,
-    cancelAmount,
-  }: TossPaymentCancelDto): Promise<any> {
     try {
-      const postData = {
-        cancelReason,
-        cancelAmount,
-      };
-      const url = `${this.BASE_URL}/payments/${paymentKey}/cancel`;
-      const response = await axios.post(url, postData, {
-        headers: this.axiosConfigHeaders,
-      });
-      return response.data;
+      return TossPaymentsApi.getPaymentsByDate(dto);
     } catch (error) {
       console.error(error.response);
       throw new HttpException(
-        error.response.message || 'error in requestCancelTossPayment',
+        error.response.message || 'error in getPaymentsByDate',
         error.response.status || 500,
       );
     }
