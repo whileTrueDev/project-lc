10856161352d02f71f3057478e18db13b2f8371c
hwasurr <iamsupermazinga@gmail.com>
2022-05-11 14:37:18 +0900

    chore: 마이그레이션 파일 충돌 가능성 제거

--- libs/prisma-orm/prisma/migrations/20220510075825_/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220510075825_/migration.sql
@@ -8,11 +8,6 @@
   - Added the required column `refundAmount` to the `Refund` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE `GoodsInquiry` MODIFY `createDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE `GoodsInquiryComment` MODIFY `createDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
 
 -- AlterTable
 ALTER TABLE `Refund` DROP COLUMN `requestDate`,
