e51ef9639aef09a1d0682eeec52598c216426d87
hwasurr <iamsupermazinga@gmail.com>
2022-05-11 13:39:02 +0900

    chore: 마이그레이션 파일 정리 추가

--- libs/prisma-orm/prisma/migrations/20220509060724_/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220509060724_/migration.sql
@@ -1,11 +1,5 @@
 -- DropForeignKey
 ALTER TABLE `CustomerCoupon` DROP FOREIGN KEY `CustomerCoupon_couponId_fkey`;
 
--- AlterTable
-ALTER TABLE `GoodsInquiry` MODIFY `createDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE `GoodsInquiryComment` MODIFY `createDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
-
 -- AddForeignKey
 ALTER TABLE `CustomerCoupon` ADD CONSTRAINT `CustomerCoupon_couponId_fkey` FOREIGN KEY (`couponId`) REFERENCES `Coupon`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
