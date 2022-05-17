27a8908965261f4b1eb933a5fb032363eb24d011
Jiyeon <a1919361@gmail.com>
2022-05-17 09:12:12 +0900

    [feat] 교환요청 삭제 훅 추가

--- libs/hooks/src/lib/mutation/useCustomerExchangeMutation.tsx
+++ libs/hooks/src/lib/mutation/useCustomerExchangeMutation.tsx
@@ -1,11 +1,14 @@
-import { CreateExchangeDto, CreateExchangeRes } from '@project-lc/shared-types';
+import {
+  CreateExchangeDto,
+  CreateExchangeRes,
+  ExchangeDeleteRes,
+} from '@project-lc/shared-types';
 import { AxiosError } from 'axios';
 import { useQueryClient, useMutation, UseMutationResult } from 'react-query';
 import axios from '../../axios';
 import { INFINITE_ORDER_LIST_QUERY_KEY } from '../queries/useOrderList';
 
 /** 교환요청 생성 훅 */
-
 export const useCustomerExchangeMutation = (): UseMutationResult<
   CreateExchangeRes,
   AxiosError,
@@ -15,6 +18,24 @@ export const useCustomerExchangeMutation = (): UseMutationResult<
   return useMutation<CreateExchangeRes, AxiosError, CreateExchangeDto>(
     (dto: CreateExchangeDto) =>
       axios.post<CreateExchangeRes>('/exchange', dto).then((res) => res.data),
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(INFINITE_ORDER_LIST_QUERY_KEY);
+      },
+    },
+  );
+};
+
+/** 교환요청 삭제 훅 */
+export const useDeleteCustomerExchange = (): UseMutationResult<
+  ExchangeDeleteRes,
+  AxiosError,
+  number
+> => {
+  const queryClient = useQueryClient();
+  return useMutation<ExchangeDeleteRes, AxiosError, number>(
+    (exchangeId: number) =>
+      axios.delete<ExchangeDeleteRes>(`/exchange/${exchangeId}`).then((res) => res.data),
     {
       onSuccess: (data) => {
         queryClient.invalidateQueries(INFINITE_ORDER_LIST_QUERY_KEY);
