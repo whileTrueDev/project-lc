2ee2c9b1d31e25f71eb0f682631f187e5d145d23
Jiyeon <a1919361@gmail.com>
2022-05-23 15:31:00 +0900

    [feat] 판매자센터 mypage/orders 페이지 복사 및 백업

--- apps/web-seller/pages/mypage/orders/backup-index.tsx
+++ apps/web-seller/pages/mypage/orders/backup-index.tsx
@@ -1,10 +1,8 @@
 import { MypageLayout } from '@project-lc/components-shared/MypageLayout';
-import { OrderFilterConsole } from '@project-lc/components-seller/kkshow-order/OrderFilterConsole';
+import { OrderFilterConsole } from '@project-lc/components-seller/OrderFilterConsole';
 import dynamic from 'next/dynamic';
 
-const OrderList = dynamic(
-  () => import('@project-lc/components-seller/kkshow-order/OrderList'),
-);
+const OrderList = dynamic(() => import('@project-lc/components-seller/OrderList'));
 
 export function Index(): JSX.Element {
   return (
