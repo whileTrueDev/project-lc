7f76b8144f4a6d844bb6fe52a5c621102bd24a46
hwasurr <iamsupermazinga@gmail.com>
2022-05-24 12:02:16 +0900

    feat: 사이드바 기능 추가
    - 기본으로 모두 열려있을 수 있는 기능 추가
    - focus시 색상처리 제거 (현재페이지 표시에 혼란을 줌)

--- libs/components-shared/src/lib/navbar/MypageSidebar.tsx
+++ libs/components-shared/src/lib/navbar/MypageSidebar.tsx
@@ -25,8 +25,13 @@ import { useCallback, useMemo } from 'react';
 export interface MypageSidebarPrpps {
   navLinks: Array<MypageLink>;
   isFolded?: boolean;
+  defaultAllOpen?: boolean; // 기본적으로 모든 사이드바네비 카테고리가 오픈되어있는 지 여부
 }
-export function MypageSidebar({ navLinks, isFolded }: MypageSidebarPrpps): JSX.Element {
+export function MypageSidebar({
+  navLinks,
+  isFolded,
+  defaultAllOpen = false,
+}: MypageSidebarPrpps): JSX.Element {
   const realNavLinks = navLinks.filter((l) => !l.isInvisible);
   const router = useRouter();
 
@@ -53,7 +58,14 @@ export function MypageSidebar({ navLinks, isFolded }: MypageSidebarPrpps): JSX.E
   }
 
   return (
-    <Accordion as="nav" allowMultiple allowToggle defaultIndex={[matchedLinkIndex]}>
+    <Accordion
+      as="nav"
+      allowMultiple
+      allowToggle
+      defaultIndex={
+        defaultAllOpen ? [...Array(realNavLinks.length).keys()] : [matchedLinkIndex]
+      }
+    >
       {realNavLinks.map((link) => (
         <SidebarItem key={link.name} link={link} isMatched={isMatched(link)} />
       ))}
@@ -134,7 +146,6 @@ export function SidebarItem({ link, isMatched }: SidebarItemProps): JSX.Element 
         fontWeight={isMatched ? 'bold' : 'medium'}
         fontSize={{ base: 'lg', sm: 'sm' }}
         _hover={{ bg: hoverColor, color: 'whiteAlpha.900' }}
-        _focus={{ bg: hoverColor, color: 'whiteAlpha.900' }}
         justifyContent="space-between"
         alignItems="center"
         onClick={() => {
@@ -178,7 +189,6 @@ function SidebarChildItem({
         role="group"
         rounded="md"
         _hover={{ bg: hoverColor, color: 'whiteAlpha.900' }}
-        _focus={{ bg: hoverColor, color: 'whiteAlpha.900' }}
         bg={isMatched ? 'blue.500' : 'none'}
         color={isMatched ? 'white' : 'inherit'}
         fontWeight={isMatched ? 'bold' : 'medium'}
