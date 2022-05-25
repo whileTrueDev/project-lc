dda31c25479775f0769d9debdfece3a9ec11ecd8
Jiyeon <a1919361@gmail.com>
2022-05-25 10:53:50 +0900

    [feat] 크크쇼 반품테이블 업데이트 훅 작성

--- libs/hooks/src/lib/mutation/useUpdateReturnStatusMutation.tsx
+++ libs/hooks/src/lib/mutation/useUpdateReturnStatusMutation.tsx
@@ -1,8 +1,13 @@
 import { AxiosError } from 'axios';
 import { useMutation, UseMutationResult, useQueryClient } from 'react-query';
-import { ChangeReturnStatusDto } from '@project-lc/shared-types';
+import {
+  ChangeReturnStatusDto,
+  UpdateReturnDto,
+  UpdateReturnRes,
+} from '@project-lc/shared-types';
 import axios from '../../axios';
 
+/** 퍼스트몰 db 반품 업데이트 훅 */
 export const useUpdateReturnStatusMutation = (): UseMutationResult<
   boolean,
   AxiosError,
@@ -17,6 +22,30 @@ export const useUpdateReturnStatusMutation = (): UseMutationResult<
     {
       onSuccess: () => {
         queryClient.invalidateQueries('FmOrder', { refetchInactive: true });
+      },
+    },
+  );
+};
+
+export type ReturnMutationDto = {
+  returnId: number;
+  dto: UpdateReturnDto;
+};
+/** 크크쇼 db 반품 업데이트 훅 */
+export const useUpdateReturnMutation = (): UseMutationResult<
+  UpdateReturnRes,
+  AxiosError,
+  ReturnMutationDto
+> => {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    ({ returnId, dto }: ReturnMutationDto) => {
+      return axios.patch(`/return/${returnId}`, dto);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('OrderDetail');
       },
     },
   );
