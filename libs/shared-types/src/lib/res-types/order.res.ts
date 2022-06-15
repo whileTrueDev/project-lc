c0be6d0c1e18e04dcc9348f29efd8cdebc1af959
leejineun <43170606+leejineun@users.noreply.github.com>
2022-06-15 10:29:55 +0900

    fix: build error

--- libs/shared-types/src/lib/res-types/order.res.ts
+++ libs/shared-types/src/lib/res-types/order.res.ts
@@ -59,7 +59,7 @@ export type OrderCancellationBaseData = Pick<
 > & {
   items: OrderCancellationItem[];
 };
-export type ExportBaseData = Export;
+export type ExportBaseData = Export & { items: ExportItem[] };
 export type ExchangeBaseData = Pick<
   Exchange,
   'id' | 'exchangeCode' | 'status' | 'requestDate' | 'completeDate'
