e4e070a1b1377ec4df2cadf75f5d29b60861a0b5
leejineun <43170606+leejineun@users.noreply.github.com>
2022-06-15 15:23:35 +0900

    refactor: 사용자쿠폰 사용됨, 만료됨 상태에서도 발행목록에 뜨도록 변경

--- libs/nest-modules-coupon/src/lib/customer-coupon.service.ts
+++ libs/nest-modules-coupon/src/lib/customer-coupon.service.ts
@@ -20,7 +20,6 @@ export class CustomerCouponService {
       where: {
         customerId: Number(dto.customerId) || undefined,
         couponId: Number(dto.couponId) || undefined,
-        status: 'notUsed',
       },
       include: {
         customer: {
