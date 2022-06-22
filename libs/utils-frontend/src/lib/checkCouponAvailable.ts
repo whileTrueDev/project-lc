4499327aea3a08fcb1ea3cd5ddc6a328dce0a856
Jiyeon <a1919361@gmail.com>
2022-06-22 17:45:51 +0900

    [fix] 쿠폰 적용불가 문구 수정

--- libs/utils-frontend/src/lib/checkCouponAvailable.ts
+++ libs/utils-frontend/src/lib/checkCouponAvailable.ts
@@ -36,13 +36,13 @@ export function checkCouponAvailable({
   // 주문금액이 쿠폰 할인 금액보다 적은경우 사용불가처리
   if (unit === 'W' && orderPrice < amount) {
     available = false;
-    reasons.push('주문금액이 쿠폰 할인금액보다 적습니다.');
+    reasons.push('주문 상품금액이 쿠폰 할인금액보다 적습니다.');
   }
 
   // 최소주문금액 확인
   if (orderPrice < minOrderAmountWon) {
     available = false;
-    reasons.push(`최소주문금액이 ${minOrderAmountWon}원 이상이어야 적용가능합니다.`);
+    reasons.push(`주문 상품금액이 ${minOrderAmountWon}원 이상이어야 적용가능합니다.`);
   }
 
   // 쿠폰적용 가능한 상품 포함되었는지 확인
