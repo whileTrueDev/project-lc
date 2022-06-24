bb3b9ce17bfaf2559fbcbe8bf08120af301e7344
Jiyeon <a1919361@gmail.com>
2022-06-24 16:15:33 +0900

    [fix] 환불계좌 표시 수정

--- libs/components-shared/src/lib/order/RelatedRefundData.tsx
+++ libs/components-shared/src/lib/order/RelatedRefundData.tsx
@@ -7,10 +7,16 @@ import TextDotConnector from '@project-lc/components-core/TextDotConnector';
 export interface RelatedRefundDataProps {
   refund?: Refund | null;
   estimatedRefundAmount: number; // 환불예정금액
+  /** 소비자가 환불받을 계좌(관리자 환불 처리 전 보여질 환불예정계좌) */
+  refundAccount?: string;
+  /** 소비자가 환불받을 은행(관리자 환불 처리 전 보여질 환불예정은행) */
+  refundBank?: string;
 }
 export function RelatedRefundData({
   refund,
   estimatedRefundAmount,
+  refundAccount,
+  refundBank,
 }: RelatedRefundDataProps): JSX.Element {
   return (
     <Stack>
@@ -22,19 +28,33 @@ export function RelatedRefundData({
               <Text>환불 완료 금액 :</Text>
               <Text>{refund.refundAmount.toLocaleString()}원</Text>
             </Stack>
-            <Stack>
-              <Text>환불 계좌 :</Text>
-              <Text>
-                {refund.refundBank} <TextDotConnector />
-                {refund.refundAccount}
-              </Text>
-            </Stack>
+
+            {refund.refundAccount && (
+              <Stack>
+                <Text>환불 계좌 :</Text>
+                <Text>
+                  {refund.refundBank} <TextDotConnector />
+                  {refund.refundAccount}
+                </Text>
+              </Stack>
+            )}
           </>
         ) : (
-          <Stack>
-            <Text>환불 예정 금액 :</Text>
-            <Text>{estimatedRefundAmount.toLocaleString()}원</Text>
-          </Stack>
+          <>
+            <Stack>
+              <Text>환불 예정 금액 :</Text>
+              <Text>{estimatedRefundAmount.toLocaleString()}원</Text>
+            </Stack>
+            {refundAccount && (
+              <Stack>
+                <Text>환불 예정 계좌 :</Text>
+                <Text>
+                  {refundBank} <TextDotConnector />
+                  {refundAccount}
+                </Text>
+              </Stack>
+            )}
+          </>
         )}
       </Stack>
     </Stack>
