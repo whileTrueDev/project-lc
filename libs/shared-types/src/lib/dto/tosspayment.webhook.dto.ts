f70af68510b2ef6c6bbb780f7d6775933524a31c
hwasurr <iamsupermazinga@gmail.com>
2022-06-28 09:24:10 +0900

    feat: tosspayments 버전업데이트에 따른 변경사항 수정

--- libs/shared-types/src/lib/dto/tosspayment.webhook.dto.ts
+++ libs/shared-types/src/lib/dto/tosspayment.webhook.dto.ts
@@ -6,7 +6,7 @@ export class TossVirtualAccountDto {
   /** secret: 가상계좌 웹훅 요청이 정상적인 요청인지 검증하기 위한 값입니다. 이 값이 결제 승인 API의 응답으로 돌아온 secret과 같으면 정상적인 요청입니다. */
   secret: string | null;
   /** status: 입금 처리 상태입니다. 고객이 가상계좌에 입금하면 값이 DONE입니다. 입금이 취소되면 값이 CANCELED 입니다. */
-  status: 'DONE' | 'CANCELED' | 'PARTIAL_CANCELED';
+  status: 'DONE' | 'CANCELED' | 'PARTIAL_CANCELED' | 'WATING_FOR_DEPOSIT';
   /** orderId: 가맹점에서 주문건에 대해 발급한 고유 ID입니다. */
   orderId: string;
 }
@@ -21,7 +21,7 @@ export class TossVirtualAccountTranslatedDto {
   /** secret: 가상계좌 웹훅 요청이 정상적인 요청인지 검증하기 위한 값입니다. 이 값이 결제 승인 API의 응답으로 돌아온 secret과 같으면 정상적인 요청입니다. */
   secret: string | null;
   /** status: 입금 처리 상태입니다. 고객이 가상계좌에 입금하면 값이 DONE입니다. 입금이 취소되면 값이 CANCELED 입니다. */
-  status: 'DONE' | 'CANCELED' | 'PARTIAL_CANCELED';
+  status: 'DONE' | 'CANCELED' | 'PARTIAL_CANCELED' | 'WATING_FOR_DEPOSIT';
   /** orderId: 가맹점에서 주문건에 대해 발급한 고유 ID, 크크쇼에서의 Order.orderCode와 동일하므로 가독성 향상을 위해 orderCode로 이름 변경. */
   orderCode: string;
 }
