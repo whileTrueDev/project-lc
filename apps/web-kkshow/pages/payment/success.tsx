774d82383a9d2fbfdce7fcc04020e1dd9eb6f465
Jiyeon <a1919361@gmail.com>
2022-06-30 15:51:03 +0900

    [fix] 결제승인요청에 대한 에러핸들러 추가

--- apps/web-kkshow/pages/payment/success.tsx
+++ apps/web-kkshow/pages/payment/success.tsx
@@ -75,57 +75,64 @@ export function Success(): JSX.Element {
         orderId: orderCode,
         paymentKey,
         amount: redirectAmount,
-      }).then((item) => {
-        deleteCookie('amount');
-        isRequested.current = true;
-        if (item.status === 'error') {
-          router.push(`/payment/fail?message=${item.message}`);
-        } else {
-          const orderPaymentId = item.orderPaymentId || undefined; // 토스페이먼츠 결제요청 후 생성한 OrderPayment.id;
+      })
+        .then((item) => {
+          deleteCookie('amount');
+          isRequested.current = true;
+          if (item.status === 'error') {
+            router.push(`/payment/fail?message=${item.message}`);
+          } else {
+            const orderPaymentId = item.orderPaymentId || undefined; // 토스페이먼츠 결제요청 후 생성한 OrderPayment.id;
 
-          // * createOrderDto 만들기 :  createOrderForm 에서 createOrderDto에 해당하는 데이터만 가져오기
-          const createOrderDtoData = extractCreateOrderDtoDataFromCreateOrderForm(order);
+            // * createOrderDto 만들기 :  createOrderForm 에서 createOrderDto에 해당하는 데이터만 가져오기
+            const createOrderDtoData =
+              extractCreateOrderDtoDataFromCreateOrderForm(order);
 
-          const paymentPrice = tossPaymentsAmount; // 결제금액 = 할인(쿠폰,할인코드,마일리지 적용)이후 사용자가 실제 결제한/입금해야 할 금액 + 총 배송비
+            const paymentPrice = tossPaymentsAmount; // 결제금액 = 할인(쿠폰,할인코드,마일리지 적용)이후 사용자가 실제 결제한/입금해야 할 금액 + 총 배송비
 
-          const createOrderDto: CreateOrderDto = {
-            ...createOrderDtoData,
-            paymentId: orderPaymentId,
-            orderCode,
-            recipientEmail: order.recipientEmail || '',
-            paymentPrice,
-          };
+            const createOrderDto: CreateOrderDto = {
+              ...createOrderDtoData,
+              paymentId: orderPaymentId,
+              orderCode,
+              recipientEmail: order.recipientEmail || '',
+              paymentPrice,
+            };
 
-          // * CreateOrderShippingDto 만들기 :  store.shipping을 dto 형태로 바꾸기
-          const shippingDto: CreateOrderShippingDto = {
-            shipping: OrderShippingDataToDto(shipping),
-          };
+            // * CreateOrderShippingDto 만들기 :  store.shipping을 dto 형태로 바꾸기
+            const shippingDto: CreateOrderShippingDto = {
+              shipping: OrderShippingDataToDto(shipping),
+            };
 
-          const dto = {
-            order: createOrderDto,
-            shipping: shippingDto,
-          };
+            const dto = {
+              order: createOrderDto,
+              shipping: shippingDto,
+            };
 
-          // * 주문생성
-          createOrder
-            .mutateAsync(dto)
-            .then((res) => {
-              // 주문스토어 주문,배송비정보 리셋
-              resetOrder();
-              resetShippingData();
-              resetShopNames();
+            // * 주문생성
+            createOrder
+              .mutateAsync(dto)
+              .then((res) => {
+                // 주문스토어 주문,배송비정보 리셋
+                resetOrder();
+                resetShippingData();
+                resetShopNames();
 
-              // 주문완료페이지로 이동
-              const orderId = res.id;
-              router.push(`/payment/receipt?orderId=${orderId}&orderCode=${orderCode}`);
-            })
-            .catch((e) => {
-              console.error(e);
-              console.error(e.response?.data?.message);
-              router.push('/payment/fail?message=주문생성 오류');
-            });
-        }
-      });
+                // 주문완료페이지로 이동
+                const orderId = res.id;
+                router.push(`/payment/receipt?orderId=${orderId}&orderCode=${orderCode}`);
+              })
+              .catch((e) => {
+                console.error(e);
+                console.error(e.response?.data?.message);
+                router.push('/payment/fail?message=주문생성 오류');
+              });
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          console.error(err.response?.data?.message);
+          router.push('/payment/fail?message=결제승인요청 오류');
+        });
     } else if (
       orderCode &&
       paymentKey &&
