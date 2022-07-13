fac8e076190c04073ea7fe01e22fc7dff29ebbb1
Jiyeon <a1919361@gmail.com>
2022-07-13 14:54:57 +0900

    [fix] 토스 페이먼츠 심사 관련 웹수정사항

--- apps/web-kkshow/pages/shopping.tsx
+++ apps/web-kkshow/pages/shopping.tsx
@@ -41,7 +41,7 @@ export default function Shopping(): JSX.Element {
         <ShoppingEventBanner />
         <ShoppingRecommendations />
         <ShoppingReviews />
-        <ShoppingKeywords />
+        {/* <ShoppingKeywords /> */}
       </KkshowLayout>
     </Box>
   );
