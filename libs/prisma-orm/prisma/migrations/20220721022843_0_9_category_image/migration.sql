908d5a579a970706cde26234f6b527798a590532
hwasurr <iamsupermazinga@gmail.com>
2022-07-21 17:32:22 +0900

    db(kkshow-shopping-tab-category): 테이블 및 기본값 추가

--- libs/prisma-orm/prisma/migrations/20220721022843_0_9_category_image/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220721022843_0_9_category_image/migration.sql
@@ -2,6 +2,19 @@
 -- AlterTable
 ALTER TABLE `GoodsCategory` ADD COLUMN `imageSrc` LONGTEXT NULL;
 
+-- CreateTable
+CREATE TABLE `KkshowShoppingTabCategory` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `goodsCategoryId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `KkshowShoppingTabCategory_goodsCategoryId_key`(`goodsCategoryId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `KkshowShoppingTabCategory` ADD CONSTRAINT `KkshowShoppingTabCategory_goodsCategoryId_fkey` FOREIGN KEY (`goodsCategoryId`) REFERENCES `GoodsCategory`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 -- 최초 카테고리 이미지 데이터 추가
 UPDATE `GoodsCategory` SET `imageSrc` = "https://project-lc-dev-test.s3.ap-northeast-2.amazonaws.com/goods-category/8_ICTMpESlc4XXKBQdSE8/220721165004" WHERE `categoryCode` = "8_ICTMpESlc4XXKBQdSE8";
 UPDATE `GoodsCategory` SET `imageSrc` = "https://project-lc-dev-test.s3.ap-northeast-2.amazonaws.com/goods-category/Y0UhII4ELTwhdi5Fkw6c2/220721165016" WHERE `categoryCode` = "Y0UhII4ELTwhdi5Fkw6c2";
@@ -13,3 +26,8 @@ UPDATE `GoodsCategory` SET `imageSrc` = 'https://project-lc-dev-test.s3.ap-north
 UPDATE `GoodsCategory` SET `imageSrc` = 'https://project-lc-dev-test.s3.ap-northeast-2.amazonaws.com/goods-category/_IGJGLoiep3kDtAXR5sez/220721165947' WHERE `categoryCode` = "_IGJGLoiep3kDtAXR5sez";
 UPDATE `GoodsCategory` SET `imageSrc` = 'https://project-lc-dev-test.s3.ap-northeast-2.amazonaws.com/goods-category/b-0sqerAdOBJ464uNH2R4/220721165930' WHERE `categoryCode` = "b-0sqerAdOBJ464uNH2R4";
 UPDATE `GoodsCategory` SET `imageSrc` = 'https://project-lc-dev-test.s3.ap-northeast-2.amazonaws.com/goods-category/cablIPmpwaBiyKfCKDd_L/220721165917' WHERE `categoryCode` = "cablIPmpwaBiyKfCKDd_L";
+
+-- 최초 크크마켓 카테고리 목록 데이터 추가
+INSERT INTO KkshowShoppingTabCategory (goodsCategoryId)
+VALUES
+    (22), (14), (21), (15), (12), (3), (4), (10), (1), (6);
