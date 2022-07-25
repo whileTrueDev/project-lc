5878368fdc1fd8dbce3ac1c134fa117b69d22a12
hwasurr <iamsupermazinga@gmail.com>
2022-07-25 17:55:13 +0900

    feat: subnav 스와이퍼 구성

--- libs/components-web-kkshow/src/lib/KkshowSubNavbar.tsx
+++ libs/components-web-kkshow/src/lib/KkshowSubNavbar.tsx
@@ -1,26 +1,31 @@
-import { Flex, Link, Text } from '@chakra-ui/react';
+import { Box, Link, Text } from '@chakra-ui/react';
 import { useKkshowSubNav } from '@project-lc/hooks';
 import NextLink from 'next/link';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function KkshowSubNavbar(): JSX.Element {
   const { data: subNavLinks } = useKkshowSubNav();
   return (
-    <Flex
-      display={{ base: 'none', md: 'flex' }}
-      maxW="5xl"
-      m="auto"
-      minH="60px"
-      px={4}
-      py={4}
-      gap={4}
-    >
-      {subNavLinks?.map((subNavLink) => (
-        <NextLink key={subNavLink.id} href={subNavLink.link} passHref>
-          <Link pr={2} href={subNavLink.link}>
-            <Text>{subNavLink.name}</Text>
-          </Link>
-        </NextLink>
-      ))}
-    </Flex>
+    <Box maxW="5xl" m="auto" minH="60px" px={4} py={4} gap={4}>
+      <Swiper slidesPerView="auto" style={{ margin: 0 }}>
+        {subNavLinks?.map((subNavLink) => (
+          <SwiperSlide
+            key={subNavLink.id}
+            style={{
+              width: 'auto',
+              paddingRight: 16,
+              alignItems: 'center',
+              display: 'flex',
+            }}
+          >
+            <NextLink href={subNavLink.link} passHref>
+              <Link pr={2} href={subNavLink.link}>
+                <Text>{subNavLink.name}</Text>
+              </Link>
+            </NextLink>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
   );
 }
