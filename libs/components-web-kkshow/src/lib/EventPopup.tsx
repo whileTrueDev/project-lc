3cfa389b85a8532b8330ae356a8a31f737bc0714
Jiyeon <a1919361@gmail.com>
2022-08-02 13:44:55 +0900

    feat: 이벤트배너 이미지 클릭시 회원가입페이지로 이동

--- libs/components-web-kkshow/src/lib/EventPopup.tsx
+++ libs/components-web-kkshow/src/lib/EventPopup.tsx
@@ -9,10 +9,14 @@ import {
   Stack,
   useBoolean,
   useDisclosure,
+  LinkBox,
+  LinkOverlay,
 } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import { s3 } from '@project-lc/utils-s3';
 import { setCookie, getCookie, deleteCookie } from '@project-lc/utils-frontend';
 import { useEffect } from 'react';
+import { getKkshowWebHost } from '@project-lc/utils';
 
 // s3에 저장된 배너이미지
 const KKSHOW_OPEN_EVENT_IMAGE_KEY = 'public/kks_open_event.jpeg';
@@ -23,12 +27,21 @@ export function EventPopup(): JSX.Element {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const [checked, { toggle }] = useBoolean(false);
 
+  const onCheckBoxChange = (): void => {
+    if (!checked) {
+      const maxAge = 24 * 60 * 60; // 1일, 초(second)단위
+      setCookie(EVENT_POPUP_COOKIE, 'Y', { maxAge });
+    } else {
+      deleteCookie(EVENT_POPUP_COOKIE);
+    }
+    toggle();
+  };
+
   useEffect(() => {
     const cookieExist = getCookie(EVENT_POPUP_COOKIE);
     if (!cookieExist) {
       onOpen();
     }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -43,7 +56,15 @@ export function EventPopup(): JSX.Element {
       <ModalContent>
         <ModalCloseButton />
         <ModalBody pt={10} px={0}>
-          <Image src={s3.getSavedObjectUrl(KKSHOW_OPEN_EVENT_IMAGE_KEY)} />
+          {/* 이미지배너 링크 */}
+          <LinkBox>
+            <NextLink href={`${getKkshowWebHost()}/signup`} passHref>
+              <LinkOverlay>
+                <Image src={s3.getSavedObjectUrl(KKSHOW_OPEN_EVENT_IMAGE_KEY)} />
+              </LinkOverlay>
+            </NextLink>
+          </LinkBox>
+
           <Stack
             pt={2}
             px={1}
@@ -51,18 +72,8 @@ export function EventPopup(): JSX.Element {
             alignItems="center"
             justifyContent="space-between"
           >
-            <Checkbox
-              isChecked={checked}
-              onChange={(e) => {
-                if (!checked) {
-                  const maxAge = 24 * 60 * 60; // 1일, 초(second)단위
-                  setCookie(EVENT_POPUP_COOKIE, 'Y', { maxAge });
-                } else {
-                  deleteCookie(EVENT_POPUP_COOKIE);
-                }
-                toggle();
-              }}
-            >
+            {/* 체크박스 */}
+            <Checkbox isChecked={checked} onChange={onCheckBoxChange}>
               오늘 하루동안 안보기
             </Checkbox>
             <Button size="xs" colorScheme="blue" mr={3} onClick={onClose}>
