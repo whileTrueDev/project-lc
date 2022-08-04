32e5a92fd55a682855762013517d1157f28589b8
hwasurr <iamsupermazinga@gmail.com>
2022-08-04 16:27:47 +0900

    improve(kkshow): 상품상세 - 기타정보 이름 렌더링 제거

--- libs/components-web-kkshow/src/lib/goods/GoodsViewAdditionalInfo.tsx
+++ libs/components-web-kkshow/src/lib/goods/GoodsViewAdditionalInfo.tsx
@@ -12,15 +12,14 @@ export function GoodsViewAdditionalInfo(): JSX.Element | null {
   return (
     <Box maxW="5xl" m="auto" id="goods-info" minH="50vh" p={2} pt={20}>
       <Text fontSize="2xl">교환/반품/배송정보</Text>
-      {goods.data.GoodsInfo ? (
-        <>
-          <Text>{goods.data.GoodsInfo.info_name}</Text>
+      <Box my={2}>
+        {goods.data.GoodsInfo ? (
           <Box dangerouslySetInnerHTML={{ __html: goods.data.GoodsInfo.info_value }} />
-        </>
-      ) : (
-        // 상품공통정보 없는경우(= 등록하지 않은 경우) 기본상품공통정보 표시
-        <GoodsDefaultCommonInfoText />
-      )}
+        ) : (
+          // 상품공통정보 없는경우(= 등록하지 않은 경우) 기본상품공통정보 표시
+          <GoodsDefaultCommonInfoText />
+        )}
+      </Box>
     </Box>
   );
 }
