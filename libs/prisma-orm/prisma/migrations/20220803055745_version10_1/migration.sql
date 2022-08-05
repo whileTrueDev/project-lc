3d770233b51fa07f30bca68613dd63bb370cb3ca
hwasurr <iamsupermazinga@gmail.com>
2022-08-05 16:07:50 +0900

    db: 마이그레이션 정리 + 라이브쇼핑메시지세팅-팬닉기본값 추가

--- libs/prisma-orm/prisma/migrations/20220803055745_version10_1/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220803055745_version10_1/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE `LiveShoppingMessageSetting` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `levelCutOffPoint` INTEGER NOT NULL DEFAULT 30000,
     `ttsSetting` ENUM('full', 'nick_purchase', 'nick_purchase_price', 'only_message', 'no_tts', 'no_sound') NOT NULL DEFAULT 'full',
-    `fanNick` VARCHAR(191) NOT NULL DEFAULT '',
+    `fanNick` VARCHAR(191) NOT NULL DEFAULT '비회원',
     `liveShoppingId` INTEGER NOT NULL,
 
     UNIQUE INDEX `LiveShoppingMessageSetting_liveShoppingId_key`(`liveShoppingId`),
