5eba7c0448b98c3a901abe0fa03a9de42a6a4cfd
hwasurr <iamsupermazinga@gmail.com>
2022-08-17 13:01:47 +0900

    fix: 합포장 출고처리 선택한 shipping만 처리하도록 구성

--- libs/components-seller/src/lib/ExportBundleDialog.tsx
+++ libs/components-seller/src/lib/ExportBundleDialog.tsx
@@ -137,11 +137,19 @@ export function ExportBundleDialog({
     const _orders: CreateKkshowExportDto[] = [];
     selectedKeys.forEach((k) => {
       const data = formData[Number(k)];
-      _orders.push({ ...data, deliveryCompany, deliveryNumber });
+      const realData = {
+        ...data,
+        items: data.items.filter((x) => !!x.quantity),
+        deliveryCompany,
+        deliveryNumber,
+      };
+      _orders.push(realData);
     });
-
+    const realOrders = _orders.filter((d) =>
+      selectedOrderShippings.find((x) => x.orderId === d.orderId),
+    );
     // 합포장 출고처리 요청
-    return exportBundle({ exportOrders: _orders });
+    return exportBundle({ exportOrders: realOrders });
   }
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
