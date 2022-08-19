37fade3c6f7d706dd4fc7af56544b14e62adf33f
Jiyeon <a1919361@gmail.com>
2022-08-19 10:23:31 +0900

    fix : 방송인 선택시 관리자메시지 부분만 활성화되는 코드 삭제

--- apps/overlay-controller/src/lib/admin.js
+++ apps/overlay-controller/src/lib/admin.js
@@ -19,7 +19,6 @@ const liveShoppingStateSocket = io(
 socket.on('creator list from server', (data) => {
   if (data && data.length !== 0) {
     $('#connection-status').text('✔️ 정상');
-    $('.admin-to-bc-live-state-board-box button').attr('disabled', false);
   } else {
     $('#connection-status').text('❌ 연결되지 않음');
   }
