c4681695875a7324de8fcdc25831e17d2d334517
hwasurr <iamsupermazinga@gmail.com>
2022-08-22 16:19:24 +0900

    feat: 네이버 소셜 로그인 임시제거

--- libs/components-shared/src/lib/SocialButtonGroup.tsx
+++ libs/components-shared/src/lib/SocialButtonGroup.tsx
@@ -1,7 +1,7 @@
 import { Divider, Stack, Text, VStack } from '@chakra-ui/react';
 import GoogleLoginButton, { UserTypeProps } from './GoogleLoginButton';
 import KakaoLoginButton from './KakaoLoginButton';
-import NaverLoginButton from './NaverLoginButton';
+// import NaverLoginButton from './NaverLoginButton';
 
 export function SocialButtonGroup({
   userType = 'seller',
@@ -21,7 +21,12 @@ export function SocialButtonGroup({
         <Divider flex={{ base: 0.2 }} />
       </Stack>
       <GoogleLoginButton userType={userType} />
-      <NaverLoginButton userType={userType} />
+      {/* 
+      임시제거 220822 by dan
+      연관 일감: [크크쇼,api] 네이버 소셜 로그인 일시 중단 (@[소셜 로그인] 네이버 소셜로그인시 네이버 nickname이 크크쇼 이름으로 설정됨 해당 일감 작업이후 중단 제거)
+      https://www.notion.so/whiletrue/api-1a0ad5939f5a482694feb811e7730bad
+      */}
+      {/* <NaverLoginButton userType={userType} /> */}
       <KakaoLoginButton userType={userType} />
     </VStack>
   );
