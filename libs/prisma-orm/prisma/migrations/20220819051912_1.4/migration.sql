d945dd87417a6c1d53ecd5cf653f9cd16715531c
iamsupermazinga <iamsupermazinga@gmail.com>
2022-08-23 13:44:18 +0900

    db: 마이그레이션 파일 정리

--- libs/prisma-orm/prisma/migrations/20220819051912_1.4/migration.sql
+++ libs/prisma-orm/prisma/migrations/20220819051912_1.4/migration.sql
@@ -1,3 +1,9 @@
+-- DropForeignKey
+ALTER TABLE `CustomerCoupon` DROP FOREIGN KEY `CustomerCoupon_customerId_fkey`;
+
+-- AddForeignKey
+ALTER TABLE `CustomerCoupon` ADD CONSTRAINT `CustomerCoupon_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `Customer`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 -- social account 액세스,리프레시 토큰 길이에 따른 오류로 인해 Varchar(191) -> Text 로 타입 수정
 -- AlterTable
 ALTER TABLE `BroadcasterSocialAccount` MODIFY `accessToken` TEXT NULL,
