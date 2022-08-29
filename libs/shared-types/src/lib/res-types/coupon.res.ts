3c2a1c5d45bde7877a4576181dc11003043e0cb2
Jiyeon <a1919361@gmail.com>
2022-08-29 16:46:48 +0900

    feat : 쿠폰 목록 응답에 상품명, 이미지 추가

--- libs/shared-types/src/lib/res-types/coupon.res.ts
+++ libs/shared-types/src/lib/res-types/coupon.res.ts
@@ -1,10 +1,23 @@
-import { CustomerCoupon, Coupon, Customer, CustomerCouponLog } from '@prisma/client';
+import {
+  CustomerCoupon,
+  Coupon,
+  Customer,
+  CustomerCouponLog,
+  Goods,
+  GoodsImages,
+} from '@prisma/client';
 
 type CustomerCouponCustomer = Pick<Customer, 'nickname' | 'email'>;
 
 export interface CustomerCouponRes extends CustomerCoupon {
   customer: CustomerCouponCustomer;
-  coupon: Coupon & { goods: { id: number }[] }; // 쿠폰적용가능 혹은 쿠폰적용불가 상품 id[]
+  coupon: Coupon & {
+    goods: {
+      id: Goods['id']; // 쿠폰적용가능 혹은 쿠폰적용불가 상품 id
+      goods_name: Goods['goods_name'];
+      image: GoodsImages[]; // 연결된 상품 이미지 중 take 1
+    }[];
+  };
 }
 
 export interface CustomerCouponLogRes extends CustomerCouponLog {
