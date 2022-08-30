a692044aa98c05835fedf5740917197b73e7e1e0
Jiyeon <a1919361@gmail.com>
2022-08-30 11:36:10 +0900

    feat : 쿠폰 상세정보 모달에 적용가능한 상품목록 추가

---
+++ libs/components-web-kkshow/src/lib/mypage/benefits/CustomerCouponApplyGoodsList.tsx
@@ -0,0 +1,54 @@
+import { Grid, GridItem, Image, Link } from '@chakra-ui/react';
+import { Goods, GoodsImages } from '@prisma/client';
+import { CustomerCouponRes } from '@project-lc/shared-types';
+import NextLink from 'next/link';
+
+export interface CustomerCouponApplyGoodsListProps {
+  goodsList: CustomerCouponRes['coupon']['goods'];
+}
+export function CustomerCouponApplyGoodsList({
+  goodsList,
+}: CustomerCouponApplyGoodsListProps): JSX.Element {
+  return (
+    <Grid
+      gridTemplateColumns="repeat(6, 1fr)"
+      fontSize={{ base: 'sm', md: 'md' }}
+      rowGap={1}
+      maxHeight="120px"
+      overflowY="auto"
+    >
+      {goodsList.map((goods) => (
+        <CouponApplyGoodsLink key={goods.id} {...goods} />
+      ))}
+    </Grid>
+  );
+}
+
+export default CustomerCouponApplyGoodsList;
+
+function CouponApplyGoodsLink({
+  id,
+  goods_name,
+  image,
+}: {
+  id: Goods['id'];
+  goods_name: Goods['goods_name'];
+  image: GoodsImages[];
+}): JSX.Element {
+  return (
+    <>
+      <GridItem display="flex" alignItems="center">
+        <Image
+          boxSize="35px"
+          objectFit="cover"
+          src={image[0] ? image[0].image : undefined}
+        />
+      </GridItem>
+      <GridItem colSpan={5}>
+        <NextLink passHref href={`/goods/${id}`}>
+          <Link fontSize="sm">{goods_name}</Link>
+        </NextLink>
+      </GridItem>
+    </>
+  );
+}
