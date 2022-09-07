420bbcfe8a038c8e346e294bea03003153374c31
hwasurr <iamsupermazinga@gmail.com>
2022-09-07 14:07:51 +0900

    db: 마이그레이션 병합

--- libs/prisma-orm/prisma/migrations/20220901063045_remove_orderstep/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `step` on the `Order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Order` DROP COLUMN `step`;
