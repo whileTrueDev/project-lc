27d6ca8a29365a82787086162e7ab9e6fab4e618
Jiyeon <a1919361@gmail.com>
2022-09-16 10:48:20 +0900

    feat : 마이그레이션 파일 생성, 초기값 저장

---
+++ libs/prisma-orm/prisma/migrations/20220916013633_/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `AdminLastCheckedData` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `data` JSON NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+-- 관리자가 확인한 가장 최근 id 저장 초기데이터 생성
+INSERT INTO `AdminLastCheckedData` (`id`, `data`)
+VALUES
+	(1, '{}');
+
