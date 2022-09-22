af6ddb72d8b6964b28fd3fc66bc452ffe2c8101b
Jiyeon <a1919361@gmail.com>
2022-09-22 11:28:44 +0900

    feat : 현황판 소켓서버에 아웃트로 송출 이벤트 핸들러 추가

--- libs/shared-types/src/lib/event-types/live-shopping-state.event-type.ts
+++ libs/shared-types/src/lib/event-types/live-shopping-state.event-type.ts
@@ -8,10 +8,22 @@ export interface LiveShoppingStateServerToClientEvents {
   adminMessageCreated: (data: AdminMessage) => void;
   adminAlertCreated: () => void;
   purchaseMessageUpdated: () => void;
+  playOutro: (roomName?: string) => void;
 }
 
-export interface LiveShoppingStateClientToServerEvents {
+/** LiveShoppingStateGateway 에서 구독하는 메시지(@SubscribeMessage) 타입 */
+export interface LiveShoppingStateServerSubscribeEvents
+  extends LiveShoppingStateBoardToServerEvents,
+    OverlayControllerToServerEvents {}
+
+/** 현황판에서 emit */
+export interface LiveShoppingStateBoardToServerEvents {
   subscribe: (liveShoppingId: number) => void;
+  requestOutroPlay: (roomName?: string) => void;
+}
+
+/** 오버레이 컨트롤러에서 emit */
+export interface OverlayControllerToServerEvents {
   createAdminMessage: (data: AdminMessage) => void;
   createAdminAlert: (liveShoppingId: number) => void;
   updatePurchaseMessage: (liveShoppingId: number) => void;
