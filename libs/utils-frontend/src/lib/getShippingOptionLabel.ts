14829a7e67b9ebbc1976a13adad7f9420df095bb
Jiyeon <a1919361@gmail.com>
2022-10-27 09:43:58 +0900

    fix : 배송비표시 구간반복 배송옵션 글자가 '~당'으로 표시되지 않는 문제 수정

--- libs/utils-frontend/src/lib/getShippingOptionLabel.ts
+++ libs/utils-frontend/src/lib/getShippingOptionLabel.ts
@@ -1,12 +1,19 @@
 import { ShippingOptionDto } from '@project-lc/shared-types';
 
-export function getShippingOptionLabel(item: ShippingOptionDto, suffix: string): string {
-  const { section_st: sectionStart, section_ed: sectionEnd } = item;
+export function getShippingOptionLabel(
+  item: ShippingOptionDto,
+  suffix: string,
+  isLastOption?: boolean,
+): string {
+  const { section_st: sectionStart, section_ed: sectionEnd, shipping_opt_type } = item;
 
-  const startLabel = sectionStart
-    ? `${sectionStart.toLocaleString()} ${suffix} 이상`
-    : `0 ${suffix} 이상`;
-  const endLabel = sectionEnd ? `${sectionEnd.toLocaleString()} ${suffix} 미만` : '';
+  const startValue = sectionStart ? sectionStart.toLocaleString() : 1;
+  const startLabel = '이상';
+  const startSection = `${startValue} ${suffix} ${startLabel}`;
 
-  return `${startLabel} ~ ${endLabel}`;
+  const endValue = sectionEnd ? sectionEnd.toLocaleString() : '';
+  const endLabel = shipping_opt_type.includes('_rep') && isLastOption ? '당' : '미만';
+  const endSection = `${endValue} ${suffix} ${endLabel}`;
+
+  return `${startSection} ~ ${endSection}`;
 }
